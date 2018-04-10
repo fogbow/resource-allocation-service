b20edc3e64d6c1c807c48fd4572a67d93d4a900a
Lucas Lima Vieira <lima.lucaslv@gmail.com>
2018-04-10 16:25:06 -0300

    Create order classes and compute plugin interface

---
+++ src/main/java/org/fogbowcloud/manager/core/plugins/ComputePlugin.java
@@ -0,0 +1,27 @@
+package org.fogbowcloud.manager.core.plugins;
+
+import java.util.List;
+
+import org.fogbowcloud.manager.core.models.ComputeInstance;
+import org.fogbowcloud.manager.core.models.StorageLink;
+import org.fogbowcloud.manager.core.models.Token;
+import org.fogbowcloud.manager.core.models.orders.ComputeOrder;
+
+public interface ComputePlugin {
+
+	public String requestInstance(ComputeOrder computeOrder, String imageId);
+
+	public ComputeInstance getInstance(Token localToken, String instanceId); 
+	
+	public List<ComputeInstance> getInstances(Token localToken);
+	
+	public void removeInstance(Token localToken, String instanceId);
+	
+	public void removeInstances(Token localToken);
+		
+	public String attachStorage(Token localToken, StorageLink storageLink);
+
+	public String detachStorage(Token localToken, StorageLink storageLink);
+	
+	public String getImageId(Token localToken, String imageName);
+}
