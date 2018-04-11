6e72829a141dc780afc439ab276d2294299c4204
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-04-11 14:28:50 -0300

    Added Order States

--- src/main/java/org/fogbowcloud/manager/core/models/orders/OrderState.java
+++ src/main/java/org/fogbowcloud/manager/core/models/orders/OrderState.java
@@ -1,5 +1,16 @@
 package org.fogbowcloud.manager.core.models.orders;
 
-public class OrderState {
+public enum OrderState {
+	
+	OPEN("open"), PENDING("pending"), SPAWNING("spawning"), FULFILLED("fulfilled"), FAILED("failed"), CLOSED("closed");
 
+	private String value;
+
+	private OrderState(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return this.value;
+	}
 }
