b61c56263d66e5e8b916c2e6c9ccb17030a7b6e5
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-04-11 19:54:15 -0300

    Add Credential model and Constructor

--- src/main/java/org/fogbowcloud/manager/core/models/Credential.java
+++ src/main/java/org/fogbowcloud/manager/core/models/Credential.java
@@ -1,5 +1,33 @@
 package org.fogbowcloud.manager.core.models;
 
 public class Credential {
+	private String name;
+	private boolean required;
+	private String valueDefault;
+	
+	public Credential(String name, boolean required, String valueDefault) {
+		this.name = name;
+		this.required = required;
+		this.valueDefault = valueDefault;
+	}
+	
+	public String getName() {
+		return name;
+	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getValueDefault() {
+		return valueDefault;
+	}
+	
+	public void setValueDefault(String valueDefault) {
+		this.valueDefault = valueDefault;
+	}
+
+	public boolean isRequired() {
+		return required;
+	}
 }
