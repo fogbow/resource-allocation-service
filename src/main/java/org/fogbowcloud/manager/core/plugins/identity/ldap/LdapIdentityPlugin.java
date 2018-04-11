4d38839aff1cfc661b6ce356f860072fd875ad81
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-04-11 19:57:40 -0300

    Update LdapIdentityPlugin implementation

--- src/main/java/org/fogbowcloud/manager/core/plugins/identity/ldap/LdapIdentityPlugin.java
+++ src/main/java/org/fogbowcloud/manager/core/plugins/identity/ldap/LdapIdentityPlugin.java
@@ -27,32 +27,31 @@ import javax.naming.directory.SearchResult;
 import org.apache.commons.codec.Charsets;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.log4j.Logger;
+import org.fogbowcloud.manager.core.models.Credential;
 import org.fogbowcloud.manager.core.models.token.Token;
 import org.fogbowcloud.manager.core.plugins.IdentityPlugin;
-import org.fogbowcloud.manager.core.plugins.util.Credential;
-import org.fogbowcloud.manager.core.plugins.util.RSAUtils;
-import org.fogbowcloud.manager.occi.model.ErrorType;
-import org.fogbowcloud.manager.occi.model.OCCIException;
-import org.fogbowcloud.manager.occi.model.ResponseConstants;
+import org.fogbowcloud.manager.core.plugins.identity.util.RSAUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
 
 public class LdapIdentityPlugin implements IdentityPlugin {
 
 	private static final String ATT_EXPIRATION_DATE = "expirationDate";
 	private static final String ATT_NAME = "name";
 	private static final String ATT_LOGIN = "lognin";
-	
+
 	private static final Logger LOGGER = Logger.getLogger(LdapIdentityPlugin.class);
-	private static final long EXPIRATION_INTERVAL = TimeUnit.DAYS.toMillis(365); // One year
+	private static final long EXPIRATION_INTERVAL = TimeUnit.DAYS.toMillis(365); // One
+																					// year
 
 	private static final String PROP_LDAP_BASE = "ldap_base";
 	private static final String PROP_LDAP_URL = "ldap_identity_url";
 	private static final String PROP_LDAP_ENCRYPT_TYPE = "ldap_encrypt_type";
-	
+
 	private static final String PRIVATE_KEY_PATH = "private_key_path";
 	private static final String PUBLIC_KEY_PATH = "public_key_path";
-
+	
 	public static final String CRED_USERNAME = "username";
 	public static final String CRED_PASSWORD = "password";
 	public static final String CRED_AUTH_URL = "authUrl";
@@ -68,75 +67,72 @@ public class LdapIdentityPlugin implements IdentityPlugin {
 	private String ldapBase;
 	private String ldapUrl;
 	private String encryptType;
-	
+
 	private String privateKeyPath;
 	private String publicKeyPath;
 
 	public LdapIdentityPlugin(Properties properties) {
-
-		ldapBase = properties.getProperty(PROP_LDAP_BASE);
-		ldapUrl = properties.getProperty(PROP_LDAP_URL);
-		encryptType = properties.getProperty(PROP_LDAP_ENCRYPT_TYPE);
-		privateKeyPath = properties.getProperty(PRIVATE_KEY_PATH);
-		publicKeyPath = properties.getProperty(PUBLIC_KEY_PATH);
+		this.ldapBase = properties.getProperty(PROP_LDAP_BASE);
+		this.ldapUrl = properties.getProperty(PROP_LDAP_URL);
+		this.encryptType = properties.getProperty(PROP_LDAP_ENCRYPT_TYPE);
+		this.privateKeyPath = properties.getProperty(PRIVATE_KEY_PATH);
+		this.publicKeyPath = properties.getProperty(PUBLIC_KEY_PATH);
 	}
 
 	@Override
 	public Token createToken(Map<String, String> userCredentials) {
-		
+
 		String uid = userCredentials.get(CRED_USERNAME);
 		String password = userCredentials.get(CRED_PASSWORD);
 		String name = null;
-		
+
 		extractLdapPropertiesFromCredentials(userCredentials);
-		
+
 		parseCredentials(userCredentials);
-		
+
 		try {
 			name = ldapAuthenticate(uid, password);
 		} catch (Exception e) {
 			LOGGER.error("Couldn't load account summary from LDAP Server.", e);
-			throw new Exception("UNAUTHORIZED");
+			throw new RuntimeException(HttpStatus.UNAUTHORIZED.toString());
 		}
 
 		Map<String, String> attributes = new HashMap<String, String>();
 		Date expirationDate = new Date(new Date().getTime() + EXPIRATION_INTERVAL);
-		
-		try {					
+
+		try {
 			JSONObject json = new JSONObject();
 			json.put(ATT_LOGIN, uid);
 			json.put(ATT_NAME, name);
 			json.put(ATT_EXPIRATION_DATE, expirationDate.getTime());
-			
+
 			String signature = createSignature(json);
-			
+
 			String accessId = json.toString() + ACCESSID_SEPARATOR + signature;
-			
-			accessId = new String(Base64.encodeBase64(accessId.getBytes(Charsets.UTF_8), 
-					false, false), Charsets.UTF_8);
-			
+
+			accessId = new String(Base64.encodeBase64(accessId.getBytes(Charsets.UTF_8), false, false), Charsets.UTF_8);
+
 			return new Token(accessId, new Token.User(uid, name), expirationDate, attributes);
-			
 		} catch (Exception e) {
 			LOGGER.error("Erro while trying to sign the token.", e);
-			throw new Exception("Erro while trying to sign the token.");
+			throw new RuntimeException(HttpStatus.UNAUTHORIZED.toString());
 		}
-		
+
 	}
 
 	private void extractLdapPropertiesFromCredentials(Map<String, String> userCredentials) {
 		if (ldapBase == null || ldapBase.isEmpty()) {
 			ldapBase = userCredentials.get(CRED_LDAP_BASE);
 		}
-		
+
 		if (ldapUrl == null || ldapUrl.isEmpty()) {
 			ldapUrl = userCredentials.get(CRED_AUTH_URL);
 		}
-		
+
 		if (privateKeyPath == null || privateKeyPath.isEmpty()) {
 			privateKeyPath = userCredentials.get(CRED_PRIVATE_KEY);
 		}
-		
+
 		if (publicKeyPath == null || publicKeyPath.isEmpty()) {
 			publicKeyPath = userCredentials.get(CRED_PUBLIC_KEY);
 		}
@@ -144,51 +140,50 @@ public class LdapIdentityPlugin implements IdentityPlugin {
 
 	@Override
 	public Token reIssueToken(Token token) {
-		
 		return token;
 	}
 
 	@Override
 	public Token getToken(String accessId) {
 		try {
-			
-			String decodedAccessId = new String(Base64.decodeBase64(accessId),
-					Charsets.UTF_8);
-			
+			String decodedAccessId = new String(Base64.decodeBase64(accessId), Charsets.UTF_8);
+
 			String split[] = decodedAccessId.split(ACCESSID_SEPARATOR);
-			if(split == null || split.length < 2){
+			if (split == null || split.length < 2) {
 				LOGGER.error("Invalid accessID: " + decodedAccessId);
-				throw new Exception("UNAUTHORIZED");
+				throw new RuntimeException(HttpStatus.UNAUTHORIZED.toString());
 			}
-			
+
 			String tokenMessage = split[0];
 			String signature = split[1];
-			
+
 			JSONObject root = new JSONObject(tokenMessage);
 			Date expirationDate = new Date(root.getLong(ATT_EXPIRATION_DATE));
-			
-			if(!verifySign(tokenMessage, signature)){
-				LOGGER.error("Invalid accessID: "+decodedAccessId);
-				throw new Exception("UNAUTHORIZED");
+
+			if (!verifySign(tokenMessage, signature)) {
+				LOGGER.error("Invalid accessID: " + decodedAccessId);
+				throw new RuntimeException(HttpStatus.UNAUTHORIZED.toString());
 			}
-			
+
 			String uuid = root.getString(ATT_LOGIN);
 			String name = root.getString(ATT_NAME);
-			return new Token(accessId, new Token.User(uuid, name), expirationDate, 
-					new HashMap<String, String>());			
+			return new Token(accessId, new Token.User(uuid, name), expirationDate, new HashMap<String, String>());
 		} catch (JSONException e) {
 			LOGGER.error("Exception while getting token from json.", e);
-			throw new Exception("UNAUTHORIZED");
-		}			
+			throw new RuntimeException(HttpStatus.UNAUTHORIZED.toString());
+		}
 	}
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	protected String ldapAuthenticate(String uid, String password) throws Exception {
 
+		String contextFactory = "com.sun.jndi.ldap.LdapCtxFactory";
+		String securityAuthentication = "simple";
+		
 		Hashtable env = new Hashtable();
-		env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-		env.put(Context.PROVIDER_URL, ldapUrl);
-		env.put(Context.SECURITY_AUTHENTICATION, "simple");
+		env.put(Context.INITIAL_CONTEXT_FACTORY, contextFactory);
+		env.put(Context.PROVIDER_URL, this.ldapUrl);
+		env.put(Context.SECURITY_AUTHENTICATION, securityAuthentication);
 
 		DirContext ctx = null;
 		String name = null;
@@ -198,13 +193,13 @@ public class LdapIdentityPlugin implements IdentityPlugin {
 
 			ctx = new InitialDirContext(env);
 
-			//Search the directory to get User Name and Domain from UID
+			// Search the directory to get User Name and Domain from UID
 			String filter = "(&(objectClass=inetOrgPerson)(uid={0}))";
 			SearchControls ctls = new SearchControls();
 			ctls.setSearchScope(SearchControls.SUBTREE_SCOPE);
 			ctls.setReturningAttributes(new String[0]);
 			ctls.setReturningObjFlag(true);
-			NamingEnumeration enm = ctx.search(ldapBase, filter, new String[] { uid }, ctls);
+			NamingEnumeration enm = ctx.search(this.ldapBase, filter, new String[] { uid }, ctls);
 
 			String dn = null;
 
@@ -218,7 +213,7 @@ public class LdapIdentityPlugin implements IdentityPlugin {
 				// uid not found or not unique
 				throw new NamingException("Authentication failed");
 			}
-			
+
 			// Bind with found DN and given password
 			ctx.addToEnvironment(Context.SECURITY_PRINCIPAL, dn);
 			ctx.addToEnvironment(Context.SECURITY_CREDENTIALS, password);
@@ -228,9 +223,9 @@ public class LdapIdentityPlugin implements IdentityPlugin {
 			enm.close();
 
 			return name;
-			
+
 		} catch (Exception e) {
-			LOGGER.error("Error while authenticate " + uid +" - Error: "+e.getMessage());
+			LOGGER.error("Error while authenticate " + uid + " - Error: " + e.getMessage());
 			throw e;
 		} finally {
 			ctx.close();
@@ -240,9 +235,9 @@ public class LdapIdentityPlugin implements IdentityPlugin {
 
 	private String extractUserName(SearchResult result) {
 		String nameGroup[] = result.getName().split(",");
-		if(nameGroup != null && nameGroup.length > 0){
+		if (nameGroup != null && nameGroup.length > 0) {
 			String cnName[] = nameGroup[0].split("=");
-			if(cnName != null && cnName.length > 1){
+			if (cnName != null && cnName.length > 1) {
 				return cnName[1];
 			}
 		}
@@ -250,12 +245,11 @@ public class LdapIdentityPlugin implements IdentityPlugin {
 	}
 
 	private String encryptPassword(String password) throws NoSuchAlgorithmException, UnsupportedEncodingException {
-
-		if (encryptType == null || encryptType.isEmpty()) {
+		if (this.encryptType == null || this.encryptType.isEmpty()) {
 			return password;
 		}
 
-		MessageDigest algorithm = MessageDigest.getInstance(encryptType);
+		MessageDigest algorithm = MessageDigest.getInstance(this.encryptType);
 		byte messageDigest[] = algorithm.digest(password.getBytes("UTF-8"));
 
 		StringBuilder hexString = new StringBuilder();
@@ -263,10 +257,10 @@ public class LdapIdentityPlugin implements IdentityPlugin {
 			hexString.append(String.format("%02X", 0xFF & b));
 		}
 
-		return PASSWORD_ENCRYPTED.replaceAll(ENCRYPT_TYPE, encryptType).replaceAll(ENCRYPT_PASS, hexString.toString());
+		return PASSWORD_ENCRYPTED.replaceAll(ENCRYPT_TYPE, this.encryptType).replaceAll(ENCRYPT_PASS, hexString.toString());
 
 	}
-	
+
 	@Override
 	public boolean isValid(String accessId) {
 		try {
@@ -280,58 +274,56 @@ public class LdapIdentityPlugin implements IdentityPlugin {
 	protected String createSignature(JSONObject json) throws IOException, GeneralSecurityException,
 			NoSuchAlgorithmException, InvalidKeyException, SignatureException, UnsupportedEncodingException {
 		RSAPrivateKey privateKey = null;
-		privateKey = RSAUtils.getPrivateKey(privateKeyPath);
+		privateKey = RSAUtils.getPrivateKey(this.privateKeyPath);
 		String signature = RSAUtils.sign(privateKey, json.toString());
 		return signature;
 	}
-	
+
 	protected boolean verifySign(String tokenMessage, String signature) {
 		RSAPublicKey publicKey = null;
 		try {
-			publicKey = RSAUtils.getPublicKey(publicKeyPath);
-			return RSAUtils.verify(publicKey, tokenMessage, signature);	
+			publicKey = RSAUtils.getPublicKey(this.publicKeyPath);
+			return RSAUtils.verify(publicKey, tokenMessage, signature);
 		} catch (Exception e) {
 			LOGGER.error("Erro while trying to validate sing of the token.", e);
-			throw new OCCIException(ErrorType.INTERNAL_SERVER_ERROR, "Erro while trying to validate sing of the token.");
-		}		
+			throw new RuntimeException(HttpStatus.INTERNAL_SERVER_ERROR.toString());
+		}
 	}
-	
+
 	private void parseCredentials(Map<String, String> userCredentials) {
 		String credLdapUrl = userCredentials.get(CRED_AUTH_URL);
-		if(credLdapUrl != null && !credLdapUrl.isEmpty()){
+		if (credLdapUrl != null && !credLdapUrl.isEmpty()) {
 			ldapUrl = credLdapUrl;
 		}
 		String credLdapBase = userCredentials.get(CRED_LDAP_BASE);
-		if(credLdapBase != null && !credLdapBase.isEmpty()){
+		if (credLdapBase != null && !credLdapBase.isEmpty()) {
 			ldapBase = credLdapBase;
 		}
 		String credEncryptType = userCredentials.get(CRED_LDAP_ENCRYPT);
-		if(credEncryptType != null && !credEncryptType.isEmpty()){
+		if (credEncryptType != null && !credEncryptType.isEmpty()) {
 			encryptType = credEncryptType;
 		}
-		
+
 		String credPrivateKeyPath = userCredentials.get(CRED_PRIVATE_KEY);
-		if(credPrivateKeyPath != null && !credPrivateKeyPath.isEmpty()){
+		if (credPrivateKeyPath != null && !credPrivateKeyPath.isEmpty()) {
 			privateKeyPath = credPrivateKeyPath;
 		}
-		
+
 		String credPublicKeyPath = userCredentials.get(CRED_PUBLIC_KEY);
-		if(credPublicKeyPath != null && !credPublicKeyPath.isEmpty()){
+		if (credPublicKeyPath != null && !credPublicKeyPath.isEmpty()) {
 			publicKeyPath = credPublicKeyPath;
 		}
 	}
 
 	@Override
 	public Credential[] getCredentials() {
-		return new Credential[] { 
-				new Credential(CRED_USERNAME, true, null),
-				new Credential(CRED_PASSWORD, true, null), 
-				new Credential(CRED_AUTH_URL, true, null),
+		return new Credential[] { new Credential(CRED_USERNAME, true, null), 
+				new Credential(CRED_PASSWORD, true, null),
+				new Credential(CRED_AUTH_URL, true, null), 
 				new Credential(CRED_LDAP_BASE, true, null),
-				new Credential(CRED_LDAP_ENCRYPT, false, null),
+				new Credential(CRED_LDAP_ENCRYPT, false, null), 
 				new Credential(CRED_PRIVATE_KEY, true, null),
-				new Credential(CRED_PUBLIC_KEY, false, null)
-		};
+				new Credential(CRED_PUBLIC_KEY, false, null) };
 	}
 
 	@Override
