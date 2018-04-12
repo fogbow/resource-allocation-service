8a120a5942a059a95b899d87b4ff5f70b9bbf008
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-04-12 14:32:08 -0300

    Added initial manager core Instance Provider contract

---
+++ src/main/java/org/fogbowcloud/manager/core/instanceprovider/InstanceProvider.java
@@ -0,0 +1,8 @@
+package org.fogbowcloud.manager.core.instanceprovider;
+
+import org.fogbowcloud.manager.core.models.orders.Order;
+import org.fogbowcloud.manager.core.models.orders.instances.OrderInstance;
+
+public interface InstanceProvider {
+	public OrderInstance requestInstance(Order order);
+}
