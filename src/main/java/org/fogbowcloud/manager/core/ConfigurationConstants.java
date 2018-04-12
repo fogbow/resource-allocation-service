1ca30fa38270f82aadd0f845647b06eba79aa8af
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-04-12 14:32:49 -0300

    Added initial configuration constants

---
+++ src/main/java/org/fogbowcloud/manager/core/ConfigurationConstants.java
@@ -0,0 +1,9 @@
+package org.fogbowcloud.manager.core;
+
+public class ConfigurationConstants {
+
+	
+	// SCHEDULER PERIODS KEYS
+	public static final String OPEN_ORDERS_SCHEDULER_PERIOD_KEY = "open_orders_scheduler_period";
+
+}
