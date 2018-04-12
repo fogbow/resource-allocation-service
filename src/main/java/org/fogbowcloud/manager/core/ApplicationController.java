c32bdcb5790c7a728e1d37baf47e261f6edda03a
wassoares <wlfsoares@gmail.com>
2018-04-12 11:31:59 -0300

    create interface between REST and the Controller called ApplicationController.java

---
+++ src/main/java/org/fogbowcloud/manager/core/ApplicationController.java
@@ -0,0 +1,64 @@
+package org.fogbowcloud.manager.core;
+
+import java.util.Collection;
+
+import org.fogbowcloud.manager.core.models.orders.ComputeOrder;
+import org.fogbowcloud.manager.core.models.orders.NetworkOrder;
+import org.fogbowcloud.manager.core.models.orders.StorageOrder;
+
+public class ApplicationController {
+
+	//private AuthenticationController authenticationController;
+	//private ManagerController managerController;
+	
+	public ApplicationController() {}
+
+	public void allocateComputeOrder(ComputeOrder computeOrder) {
+		
+	}
+	
+	public Collection<ComputeOrder> findAllComputeOrder() {
+		return null;		
+	}
+	
+	public ComputeOrder findComputeOrderById(Integer id) {
+		return null;		
+	}
+	
+	public void removeComputeOrder(Integer id) {
+		
+	}
+	
+	public void allocateNetworkOrder(NetworkOrder networkOrder) {
+		
+	}
+	
+	public Collection<NetworkOrder> findAllNetworkOrder() {
+		return null;		
+	}
+	
+	public NetworkOrder findNetworkOrderById(Integer id) {
+		return null;		
+	}
+	
+	public void removeNetworkOrder(Integer id) {
+		
+	}
+	
+	public void allocateStorageOrder(StorageOrder storageOrder) {
+		
+	}
+	
+	public Collection<StorageOrder> findAllStorageOrder() {
+		return null;		
+	}
+	
+	public StorageOrder findStorageOrderById(Integer id) {
+		return null;		
+	}
+	
+	public void removeStorageOrder(Integer id) {
+		
+	}	
+	
+}
