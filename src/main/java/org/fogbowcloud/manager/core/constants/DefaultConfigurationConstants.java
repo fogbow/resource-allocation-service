406406c60d0548e593ebe533ebc045aa0496fa64
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-04-19 11:10:04 -0300

    Changed attend open orders thread scheduler period, by the fact that now the attend open orders method will process only one Order per time

--- src/main/java/org/fogbowcloud/manager/core/constants/DefaultConfigurationConstants.java
+++ src/main/java/org/fogbowcloud/manager/core/constants/DefaultConfigurationConstants.java
@@ -3,5 +3,5 @@ package org.fogbowcloud.manager.core.constants;
 public class DefaultConfigurationConstants {
 	
 	// SCHEDULER PERIODS
-	public static final String OPEN_ORDERS_SCHEDULER_PERIOD = "30000";
+	public static final String OPEN_ORDERS_SCHEDULER_PERIOD = "1000"; // 1 second
 }
