77ab0560027af8e775b5fc3d50a258546e607552
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-04-20 17:41:53 -0300

    Changing OpenOrderSchedulerPeriod Configuration Name

--- src/main/java/org/fogbowcloud/manager/core/constants/ConfigurationConstants.java
+++ src/main/java/org/fogbowcloud/manager/core/constants/ConfigurationConstants.java
@@ -5,6 +5,6 @@ public class ConfigurationConstants {
 	public static final String XMPP_ID_KEY = "xmpp_jid";
 	
 	// SCHEDULER PERIODS KEYS
-	public static final String OPEN_ORDERS_SCHEDULER_PERIOD_KEY = "open_orders_scheduler_period";
+	public static final String OPEN_ORDERS_SLEEP_TIME_KEY = "open_orders_scheduler_period";
 
 }
--- src/main/java/org/fogbowcloud/manager/core/constants/DefaultConfigurationConstants.java
+++ src/main/java/org/fogbowcloud/manager/core/constants/DefaultConfigurationConstants.java
@@ -3,5 +3,5 @@ package org.fogbowcloud.manager.core.constants;
 public class DefaultConfigurationConstants {
 	
 	// SCHEDULER PERIODS
-	public static final String OPEN_ORDERS_SCHEDULER_PERIOD = "1000"; // 1 second
+	public static final String OPEN_ORDERS_SLEEP_TIME = "1000"; // 1 second
 }
