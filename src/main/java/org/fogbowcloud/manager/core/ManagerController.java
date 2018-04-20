960be80c05fada14df2cf015971f4f3a52e76b02
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-04-20 17:43:44 -0300

    Removing Threads methods from ManagerController and Adding new Thread class to process open orders

--- src/main/java/org/fogbowcloud/manager/core/ManagerController.java
+++ src/main/java/org/fogbowcloud/manager/core/ManagerController.java
@@ -1,113 +1,47 @@
 package org.fogbowcloud.manager.core;
 
 import java.util.Properties;
-import java.util.TimerTask;
-import java.util.concurrent.Executors;
 
 import org.apache.log4j.Logger;
 import org.fogbowcloud.manager.core.constants.ConfigurationConstants;
-import org.fogbowcloud.manager.core.constants.DefaultConfigurationConstants;
 import org.fogbowcloud.manager.core.instanceprovider.InstanceProvider;
-import org.fogbowcloud.manager.core.models.orders.Order;
 import org.fogbowcloud.manager.core.models.orders.OrderRegistry;
-import org.fogbowcloud.manager.core.models.orders.OrderState;
-import org.fogbowcloud.manager.core.models.orders.instances.OrderInstance;
+import org.fogbowcloud.manager.core.threads.AttendOpenOrdersThread;
 
 public class ManagerController {
 
 	private OrderRegistry orderRegistry;
-	private ManagerScheduledExecutorService attendOpenOrdersExecutor;
 
 	private InstanceProvider localInstanceProvider;
 	private InstanceProvider remoteInstanceProvider;
-	
+
+	private Thread attendOpenOrdersThread;
+
 	private String localMemberId;
 
 	private static final Logger LOGGER = Logger.getLogger(ManagerController.class);
 
-	public ManagerController(Properties properties) {
+	public ManagerController(Properties properties, InstanceProvider localInstanceProvider,
+			InstanceProvider remoteInstanceProvider, OrderRegistry orderRegistry) {
 		this.localMemberId = properties.getProperty(ConfigurationConstants.XMPP_ID_KEY);
-		this.attendOpenOrdersExecutor = new ManagerScheduledExecutorService(Executors.newScheduledThreadPool(1));
+		this.localInstanceProvider = localInstanceProvider;
+		this.remoteInstanceProvider = remoteInstanceProvider;
+		this.orderRegistry = orderRegistry;
 
-		this.scheduleExecutorsServices(properties);
-	}
-
-	private void scheduleExecutorsServices(Properties properties) {
-		if (!this.attendOpenOrdersExecutor.isScheduled()) {
-			String schedulerPeriodStr = properties.getProperty(ConfigurationConstants.OPEN_ORDERS_SCHEDULER_PERIOD_KEY,
-					DefaultConfigurationConstants.OPEN_ORDERS_SCHEDULER_PERIOD);
-			Long schedulerPeriod = Long.valueOf(schedulerPeriodStr);
-			this.attendOpenOrdersExecutor.scheduleAtFixedRate(new TimerTask() {
-				@Override
-				public void run() {
-					try {
-						attendOpenOrders();
-					} catch (Throwable e) {
-						LOGGER.error("Error while trying to attend open orders", e);
-					}
-				}
-			}, 0, schedulerPeriod);
-		}
+		this.attendOpenOrdersThread = new AttendOpenOrdersThread(this.localInstanceProvider,
+				this.remoteInstanceProvider, this.orderRegistry, this.localMemberId, properties);
+		
+		this.startManagerThreads();
 	}
 
 	/**
-	 * Method that try to get an Instance for an Open Order. This method can
-	 * generate a race condition. For example: a user can delete a Open Order
-	 * while this method is trying to get an Instance for this Order.
+	 * This method starts all manager threads, if you have defined a new manager
+	 * operation and this operation require a new thread to run, you should
+	 * start this thread at this method.
 	 */
-	private synchronized void attendOpenOrders() {
-		Order order = this.orderRegistry.getNextOpenOrder();
-		if (order != null) {
-			LOGGER.info("Trying to get an Instance for Order [" + order.getId() + "]");
-
-			try {
-				InstanceProvider instanceProvider = null;
-				if (order.isLocal(this.localMemberId)) {
-					LOGGER.info("The open order [" + order.getId() + "] is local");
-					
-					instanceProvider = this.localInstanceProvider;
-				} else if (order.isRemote(this.localMemberId)) {
-					LOGGER.info("The open order [" + order.getId() + "] is remote for the member ["
-							+ order.getProvidingMember() + "]");
-					
-					instanceProvider = this.remoteInstanceProvider;
-				}
-				
-				order.processOpenOrder(instanceProvider);
-				
-				if (order.isLocal(this.localMemberId)) {
-					OrderInstance orderInstance = order.getOrderInstance();
-					String orderInstanceId = orderInstance.getId();
-					if (!orderInstanceId.isEmpty()) {
-						LOGGER.info("The open order [" + order.getId() + "] got an local instance with id ["
-								+ orderInstanceId + "], setting your state to SPAWNING");
-						
-						order.setOrderState(OrderState.SPAWNING);
-					}
-				} else if (order.isRemote(this.localMemberId)) {
-					LOGGER.info("The open order [" + order.getId()
-							+ "] was requested for remote member, setting your state to PENDING");
-					
-					order.setOrderState(OrderState.PENDING);
-				}
-			} catch (Exception e) {
-				LOGGER.error("Error while trying to get an Instance for Order: " + System.lineSeparator() + order, e);
-				order.setOrderState(OrderState.FAILED);
-			}
-			this.orderRegistry.updateOrder(order);
-		}
-	}
-	
-	protected void setOrderRegistry(OrderRegistry orderRegistry) {
-		this.orderRegistry = orderRegistry;
-	}
-
-	protected void setLocalInstanceProvider(InstanceProvider localInstanceProvider) {
-		this.localInstanceProvider = localInstanceProvider;
-	}
-
-	protected void setRemoteInstanceProvider(InstanceProvider remoteInstanceProvider) {
-		this.remoteInstanceProvider = remoteInstanceProvider;
+	private void startManagerThreads() {
+		LOGGER.info("Starting Manager Threads...");
+		this.attendOpenOrdersThread.start();
 	}
 
 }
--- src/main/java/org/fogbowcloud/manager/core/threads/AttendOpenOrdersThread.java
+++ src/main/java/org/fogbowcloud/manager/core/threads/AttendOpenOrdersThread.java
@@ -1,73 +1,105 @@
-package org.fogbowcloud.manager.core.models.threads;
+package org.fogbowcloud.manager.core.threads;
+
+import java.util.Properties;
 
 import org.apache.log4j.Logger;
-import org.fogbowcloud.manager.core.ManagerController;
+import org.fogbowcloud.manager.core.constants.ConfigurationConstants;
+import org.fogbowcloud.manager.core.constants.DefaultConfigurationConstants;
 import org.fogbowcloud.manager.core.instanceprovider.InstanceProvider;
 import org.fogbowcloud.manager.core.models.orders.Order;
 import org.fogbowcloud.manager.core.models.orders.OrderRegistry;
 import org.fogbowcloud.manager.core.models.orders.OrderState;
 import org.fogbowcloud.manager.core.models.orders.instances.OrderInstance;
 
-public class AttendOpenOrderThread extends Thread {
+public class AttendOpenOrdersThread extends Thread {
 
 	private InstanceProvider localInstanceProvider;
 	private InstanceProvider remoteInstanceProvider;
 
 	private OrderRegistry orderRegistry;
-	
-	private String localMemberId;
-	
-	private static final Logger LOGGER = Logger.getLogger(AttendOpenOrderThread.class);
 
-	public AttendOpenOrderThread(InstanceProvider localInstanceProvider, InstanceProvider remoteInstanceProvider,
-			OrderRegistry orderRegistry, String localMemberId) {
+	private String localMemberId;
+
+	/**
+	 * Attribute that represents the thread sleep time when there is no Orders
+	 * to be processed.
+	 */
+	private Long sleepTime;
+
+	private static final Logger LOGGER = Logger.getLogger(AttendOpenOrdersThread.class);
+
+	public AttendOpenOrdersThread(InstanceProvider localInstanceProvider, InstanceProvider remoteInstanceProvider,
+			OrderRegistry orderRegistry, String localMemberId, Properties properties) {
 		this.localInstanceProvider = localInstanceProvider;
 		this.remoteInstanceProvider = remoteInstanceProvider;
 		this.orderRegistry = orderRegistry;
 		this.localMemberId = localMemberId;
+
+		String schedulerPeriodStr = properties.getProperty(ConfigurationConstants.OPEN_ORDERS_SLEEP_TIME_KEY,
+				DefaultConfigurationConstants.OPEN_ORDERS_SLEEP_TIME);
+		this.sleepTime = Long.valueOf(schedulerPeriodStr);
 	}
 
+	/**
+	 * Method that try to get an Instance for an Open Order. This method can
+	 * generate a race condition. For example: a user can delete a Open Order
+	 * while this method is trying to get an Instance for this Order.
+	 */
 	@Override
 	public void run() {
-		Order order = this.orderRegistry.getNextOpenOrder();
-		if (order != null) {
-			LOGGER.info("Trying to get an Instance for Order [" + order.getId() + "]");
+		while (true) {
+			Order order = this.orderRegistry.getNextOpenOrder();
+			if (order != null) {
+				synchronized (order) {
+					OrderState orderState = order.getOrderState();
 
-			try {
-				InstanceProvider instanceProvider = null;
-				if (order.isLocal(this.localMemberId)) {
-					LOGGER.info("The open order [" + order.getId() + "] is local");
-					
-					instanceProvider = this.localInstanceProvider;
-				} else if (order.isRemote(this.localMemberId)) {
-					LOGGER.info("The open order [" + order.getId() + "] is remote for the member ["
-							+ order.getProvidingMember() + "]");
-					
-					instanceProvider = this.remoteInstanceProvider;
-				}
-				
-				order.processOpenOrder(instanceProvider);
-				
-				if (order.isLocal(this.localMemberId)) {
-					OrderInstance orderInstance = order.getOrderInstance();
-					String orderInstanceId = orderInstance.getId();
-					if (!orderInstanceId.isEmpty()) {
-						LOGGER.info("The open order [" + order.getId() + "] got an local instance with id ["
-								+ orderInstanceId + "], setting your state to SPAWNING");
-						
-						order.setOrderState(OrderState.SPAWNING, this.orderRegistry);
+					if (orderState.equals(OrderState.OPEN)) {
+						LOGGER.info("Trying to get an Instance for Order [" + order.getId() + "]");
+
+						try {
+							InstanceProvider instanceProvider = null;
+							if (order.isLocal(this.localMemberId)) {
+								LOGGER.info("The open order [" + order.getId() + "] is local");
+
+								instanceProvider = this.localInstanceProvider;
+							} else if (order.isRemote(this.localMemberId)) {
+								LOGGER.info("The open order [" + order.getId() + "] is remote for the member ["
+										+ order.getProvidingMember() + "]");
+
+								instanceProvider = this.remoteInstanceProvider;
+							}
+
+							order.processOpenOrder(instanceProvider);
+
+							if (order.isLocal(this.localMemberId)) {
+								OrderInstance orderInstance = order.getOrderInstance();
+								String orderInstanceId = orderInstance.getId();
+								if (!orderInstanceId.isEmpty()) {
+									LOGGER.info("The open order [" + order.getId() + "] got an local instance with id ["
+											+ orderInstanceId + "], setting your state to SPAWNING");
+
+									order.setOrderState(OrderState.SPAWNING, this.orderRegistry);
+								}
+							} else if (order.isRemote(this.localMemberId)) {
+								LOGGER.info("The open order [" + order.getId()
+										+ "] was requested for remote member, setting your state to PENDING");
+
+								order.setOrderState(OrderState.PENDING, this.orderRegistry);
+							}
+						} catch (Exception e) {
+							LOGGER.error("Error while trying to get an Instance for Order: " + System.lineSeparator()
+									+ order, e);
+							order.setOrderState(OrderState.FAILED, this.orderRegistry);
+						}
 					}
-				} else if (order.isRemote(this.localMemberId)) {
-					LOGGER.info("The open order [" + order.getId()
-							+ "] was requested for remote member, setting your state to PENDING");
-					
-					order.setOrderState(OrderState.PENDING, this.orderRegistry);
 				}
-			} catch (Exception e) {
-				LOGGER.error("Error while trying to get an Instance for Order: " + System.lineSeparator() + order, e);
-				order.setOrderState(OrderState.FAILED, this.orderRegistry);
+			} else {
+				try {
+					Thread.sleep(this.sleepTime);
+				} catch (InterruptedException e) {
+					LOGGER.error("Error while trying to sleep Attend Order Thread", e);
+				}
 			}
 		}
 	}
-
 }
