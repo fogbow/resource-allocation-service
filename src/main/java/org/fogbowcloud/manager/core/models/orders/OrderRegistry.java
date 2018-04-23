e21d145bfe661e801c88ed316c8c9b20de65eb47
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-04-23 09:25:35 -0300

    Changing OrderRegistry contract to be more general

--- src/main/java/org/fogbowcloud/manager/core/models/orders/OrderRegistry.java
+++ src/main/java/org/fogbowcloud/manager/core/models/orders/OrderRegistry.java
@@ -1,7 +1,7 @@
 package org.fogbowcloud.manager.core.models.orders;
 
 public interface OrderRegistry {
-	public Order getNextOpenOrder();
+	public Order getNextOrderByState(OrderState orderState);
 	
 	public void updateOrder(Order order);
 }
