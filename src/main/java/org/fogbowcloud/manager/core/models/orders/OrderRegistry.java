2f3c2c102e8ce6758cacb53dfe1027d48c6e543b
wassoares <wlfsoares@gmail.com>
2018-04-23 14:04:22 -0300

    create AttendSpawningOrdersThread.class to process Spawning Orders

--- src/main/java/org/fogbowcloud/manager/core/models/orders/OrderRegistry.java
+++ src/main/java/org/fogbowcloud/manager/core/models/orders/OrderRegistry.java
@@ -1,7 +1,12 @@
 package org.fogbowcloud.manager.core.models.orders;
 
 public interface OrderRegistry {
+	
 	public Order getNextOrderByState(OrderState orderState);
+	
+	public boolean removeFromSpwaningOrders(Order order);
+	
+	public void insertIntoSpwaningOrders(Order order);
 	
 	public void updateOrder(Order order);
 }
---
+++ src/main/java/org/fogbowcloud/manager/core/threads/AttendSpawningOrdersThread.java
@@ -0,0 +1,86 @@
+package org.fogbowcloud.manager.core.threads;
+
+import org.fogbowcloud.manager.core.models.orders.Order;
+import org.fogbowcloud.manager.core.models.orders.OrderRegistry;
+import org.fogbowcloud.manager.core.models.orders.OrderState;
+import org.fogbowcloud.manager.core.models.orders.OrderType;
+import org.fogbowcloud.manager.core.models.orders.instances.ComputeOrderInstance;
+import org.fogbowcloud.manager.core.models.orders.instances.InstanceState;
+import org.fogbowcloud.manager.core.models.orders.instances.OrderInstance;
+
+public class AttendSpawningOrdersThread extends Thread {
+
+	private OrderRegistry orderRegistry;
+
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				Order order = this.orderRegistry.getNextOrderByState(OrderState.SPAWNING);
+				if (order != null) {
+					synchronized (order) {
+						OrderState orderState = order.getOrderState();
+
+						if (orderState.equals(OrderState.SPAWNING)) {
+							// get instance of the cloud...
+							OrderInstance orderInstance = order.getOrderInstance();
+							// check if type of the order is COMPUTE...
+							if (order.getType().equals(OrderType.COMPUTE)) {
+								// The attribute InstanceState is in ComputeOrderInstance, and not
+								// OrderInstance...
+								ComputeOrderInstance computeOrderInstance = (ComputeOrderInstance) orderInstance;
+								// check if the instance is running...
+								if (computeOrderInstance.getState().equals(InstanceState.ACTIVE)) {
+									// try to communicate by SSH...
+									computeOrderInstance = waitForAnswer(order);
+									
+									// if successfully do...
+									/// remove Order from spawningOrders...
+									/// set Order state to FULFILLED...
+									/// insert Order in fulfilledOrders...
+
+									// if failed after a few attempts do...									
+									/// remove Order from spawningOrders
+									/// set OrderState to FALLED...
+									/// insert Order in failedOrders...
+									changeOrderStateToFalled(order);
+
+								} else if (computeOrderInstance.getState().equals(InstanceState.INACTIVE)) {
+									changeOrderStateToFalled(order);
+								}
+
+							}
+
+						}
+
+					}
+				}
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+	}
+
+	private void changeOrderStateToFalled(Order order) {
+		/// remove Order from spawningOrders
+		this.orderRegistry.removeFromSpwaningOrders(order);									
+		/// set OrderState to FALLED...
+		order.setOrderState(OrderState.FAILED, this.orderRegistry);
+		/// insert Order in failedOrders...
+		this.orderRegistry.insertIntoSpwaningOrders(order);
+	}
+	
+	private ComputeOrderInstance waitForAnswer(Order order) {
+		int attempts = 3;
+		while (attempts > 0) {
+			try {
+				// try to get IP and Port of the instance...
+				Thread.sleep(NORM_PRIORITY);
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+		return null;
+	}
+
+}
