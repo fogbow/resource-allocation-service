ca18607b09e32a5e63fe8f280ee94ea00af70035
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-04-23 08:48:54 -0300

    Adding try catch in AttendOpenOrders run method

--- src/main/java/org/fogbowcloud/manager/core/threads/AttendOpenOrdersThread.java
+++ src/main/java/org/fogbowcloud/manager/core/threads/AttendOpenOrdersThread.java
@@ -44,62 +44,71 @@ public class AttendOpenOrdersThread extends Thread {
 	 * Method that try to get an Instance for an Open Order. This method can
 	 * generate a race condition. For example: a user can delete a Open Order
 	 * while this method is trying to get an Instance for this Order.
+	 * 
+	 * What this method should do?
 	 */
 	@Override
 	public void run() {
 		while (true) {
-			Order order = this.orderRegistry.getNextOpenOrder();
-			if (order != null) {
-				synchronized (order) {
-					OrderState orderState = order.getOrderState();
+			try {
+				Order order = this.orderRegistry.getNextOpenOrder();
+				if (order != null) {
+					synchronized (order) {
+						OrderState orderState = order.getOrderState();
 
-					if (orderState.equals(OrderState.OPEN)) {
-						LOGGER.info("Trying to get an Instance for Order [" + order.getId() + "]");
+						if (orderState.equals(OrderState.OPEN)) {
+							LOGGER.info("Trying to get an Instance for Order [" + order.getId() + "]");
 
-						try {
-							InstanceProvider instanceProvider = null;
-							if (order.isLocal(this.localMemberId)) {
-								LOGGER.info("The open order [" + order.getId() + "] is local");
+							try {
+								InstanceProvider instanceProvider = this.getInstanceProviderForOrder(order);
 
-								instanceProvider = this.localInstanceProvider;
-							} else if (order.isRemote(this.localMemberId)) {
-								LOGGER.info("The open order [" + order.getId() + "] is remote for the member ["
-										+ order.getProvidingMember() + "]");
+								order.processOpenOrder(instanceProvider);
 
-								instanceProvider = this.remoteInstanceProvider;
-							}
+								if (order.isLocal(this.localMemberId)) {
+									OrderInstance orderInstance = order.getOrderInstance();
+									String orderInstanceId = orderInstance.getId();
+									if (!orderInstanceId.isEmpty()) {
+										LOGGER.info(
+												"The open order [" + order.getId() + "] got an local instance with id ["
+														+ orderInstanceId + "], setting your state to SPAWNING");
 
-							order.processOpenOrder(instanceProvider);
+										order.setOrderState(OrderState.SPAWNING, this.orderRegistry);
+									}
+								} else if (order.isRemote(this.localMemberId)) {
+									LOGGER.info("The open order [" + order.getId()
+											+ "] was requested for remote member, setting your state to PENDING");
 
-							if (order.isLocal(this.localMemberId)) {
-								OrderInstance orderInstance = order.getOrderInstance();
-								String orderInstanceId = orderInstance.getId();
-								if (!orderInstanceId.isEmpty()) {
-									LOGGER.info("The open order [" + order.getId() + "] got an local instance with id ["
-											+ orderInstanceId + "], setting your state to SPAWNING");
-
-									order.setOrderState(OrderState.SPAWNING, this.orderRegistry);
+									order.setOrderState(OrderState.PENDING, this.orderRegistry);
 								}
-							} else if (order.isRemote(this.localMemberId)) {
-								LOGGER.info("The open order [" + order.getId()
-										+ "] was requested for remote member, setting your state to PENDING");
-
-								order.setOrderState(OrderState.PENDING, this.orderRegistry);
+							} catch (Exception e) {
+								LOGGER.error("Error while trying to get an Instance for Order: "
+										+ System.lineSeparator() + order, e);
+								order.setOrderState(OrderState.FAILED, this.orderRegistry);
 							}
-						} catch (Exception e) {
-							LOGGER.error("Error while trying to get an Instance for Order: " + System.lineSeparator()
-									+ order, e);
-							order.setOrderState(OrderState.FAILED, this.orderRegistry);
 						}
 					}
-				}
-			} else {
-				try {
+				} else {
 					Thread.sleep(this.sleepTime);
-				} catch (InterruptedException e) {
-					LOGGER.error("Error while trying to sleep Attend Order Thread", e);
 				}
+			} catch (Exception e) {
+				LOGGER.error("Error while trying to sleep Attend Order Thread", e);
 			}
 		}
 	}
+
+	protected InstanceProvider getInstanceProviderForOrder(Order order) {
+		InstanceProvider instanceProvider = null;
+		if (order.isLocal(this.localMemberId)) {
+			LOGGER.info("The open order [" + order.getId() + "] is local");
+
+			instanceProvider = this.localInstanceProvider;
+		} else if (order.isRemote(this.localMemberId)) {
+			LOGGER.info("The open order [" + order.getId() + "] is remote for the member [" + order.getProvidingMember()
+					+ "]");
+
+			instanceProvider = this.remoteInstanceProvider;
+		}
+		return instanceProvider;
+	}
+
 }
