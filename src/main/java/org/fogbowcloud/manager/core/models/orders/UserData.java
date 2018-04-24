77b502f83abfd38613fbcac2221a8fe0f3e0590e
Lucas Lima Vieira <lima.lucaslv@gmail.com>
2018-04-24 13:46:35 -0300

    Implement user data

--- src/main/java/org/fogbowcloud/manager/core/models/orders/UserData.java
+++ src/main/java/org/fogbowcloud/manager/core/models/orders/UserData.java
@@ -1,4 +1,20 @@
 package org.fogbowcloud.manager.core.models.orders;
 
+import org.apache.commons.codec.binary.Base64;
+
 public class UserData {
+
+    private String content;
+
+    public UserData(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return Base64.encodeBase64String(content.getBytes());
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
