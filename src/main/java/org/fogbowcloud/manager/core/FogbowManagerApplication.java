772264fa1ea93f0aefffb950c47171e0d5ccf258
Gustavo Yamaguchi <gustavoyama@gmail.com>
2018-04-25 11:42:51 -0300

    Removing duplicate configuration from dependency of database.

--- src/main/java/org/fogbowcloud/manager/core/FogbowManagerApplication.java
+++ src/main/java/org/fogbowcloud/manager/core/FogbowManagerApplication.java
@@ -1,14 +1,9 @@
 package org.fogbowcloud.manager.core;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class FogbowManagerApplication {
 
 	public static void main(String[] args) {
