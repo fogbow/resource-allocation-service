b491247c374caf74f9060b0223416cd97f9914eb
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-04-25 15:56:47 -0300

    Using TimeUnit class to DefaultConfiguration Constants

--- src/main/java/org/fogbowcloud/manager/core/constants/DefaultConfigurationConstants.java
+++ src/main/java/org/fogbowcloud/manager/core/constants/DefaultConfigurationConstants.java
@@ -1,7 +1,9 @@
 package org.fogbowcloud.manager.core.constants;
 
+import java.util.concurrent.TimeUnit;
+
 public class DefaultConfigurationConstants {
 	
 	// SCHEDULER PERIODS
-	public static final String OPEN_ORDERS_SLEEP_TIME = "1000"; // 1 second
+	public static final String OPEN_ORDERS_SLEEP_TIME = Long.toString(TimeUnit.SECONDS.toMillis(1)); // 1 second
 }
