856db479ac46fc1ec21e6c635cfe3e594340c8c5
Ordan Santos <ordan.santos@gmail.com>
2018-05-07 01:01:56 -0300

    removing commented lines

--- src/test/java/org/fogbowcloud/manager/core/services/AuthenticationServiceTest.java
+++ src/test/java/org/fogbowcloud/manager/core/services/AuthenticationServiceTest.java
@@ -1,6 +1,7 @@
 package org.fogbowcloud.manager.core.services;
 
 import org.apache.http.HttpStatus;
+import org.fogbowcloud.manager.core.exceptions.UnauthorizedException;
 import org.fogbowcloud.manager.core.models.token.Token;
 import org.fogbowcloud.manager.core.plugins.IdentityPlugin;
 import org.junit.Assert;
@@ -18,26 +19,26 @@ public class AuthenticationServiceTest {
 		this.identityPlugin = Mockito.mock(IdentityPlugin.class);
 		this.authenticationService = new AuthenticationService(identityPlugin);
 	}
-//
-//	@Test
-//	public void testAuthenticateService() {
-//		Token token = new Token();
-//		Mockito.doReturn(token).when(identityPlugin).getToken(Mockito.anyString());
-//		Assert.assertEquals(token, authenticationService.authenticate(Mockito.anyString()));
-//	}
-//
-//	@Test
-//	public void testInvalidAccessid() {
-//		Integer statusResponse = HttpStatus.SC_UNAUTHORIZED;
-//		Mockito.doThrow(new RuntimeException(statusResponse.toString())).when(identityPlugin)
-//				.getToken(Mockito.anyString());
-//
-//		try {
-//			authenticationService.authenticate(Mockito.anyString());
-//			Assert.fail();
-//		} catch (RuntimeException runtimeException) {
-//			Assert.assertEquals(statusResponse.toString(), runtimeException.getMessage());
-//		}
-//	}
+
+	@Test
+	public void testAuthenticateService() throws UnauthorizedException {
+		Token token = new Token();
+		Mockito.doReturn(token).when(identityPlugin).getToken(Mockito.anyString());
+		Assert.assertEquals(token, authenticationService.authenticate(Mockito.anyString()));
+	}
+
+	@Test
+	public void testInvalidAccessid() throws UnauthorizedException {
+		Integer statusResponse = HttpStatus.SC_UNAUTHORIZED;
+		Mockito.doThrow(new RuntimeException(statusResponse.toString())).when(identityPlugin)
+				.getToken(Mockito.anyString());
+
+		try {
+			authenticationService.authenticate(Mockito.anyString());
+			Assert.fail();
+		} catch (RuntimeException runtimeException) {
+			Assert.assertEquals(statusResponse.toString(), runtimeException.getMessage());
+		}
+	}
 
 }
