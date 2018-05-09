029be1b6261b7a34b5fbc29341e5d92cece0f64c
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-05-09 14:34:26 -0300

    Adding Reverse Tunnel configuration constants and refactor of manager configuration constants names to be more concise

--- src/main/java/org/fogbowcloud/manager/core/constants/ConfigurationConstants.java
+++ src/main/java/org/fogbowcloud/manager/core/constants/ConfigurationConstants.java
@@ -5,18 +5,21 @@ public class ConfigurationConstants {
 	public static final String XMPP_ID_KEY = "xmpp_jid";
 
 	// SCHEDULER PERIODS KEYS
-	public static final String OPEN_ORDERS_SLEEP_TIME_KEY = "open_orders_scheduler_period";
-	
-	public static final String SPAWNING_ORDERS_SLEEP_TIME_KEY = "spawning_orders_scheduler_period";
+	public static final String OPEN_ORDERS_SLEEP_TIME_KEY = "open_orders_sleep_time";
+	public static final String SPAWNING_ORDERS_SLEEP_TIME_KEY = "spawning_orders_sleep_time";
 
+	// USER SSH KEY 
 	public static final String SSH_COMMON_USER_KEY = "ssh_common_user";
 
-	public static final String SSH_PRIVATE_PATH_KEY = "ssh_private_key";
+	// MANAGER KEYS
+	public static final String MANAGER_SSH_PRIVATE_KEY_PATH = "manager_ssh_private_key_file_path";
+	public static final String MANAGER_SSH_PUBLIC_KEY_PATH = "manager_ssh_public_key_file_path";
 
-	public static final String TOKEN_HOST_PRIVATE_ADDRESS_KEY = "token_host_private_address";
-
-	public static final String TOKEN_HOST_HTTP_PORT_KEY = "token_host_http_port";
-
-	public static final String TOKEN_HOST_PUBLIC_ADDRESS_KEY = "token_host_public_address";
+	// REVERSE TUNNEL KEYS
+	public static final String REVERSE_TUNNEL_PRIVATE_ADDRESS_KEY = "reverse_tunnel_private_address";
+	public static final String REVERSE_TUNNEL_PUBLIC_ADDRESS_KEY = "reverse_tunnel_public_address";
+	public static final String REVERSE_TUNNEL_PORT_KEY = "reverse_tunnel_port";
+	public static final String REVERSE_TUNNEL_HTTP_PORT_KEY = "reverse_tunnel_http_port";
+	
 
 }
