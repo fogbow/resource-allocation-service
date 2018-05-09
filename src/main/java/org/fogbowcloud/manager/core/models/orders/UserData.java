0d0837bdb333f8a296fcb3f3fc19583e2fbc6991
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-05-09 14:35:16 -0300

    Refactor of UserData class

--- src/main/java/org/fogbowcloud/manager/core/models/orders/UserData.java
+++ src/main/java/org/fogbowcloud/manager/core/models/orders/UserData.java
@@ -1,29 +1,22 @@
 package org.fogbowcloud.manager.core.models.orders;
 
-import org.apache.commons.codec.binary.Base64;
-
 public class UserData {
 
-    private Long id;
-    private String content;
+	private String extraUserDataFile;
+    
+    private String extraUserDataFileType;
 
-    public UserData(String content) {
-        this.content = content;
+    public UserData(String extraUserDataFile, String extraUserDataFileType) {
+        this.extraUserDataFile = extraUserDataFile;
+        this.extraUserDataFileType = extraUserDataFileType;
     }
 
-    public Long getId() {
-        return id;
-    }
+    public String getExtraUserDataFile() {
+		return extraUserDataFile;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getContent() {
-        return Base64.encodeBase64String(content.getBytes());
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
+    public String getExtraUserDataFileType() {
+		return extraUserDataFileType;
+	}
+    
 }
