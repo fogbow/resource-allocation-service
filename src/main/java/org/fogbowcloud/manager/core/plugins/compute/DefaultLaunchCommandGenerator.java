98d6d216fd6e852b9944cb3a450441f664c05d2d
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2018-05-09 23:55:28 -0300

    changing constant attributes visibility and fixing initial implementation bugs

--- src/main/java/org/fogbowcloud/manager/core/plugins/compute/DefaultLaunchCommandGenerator.java
+++ src/main/java/org/fogbowcloud/manager/core/plugins/compute/DefaultLaunchCommandGenerator.java
@@ -2,7 +2,6 @@ package org.fogbowcloud.manager.core.plugins.compute;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.StringReader;
@@ -11,6 +10,7 @@ import java.util.Properties;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
 import org.fogbowcloud.manager.core.constants.ConfigurationConstants;
 import org.fogbowcloud.manager.core.constants.DefaultConfigurationConstants;
 import org.fogbowcloud.manager.core.exceptions.PropertyNotSpecifiedException;
@@ -21,20 +21,21 @@ import org.fogbowcloud.manager.core.plugins.compute.util.CloudInitUserDataBuilde
 
 public class DefaultLaunchCommandGenerator implements LaunchCommandGenerator {
 
-	private final String TOKEN_ID = "#TOKEN_ID#";
-	private final String TOKEN_HOST = "#TOKEN_HOST#";
-	private final String TOKEN_HOST_HTTP_PORT = "#TOKEN_HOST_HTTP_PORT#";
-	private final String TOKEN_HOST_SSH_PORT = "#TOKEN_HOST_SSH_PORT#";
-	private final String TOKEN_SSH_USER = "#TOKEN_SSH_USER#";
-	private final String TOKEN_USER_SSH_PUBLIC_KEY = "#TOKEN_USER_SSH_PUBLIC_KEY#";
-	private final String TOKEN_MANAGER_SSH_PUBLIC_KEY = "#TOKEN_MANAGER_SSH_PUBLIC_KEY#";
+	protected static final String TOKEN_ID = "#TOKEN_ID#";
+	protected static final String TOKEN_HOST = "#TOKEN_HOST#";
+	protected static final String TOKEN_HOST_HTTP_PORT = "#TOKEN_HOST_HTTP_PORT#";
+	protected static final String TOKEN_HOST_SSH_PORT = "#TOKEN_HOST_SSH_PORT#";
+	protected static final String TOKEN_SSH_USER = "#TOKEN_SSH_USER#";
+	protected static final String TOKEN_USER_SSH_PUBLIC_KEY = "#TOKEN_USER_SSH_PUBLIC_KEY#";
+	protected static final String TOKEN_MANAGER_SSH_PUBLIC_KEY = "#TOKEN_MANAGER_SSH_PUBLIC_KEY#";
 
-	private final String DEFAULT_SSH_HOST_PORT = "22";
+	protected static final String DEFAULT_SSH_HOST_PORT = "22";
 
 	public static final String USER_DATA_LINE_BREAKER = "[[\\n]]";
 
 	private final String sshReverseTunnelScriptPath = "bin/fogbow-create-reverse-tunnel";
 	private final FileReader sshReverseTunnelScript;
+
 	private final String cloudConfigFilePath = "bin/fogbow-cloud-config.cfg";
 	private final FileReader cloudConfigFile;
 
@@ -45,23 +46,33 @@ public class DefaultLaunchCommandGenerator implements LaunchCommandGenerator {
 	private final String reverseTunnelSshPort;
 	private final String reverseTunnelHttpPort;
 
-	public DefaultLaunchCommandGenerator(Properties properties)
-			throws PropertyNotSpecifiedException, FileNotFoundException, IOException {
+	private static final Logger LOGGER = Logger.getLogger(DefaultLaunchCommandGenerator.class);
 
-		this.sshReverseTunnelScript = new FileReader(this.sshReverseTunnelScriptPath);
+	public DefaultLaunchCommandGenerator(Properties properties)
+			throws PropertyNotSpecifiedException, IOException {
 		this.cloudConfigFile = new FileReader(new File(this.cloudConfigFilePath));
+
+		this.sshCommonUser = properties.getProperty(ConfigurationConstants.SSH_COMMON_USER_KEY,
+				DefaultConfigurationConstants.SSH_COMMON_USER);
 
 		String managerSshPublicKeyFilePath = properties
 				.getProperty(ConfigurationConstants.MANAGER_SSH_PUBLIC_KEY_PATH);
+		if (managerSshPublicKeyFilePath == null) {
+			throw new PropertyNotSpecifiedException(
+					ConfigurationConstants.MANAGER_SSH_PUBLIC_KEY_PATH);
+		}
 		this.managerSshPublicKey = IOUtils
 				.toString(new FileInputStream(new File(managerSshPublicKeyFilePath)));
 
-		this.sshCommonUser = properties.getProperty(ConfigurationConstants.SSH_COMMON_USER_KEY,
-				DefaultConfigurationConstants.SSH_COMMON_USER_KEY);
+		this.sshReverseTunnelScript = new FileReader(this.sshReverseTunnelScriptPath);
+
 		this.reverseTunnelPrivateIP = properties
 				.getProperty(ConfigurationConstants.REVERSE_TUNNEL_PRIVATE_ADDRESS_KEY);
+		
 		this.reverseTunnelSshPort = properties.getProperty(
-				ConfigurationConstants.REVERSE_TUNNEL_PORT_KEY, this.DEFAULT_SSH_HOST_PORT);
+				ConfigurationConstants.REVERSE_TUNNEL_PORT_KEY,
+				DefaultLaunchCommandGenerator.DEFAULT_SSH_HOST_PORT);
+		
 		this.reverseTunnelHttpPort = properties
 				.getProperty(ConfigurationConstants.REVERSE_TUNNEL_HTTP_PORT_KEY);
 
@@ -69,11 +80,11 @@ public class DefaultLaunchCommandGenerator implements LaunchCommandGenerator {
 	}
 
 	private void checkNecessaryAttributes() throws PropertyNotSpecifiedException {
-		if (this.reverseTunnelPrivateIP == null || this.reverseTunnelSshPort.trim().isEmpty()) {
+		if (this.reverseTunnelPrivateIP == null || this.reverseTunnelPrivateIP.trim().isEmpty()) {
 			throw new PropertyNotSpecifiedException(
 					ConfigurationConstants.REVERSE_TUNNEL_PRIVATE_ADDRESS_KEY);
 		}
-		if (this.managerSshPublicKey == null || this.managerSshPublicKey.trim().isEmpty()) {
+		if (this.managerSshPublicKey.trim().isEmpty()) {
 			throw new PropertyNotSpecifiedException(
 					ConfigurationConstants.MANAGER_SSH_PUBLIC_KEY_PATH);
 		}
@@ -104,21 +115,21 @@ public class DefaultLaunchCommandGenerator implements LaunchCommandGenerator {
 		String mimeString = cloudInitUserDataBuilder.buildUserData();
 
 		mimeString = applyTokensReplacements(order, mimeString);
-		
+
 		String base64String = new String(
 				Base64.encodeBase64(mimeString.getBytes(StandardCharsets.UTF_8), false, false),
 				StandardCharsets.UTF_8);
 		return base64String;
 	}
 
-	private void addExtraUserData(CloudInitUserDataBuilder cloudInitUserDataBuilder,
+	protected void addExtraUserData(CloudInitUserDataBuilder cloudInitUserDataBuilder,
 			String extraUserData, String extraUserDataContentType) {
-		
-		if (extraUserData != null || extraUserDataContentType != null) {
+
+		if (extraUserData != null && extraUserDataContentType != null) {
 			String lineSeparator = "\n";
-			String normalizedExtraUserData = extraUserData.replace(USER_DATA_LINE_BREAKER,
-					lineSeparator);
-			
+			String normalizedExtraUserData = extraUserData
+					.replace(DefaultLaunchCommandGenerator.USER_DATA_LINE_BREAKER, lineSeparator);
+
 			for (FileType fileType : CloudInitUserDataBuilder.FileType.values()) {
 				String mimeType = fileType.getMimeType();
 				if (mimeType.equals(extraUserDataContentType)) {
@@ -127,25 +138,31 @@ public class DefaultLaunchCommandGenerator implements LaunchCommandGenerator {
 					break;
 				}
 			}
+		} else {
+			LOGGER.info(
+					"Was not possible add the extra user data file, the extra user data file or file type are null");
 		}
 	}
 
-	private String applyTokensReplacements(ComputeOrder order, String mimeString) {
+	protected String applyTokensReplacements(ComputeOrder order, String mimeString) {
 		String orderId = order.getId();
 
-		mimeString.replace(this.TOKEN_ID, orderId);
-		mimeString.replace(this.TOKEN_HOST, this.reverseTunnelPrivateIP);
-		mimeString.replace(this.TOKEN_HOST_SSH_PORT, this.reverseTunnelSshPort);
-		mimeString.replace(this.TOKEN_HOST_HTTP_PORT, this.reverseTunnelHttpPort);
+		mimeString = mimeString.replace(DefaultLaunchCommandGenerator.TOKEN_ID, orderId);
+		mimeString = mimeString.replace(DefaultLaunchCommandGenerator.TOKEN_HOST, this.reverseTunnelPrivateIP);
+		mimeString = mimeString.replace(DefaultLaunchCommandGenerator.TOKEN_HOST_SSH_PORT,
+				this.reverseTunnelSshPort);
+		mimeString = mimeString.replace(DefaultLaunchCommandGenerator.TOKEN_HOST_HTTP_PORT,
+				this.reverseTunnelHttpPort);
 
 		String userPublicKey = order.getPublicKey();
 		if (userPublicKey == null) {
 			userPublicKey = "";
 		}
 
-		mimeString.replace(this.TOKEN_MANAGER_SSH_PUBLIC_KEY, this.managerSshPublicKey);
-		mimeString.replace(this.TOKEN_USER_SSH_PUBLIC_KEY, userPublicKey);
-		mimeString.replace(this.TOKEN_SSH_USER, this.sshCommonUser);
+		mimeString = mimeString.replace(DefaultLaunchCommandGenerator.TOKEN_MANAGER_SSH_PUBLIC_KEY,
+				this.managerSshPublicKey);
+		mimeString = mimeString.replace(DefaultLaunchCommandGenerator.TOKEN_USER_SSH_PUBLIC_KEY, userPublicKey);
+		mimeString= mimeString.replace(DefaultLaunchCommandGenerator.TOKEN_SSH_USER, this.sshCommonUser);
 
 		return mimeString;
 	}
