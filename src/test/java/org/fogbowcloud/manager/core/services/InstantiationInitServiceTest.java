1fc7d905decf98621df763fb992dde00a919500e
Italo Batista <italohmb@gmail.com>
2018-05-10 14:51:46 -0300

    Add unit case tests for InstatiationInitService

---
+++ src/test/java/org/fogbowcloud/manager/core/services/InstantiationInitServiceTest.java
@@ -0,0 +1,57 @@
+package org.fogbowcloud.manager.core.services;
+
+import org.fogbowcloud.manager.core.FogbowManagerApplication;
+import org.fogbowcloud.manager.core.constants.ConfigurationConstants;
+import org.fogbowcloud.manager.core.plugins.IdentityPlugin;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+@RunWith(SpringRunner.class)
+@ComponentScan
+@ContextConfiguration(classes = FogbowManagerApplication.class)
+@TestPropertySource("classpath:test.properties")
+@SpringBootTest
+public class InstantiationInitServiceTest {
+
+    @Autowired
+    private InstantiationInitService service;
+
+    private static final String TEST_CONF_FILE_FULL_PATH = "src/main/resources/test.properties";
+
+    @Before
+    public void setUp() throws Exception {
+        Properties mProperties = new Properties();
+        FileInputStream mInput = new FileInputStream(TEST_CONF_FILE_FULL_PATH);
+        mProperties.load(mInput);
+        this.service.getProperties().putAll(mProperties);
+    }
+
+    @Test
+    public void testSetUpProperties() {
+        String expected_xmpp_jid_value = "my-manager.internal.mydomain";
+
+        Assert.assertEquals(
+                this.service.getProperties().getProperty(ConfigurationConstants.XMPP_ID_KEY),
+                expected_xmpp_jid_value);
+    }
+
+    @Test
+    public void testCreateLocalIdentityPluginInstance() {
+        String expected_local_identity_class_value = "org.fogbowcloud.manager.core.plugins.identity.ldap.LdapIdentityPlugin";
+
+        IdentityPlugin plugin = this.service.getLocalIdentityPlugin();
+        Assert.assertEquals(plugin.getClass().getName(), expected_local_identity_class_value);
+    }
+
+}
