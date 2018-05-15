3dc9de437b63c6475f9f7e95b4b20ec1a52c0590
wassoares <wlfsoares@gmail.com>
2018-05-15 11:15:11 -0300

    Implementation of the 'deleteOrder' and 'isSameUser' methods in the 'OrdersService' class

--- src/main/java/org/fogbowcloud/manager/core/services/OrdersService.java
+++ src/main/java/org/fogbowcloud/manager/core/services/OrdersService.java
@@ -5,12 +5,15 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.fogbowcloud.manager.core.datastructures.OrderStateTransitioner;
 import org.fogbowcloud.manager.core.datastructures.SharedOrderHolders;
+import org.fogbowcloud.manager.core.exceptions.OrderStateTransitionException;
 import org.fogbowcloud.manager.core.exceptions.OrdersServiceException;
 import org.fogbowcloud.manager.core.models.orders.Order;
 import org.fogbowcloud.manager.core.models.orders.OrderState;
 import org.fogbowcloud.manager.core.models.orders.OrderType;
 import org.fogbowcloud.manager.core.models.token.Token;
+import org.fogbowcloud.manager.core.models.token.Token.User;
 import org.fogbowcloud.manager.core.plugins.identity.exceptions.UnauthorizedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -77,5 +80,31 @@ public class OrdersService {
             ordersHolder.getOrdersList(OrderState.OPEN).addItem(order);
         }
     }
+
+	public void deleteOrder(Order order, Token token) {
+		if (isSameUser(order, token)) {
+			synchronized (order) {
+				if (!order.getOrderState().equals(OrderState.CLOSED)) {
+					try {
+						OrderStateTransitioner.transition(order, OrderState.CLOSED);
+					} catch (OrderStateTransitionException e) {
+						LOGGER.error("Error to try change status" + order.getOrderState() + " to closed for order [" + order.getId() + "]", e);
+					}
+				} else {
+					LOGGER.info("Order [" + order.getId() + "] is not available to change state");
+				}
+			}
+		}
+	}
+
+	private boolean isSameUser(Order order, Token token) {
+		Token localToken = order.getLocalToken();
+		User orderLocalTokenUser = localToken.getUser();
+		User tokenUserAuthenticated = token.getUser();
+		if (orderLocalTokenUser.equals(tokenUserAuthenticated)) {
+			return true;
+		}
+		return false;
+	}
 
 }
