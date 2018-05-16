dfdaba0999fdd4fd2f00b1002e03d215da8c9d73
chicog <chicog@taioba1>
2018-05-16 09:45:58 -0300

    Adding authorization functionality. Without tests.

--- src/main/java/org/fogbowcloud/manager/core/services/AuthenticationService.java
+++ src/main/java/org/fogbowcloud/manager/core/services/AuthenticationService.java
@@ -1,19 +1,51 @@
 package org.fogbowcloud.manager.core.services;
 
+import org.fogbowcloud.manager.core.exceptions.UnauthenticatedException;
 import org.fogbowcloud.manager.core.models.token.Token;
+import org.fogbowcloud.manager.core.plugins.AuthorizationPlugin;
 import org.fogbowcloud.manager.core.plugins.IdentityPlugin;
 import org.fogbowcloud.manager.core.plugins.identity.exceptions.UnauthorizedException;
 
+// TODO change the name. Fubica's suggestion is : "AAAController"
 public class AuthenticationService {
 	
 	private IdentityPlugin federationIdentityPlugin;
+	private AuthorizationPlugin authorizationPlugin;
 	
-	AuthenticationService (IdentityPlugin federationIdentityPlugin){
+	AuthenticationService (IdentityPlugin federationIdentityPlugin, AuthorizationPlugin authorizationPlugin){
 		this.federationIdentityPlugin = federationIdentityPlugin;
+		this.authorizationPlugin = authorizationPlugin;
 	}
 	
-	public Token authenticate(String accessId) throws UnauthorizedException {
-		return federationIdentityPlugin.getToken(accessId);
+	// TODO to use UnautheticatedException
+	protected boolean authenticate(String accessId) throws UnauthorizedException {
+		return this.federationIdentityPlugin.isValid(accessId);
+	}
+
+	// TODO to use UnautheticatedException
+	public Token getFederationToken(String accessId) throws UnauthorizedException {
+		return this.federationIdentityPlugin.getToken(accessId);
+	}
+
+	// TODO to use UnautheticatedException
+	public Token getLocalToken(String accessId) throws UnauthorizedException {
+		return this.federationIdentityPlugin.getToken(accessId);
+	}
+	
+	protected boolean authorize(Token federationToken) throws UnauthorizedException {
+		return this.authorizationPlugin.isAuthorized(federationToken);
+	}
+	
+	public void AutenticateAndAuthorize(String accessId) throws UnauthorizedException, UnauthenticatedException {
+		boolean isAuthenticated = authenticate(accessId);
+		if (!isAuthenticated) {
+			throw new UnauthenticatedException("User not authenticated.");
+		}
+		Token federationToken = getFederationToken(accessId);
+		boolean isAuthorized = authorize(federationToken);
+		if (!isAuthorized) {
+			throw new UnauthorizedException("User not authorized.");
+		}
 	}
 	
 }
--- src/test/java/org/fogbowcloud/manager/core/services/AuthenticationServiceTest.java
+++ src/test/java/org/fogbowcloud/manager/core/services/AuthenticationServiceTest.java
@@ -1,47 +1,38 @@
 package org.fogbowcloud.manager.core.services;
 
-import java.util.Date;
-import java.util.HashMap;
-
-import org.apache.http.HttpStatus;
-import org.fogbowcloud.manager.core.models.token.Token;
+import org.fogbowcloud.manager.core.plugins.AuthorizationPlugin;
 import org.fogbowcloud.manager.core.plugins.IdentityPlugin;
 import org.fogbowcloud.manager.core.plugins.identity.exceptions.UnauthorizedException;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+//FIXME implement tests. leave for later because of the project decision
+//TODO change the name.
 public class AuthenticationServiceTest {
 
 	AuthenticationService authenticationService;
 	IdentityPlugin identityPlugin;
+	AuthorizationPlugin authorizationPlugin;
 
 	@Before
 	public void setUp() {
 		this.identityPlugin = Mockito.mock(IdentityPlugin.class);
-		this.authenticationService = new AuthenticationService(identityPlugin);
+		this.authorizationPlugin = Mockito.mock(AuthorizationPlugin.class);
+		this.authenticationService = new AuthenticationService(this.identityPlugin, this.authorizationPlugin);
 	}
 
 	@Test
-	public void testAuthenticateService() throws UnauthorizedException {
-		Token token = new Token("accessId", Mockito.mock(Token.User.class), new Date(), new HashMap<String, String>());
-		Mockito.doReturn(token).when(identityPlugin).getToken(Mockito.anyString());
-		Assert.assertEquals(token, authenticationService.authenticate(Mockito.anyString()));
+	public void testAuthenticate() throws UnauthorizedException {
+		boolean isAuthenticated = true;
+		Mockito.doReturn(isAuthenticated).when(this.identityPlugin).isValid(Mockito.anyString());
+		this.authenticationService.authenticate(Mockito.anyString());
 	}
 
-	@Test
-	public void testInvalidAccessid() throws UnauthorizedException {
-		Integer statusResponse = HttpStatus.SC_UNAUTHORIZED;
-		Mockito.doThrow(new RuntimeException(statusResponse.toString())).when(identityPlugin)
-				.getToken(Mockito.anyString());
-
-		try {
-			authenticationService.authenticate(Mockito.anyString());
-			Assert.fail();
-		} catch (RuntimeException runtimeException) {
-			Assert.assertEquals(statusResponse.toString(), runtimeException.getMessage());
-		}
+	@Test(expected=UnauthorizedException.class)
+	public void testAuthenticateNot() throws UnauthorizedException {
+		Mockito.doThrow(UnauthorizedException.class).when(this.identityPlugin).isValid(Mockito.anyString());
+		this.authenticationService.authenticate(Mockito.anyString());
 	}
-
+	
 }
