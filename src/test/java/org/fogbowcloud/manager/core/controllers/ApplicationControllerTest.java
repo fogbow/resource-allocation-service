0a11e7b0b3f2c53439f407852d69350a64977016
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2018-05-17 01:04:17 -0300

    Updating ApplicationController tests

--- src/test/java/org/fogbowcloud/manager/core/controllers/ApplicationControllerTest.java
+++ src/test/java/org/fogbowcloud/manager/core/controllers/ApplicationControllerTest.java
@@ -2,109 +2,96 @@ package org.fogbowcloud.manager.core.controllers;
 
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
 
 import org.fogbowcloud.manager.core.BaseUnitTests;
-import org.fogbowcloud.manager.core.datastructures.SharedOrderHolders;
-import org.fogbowcloud.manager.core.models.linkedList.ChainedList;
+import org.fogbowcloud.manager.core.exceptions.OrderManagementException;
 import org.fogbowcloud.manager.core.models.orders.ComputeOrder;
 import org.fogbowcloud.manager.core.models.orders.Order;
-import org.fogbowcloud.manager.core.models.orders.OrderState;
 import org.fogbowcloud.manager.core.models.orders.OrderType;
+import org.fogbowcloud.manager.core.models.orders.UserData;
 import org.fogbowcloud.manager.core.models.token.Token;
-import org.fogbowcloud.manager.core.plugins.PluginHelper;
+import org.fogbowcloud.manager.core.models.token.Token.User;
 import org.fogbowcloud.manager.core.plugins.identity.exceptions.UnauthorizedException;
-import org.fogbowcloud.manager.core.plugins.identity.ldap.LdapIdentityPlugin;
 import org.fogbowcloud.manager.core.services.AuthenticationService;
-import org.junit.After;
-import org.junit.Assert;
+import org.fogbowcloud.manager.core.services.OrderService;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 public class ApplicationControllerTest extends BaseUnitTests {
 
-	private static final Object IDENTITY_URL_KEY = "identity_url";
-	private static final Object KEYSTONE_URL = "http://localhost:" + PluginHelper.PORT_ENDPOINT;
-	
-	private Properties properties;
-	private LdapIdentityPlugin ldapIdentityPlugin;
 	private ApplicationController applicationController;
+	private AuthenticationService authenticationService;
+	private OrderService orderService;
 
 	@Before
 	public void setUp() throws UnauthorizedException {
-		this.properties = new Properties();
-        this.properties.put(IDENTITY_URL_KEY, KEYSTONE_URL);
-        this.setLdapIdentityPlugin();
-        AuthenticationService authenticationService = new AuthenticationService(this.ldapIdentityPlugin);
-        this.applicationController = Mockito.spy(ApplicationController.getInstance());
-        this.applicationController.setAuthenticationController(authenticationService);
+		this.authenticationService = Mockito.mock(AuthenticationService.class);
+		this.orderService = Mockito.mock(OrderService.class);
+		this.applicationController = ApplicationController.getInstance();
+		this.applicationController.setAuthenticationController(this.authenticationService);
+		this.applicationController.setOrderService(this.orderService);
 	}
 
-	@After
+	@Override
 	public void tearDown() {
 		super.tearDown();
 	}
 
 	@Test
-	public void testDeleteComputeOrderSuccessful() throws UnauthorizedException {
-		String orderId = this.getComputeOrderCreationId();
-		String accessId = "fake-access-id";
-		
-		this.applicationController.deleteOrder(orderId, accessId, OrderType.COMPUTE);		
+	public void testDeleteComputeOrder() throws UnauthorizedException, OrderManagementException {
+		Mockito.doNothing().when(this.orderService).deleteOrder(Mockito.any(Order.class));
 
-		SharedOrderHolders sharedOrderHolders = SharedOrderHolders.getInstance();
-		ChainedList closedOrdersList = sharedOrderHolders.getClosedOrdersList();
-		Order test = closedOrdersList.getNext();
-		
-		Assert.assertNotNull(test);
-		Assert.assertEquals(OrderState.CLOSED, test.getOrderState());
-	}
-	
-	@Test
-	public void testThrowUnauthorizedException() throws UnauthorizedException {
-		String orderId = "fake-order-id-1";
-		String accessId = "fake-access-id!#!fake-user-id!#!fake-user-name";
-		Mockito.doThrow(new UnauthorizedException()).when(this.applicationController).deleteOrder(orderId, accessId, OrderType.COMPUTE);
+		Order order = createComputeOrder();
+		Mockito.doReturn(order).when(this.orderService).getOrderByIdAndType(Mockito.any(User.class),
+				Mockito.anyString(), Mockito.any(OrderType.class));
+
+		Mockito.doReturn(order.getFederationToken()).when(this.authenticationService)
+				.authenticate(Mockito.anyString());
+
+		this.applicationController.deleteOrder(order.getId(),
+				order.getFederationToken().getAccessId(), OrderType.COMPUTE);
 	}
 
-	private void setLdapIdentityPlugin() throws UnauthorizedException {
-		Token token = createToken();
-		this.ldapIdentityPlugin = Mockito.spy(new LdapIdentityPlugin(this.properties));
-		Mockito.doReturn(token).when(ldapIdentityPlugin).getToken(Mockito.anyString());
+	@Test(expected = OrderManagementException.class)
+	public void testDeleteComputeOrderNotFound()
+			throws UnauthorizedException, OrderManagementException {
+		Mockito.doThrow(new OrderManagementException("Not found")).when(this.orderService)
+				.deleteOrder(Mockito.any(Order.class));
+
+		Order order = createComputeOrder();
+		Mockito.doReturn(order).when(this.orderService).getOrderByIdAndType(Mockito.any(User.class),
+				Mockito.anyString(), Mockito.any(OrderType.class));
+
+		Mockito.doReturn(order.getFederationToken()).when(this.authenticationService)
+				.authenticate(Mockito.anyString());
+
+		this.applicationController.deleteOrder(order.getId(),
+				order.getFederationToken().getAccessId(), OrderType.COMPUTE);
 	}
 
-	private Token createToken() {
-		String accessId = "fake-access-id";
-		String tokenUserId = "fake-user-id";
-		String tokenUserName = "fake-user-name";
-		Token.User tokenUser = new Token.User(tokenUserId, tokenUserName);
-		Date expirationTime = new Date();
-		Map<String, String> attributesMap = new HashMap<>();
-		return new Token(accessId, tokenUser, expirationTime, attributesMap);
+	@Test(expected = UnauthorizedException.class)
+	public void testDeleteComputeUnauthorized()
+			throws UnauthorizedException, OrderManagementException {
+		Order order = createComputeOrder();
+
+		Mockito.doThrow(new UnauthorizedException()).when(this.authenticationService)
+				.authenticate(Mockito.anyString());
+
+		this.applicationController.deleteOrder(order.getId(),
+				order.getFederationToken().getAccessId(), OrderType.COMPUTE);
 	}
-	
-	private String getComputeOrderCreationId() {
-		String orderId = null;
-		
-		Token token = this.createToken();
-		
-		ComputeOrder computeOrder = Mockito.spy(new ComputeOrder());
-		computeOrder.setOrderState(OrderState.OPEN);
-		computeOrder.setLocalToken(token);
-		computeOrder.setFederationToken(token);
-		
-		orderId = computeOrder.getId();
-		
-		SharedOrderHolders sharedOrderHolders = SharedOrderHolders.getInstance();
-		Map<String, Order> activeOrdersMap = sharedOrderHolders.getActiveOrdersMap();
-		activeOrdersMap.put(orderId, computeOrder);
-		
-		ChainedList openOrdersList = sharedOrderHolders.getOpenOrdersList();
-		openOrdersList.addItem(computeOrder);
-		
-		return orderId;
+
+	private Order createComputeOrder() {
+		User user = new User("fake-user-id", "fake-user-name");
+		Token localToken = new Token("fake-accessId", user, new Date(),
+				new HashMap<String, String>());
+		Token federationToken = new Token("fake-accessId", user, new Date(),
+				new HashMap<String, String>());
+
+		Order order = new ComputeOrder(localToken, federationToken, "fake-member-id",
+				"fake-member-id", 2, 2, 30, "fake-image-name", new UserData(), "fake-public-key");
+		return order;
 	}
 
 }
