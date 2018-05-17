ec65d0126bf1312a695b3c93e5f72aa91c5facf0
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2018-05-16 22:56:51 -0300

    Fixing indentation

--- src/test/java/org/fogbowcloud/manager/core/services/ComputeOrdersServiceTest.java
+++ src/test/java/org/fogbowcloud/manager/core/services/ComputeOrdersServiceTest.java
@@ -19,9 +19,9 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 public class ComputeOrdersServiceTest extends BaseUnitTests {
-	
+
 	private ComputeOrdersService computeOrdersService;
-	private Map<String, Order> activeOrdersMap;	
+	private Map<String, Order> activeOrdersMap;
 	private ChainedList openOrdersList;
 	private ChainedList pendingOrdersList;
 	private ChainedList spawningOrdersList;
@@ -32,7 +32,7 @@ public class ComputeOrdersServiceTest extends BaseUnitTests {
 	@Before
 	public void setUp() {
 		SharedOrderHolders sharedOrderHolders = SharedOrderHolders.getInstance();
-		this.activeOrdersMap = sharedOrderHolders.getActiveOrdersMap();		
+		this.activeOrdersMap = sharedOrderHolders.getActiveOrdersMap();
 		this.openOrdersList = sharedOrderHolders.getOpenOrdersList();
 		this.pendingOrdersList = sharedOrderHolders.getPendingOrdersList();
 		this.spawningOrdersList = sharedOrderHolders.getSpawningOrdersList();
@@ -41,153 +41,154 @@ public class ComputeOrdersServiceTest extends BaseUnitTests {
 		this.closedOrdersList = sharedOrderHolders.getClosedOrdersList();
 		this.computeOrdersService = Mockito.spy(new ComputeOrdersService());
 	}
-	
+
 	@After
 	public void tearDown() {
 		super.tearDown();
 	}
-	
-	@Test (expected = OrderStateTransitionException.class)
+
+	@Test(expected = OrderStateTransitionException.class)
 	public void testThrowOrderStateTransitionException() {
 		String orderId = getComputeOrderCreationId(OrderState.OPEN);
 		ComputeOrder computeOrder = (ComputeOrder) activeOrdersMap.get(orderId);
-		
+
 		Token token = this.createToken();
-		
-		Mockito.doThrow(OrderStateTransitionException.class).when(this.computeOrdersService).deleteOrder(computeOrder, token);
-		
-		this.computeOrdersService.deleteOrder(computeOrder, token);	
+
+		Mockito.doThrow(OrderStateTransitionException.class).when(this.computeOrdersService)
+				.deleteOrder(computeOrder, token);
+
+		this.computeOrdersService.deleteOrder(computeOrder, token);
 	}
-	
+
 	@Test
 	public void testDeleteOrderStateClosed() {
 		String orderId = getComputeOrderCreationId(OrderState.CLOSED);
 		ComputeOrder computeOrder = (ComputeOrder) activeOrdersMap.get(orderId);
 
 		Token token = this.createToken();
-		
+
 		this.computeOrdersService.deleteOrder(computeOrder, token);
-		
+
 		Order test = this.closedOrdersList.getNext();
 		Assert.assertNotNull(test);
-        Assert.assertEquals(computeOrder, test);
-        Assert.assertEquals(OrderState.CLOSED, test.getOrderState());		
+		Assert.assertEquals(computeOrder, test);
+		Assert.assertEquals(OrderState.CLOSED, test.getOrderState());
 	}
-	
+
 	@Test
 	public void testDeleteOrderStateFailed() {
 		String orderId = getComputeOrderCreationId(OrderState.FAILED);
 		ComputeOrder computeOrder = (ComputeOrder) activeOrdersMap.get(orderId);
 
 		Token token = this.createToken();
-		
+
 		Assert.assertNull(this.closedOrdersList.getNext());
-		
+
 		this.computeOrdersService.deleteOrder(computeOrder, token);
-		
+
 		Order test = this.closedOrdersList.getNext();
 		Assert.assertNotNull(test);
-        Assert.assertEquals(computeOrder, test);
-        Assert.assertEquals(OrderState.CLOSED, test.getOrderState());		
+		Assert.assertEquals(computeOrder, test);
+		Assert.assertEquals(OrderState.CLOSED, test.getOrderState());
 	}
-	
+
 	@Test
 	public void testDeleteOrderStateFulfilled() {
 		String orderId = getComputeOrderCreationId(OrderState.FULFILLED);
 		ComputeOrder computeOrder = (ComputeOrder) activeOrdersMap.get(orderId);
 
 		Token token = this.createToken();
-		
+
 		Assert.assertNull(this.closedOrdersList.getNext());
-		
+
 		this.computeOrdersService.deleteOrder(computeOrder, token);
-		
+
 		Order test = this.closedOrdersList.getNext();
 		Assert.assertNotNull(test);
-        Assert.assertEquals(computeOrder, test);
-        Assert.assertEquals(OrderState.CLOSED, test.getOrderState());		
+		Assert.assertEquals(computeOrder, test);
+		Assert.assertEquals(OrderState.CLOSED, test.getOrderState());
 	}
-	
+
 	@Test
 	public void testDeleteOrderStateSpawning() {
 		String orderId = getComputeOrderCreationId(OrderState.SPAWNING);
 		ComputeOrder computeOrder = (ComputeOrder) activeOrdersMap.get(orderId);
 
 		Token token = this.createToken();
-		
+
 		Assert.assertNull(this.closedOrdersList.getNext());
-		
+
 		this.computeOrdersService.deleteOrder(computeOrder, token);
-		
+
 		Order test = this.closedOrdersList.getNext();
 		Assert.assertNotNull(test);
-        Assert.assertEquals(computeOrder, test);
-        Assert.assertEquals(OrderState.CLOSED, test.getOrderState());		
+		Assert.assertEquals(computeOrder, test);
+		Assert.assertEquals(OrderState.CLOSED, test.getOrderState());
 	}
-	
+
 	@Test
 	public void testDeleteOrderStatePending() {
 		String orderId = getComputeOrderCreationId(OrderState.PENDING);
 		ComputeOrder computeOrder = (ComputeOrder) activeOrdersMap.get(orderId);
 
 		Token token = this.createToken();
-		
+
 		Assert.assertNull(this.closedOrdersList.getNext());
-		
+
 		this.computeOrdersService.deleteOrder(computeOrder, token);
-		
+
 		Order test = this.closedOrdersList.getNext();
 		Assert.assertNotNull(test);
-        Assert.assertEquals(computeOrder, test);
-        Assert.assertEquals(OrderState.CLOSED, test.getOrderState());		
+		Assert.assertEquals(computeOrder, test);
+		Assert.assertEquals(OrderState.CLOSED, test.getOrderState());
 	}
-	
+
 	@Test
 	public void testDeleteOrderStateOpen() {
 		String orderId = getComputeOrderCreationId(OrderState.OPEN);
 		ComputeOrder computeOrder = (ComputeOrder) activeOrdersMap.get(orderId);
 
 		Token token = this.createToken();
-		
+
 		Assert.assertNull(this.closedOrdersList.getNext());
-		
+
 		this.computeOrdersService.deleteOrder(computeOrder, token);
-		
+
 		Order test = this.closedOrdersList.getNext();
 		Assert.assertNotNull(test);
-        Assert.assertEquals(computeOrder, test);
-        Assert.assertEquals(OrderState.CLOSED, test.getOrderState());		
+		Assert.assertEquals(computeOrder, test);
+		Assert.assertEquals(OrderState.CLOSED, test.getOrderState());
 	}
-	
+
 	private Token createToken() {
-        String accessId = "fake-access-id";
-        Token.User tokenUser = this.createTokenUser();
-        Date expirationTime = new Date();
-        Map<String, String> attributesMap = new HashMap<>();
-        return  new Token(accessId, tokenUser, expirationTime, attributesMap);
-    }
+		String accessId = "fake-access-id";
+		Token.User tokenUser = this.createTokenUser();
+		Date expirationTime = new Date();
+		Map<String, String> attributesMap = new HashMap<>();
+		return new Token(accessId, tokenUser, expirationTime, attributesMap);
+	}
 
 	private Token.User createTokenUser() {
 		String tokenUserId = "fake-user-id";
-        String tokenUserName = "fake-user-name";
-        Token.User tokenUser = new Token.User(tokenUserId, tokenUserName);
+		String tokenUserName = "fake-user-name";
+		Token.User tokenUser = new Token.User(tokenUserId, tokenUserName);
 		return tokenUser;
 	}
-	
+
 	private String getComputeOrderCreationId(OrderState orderState) {
 		String orderId = null;
-		
+
 		Token token = this.createToken();
-		
+
 		ComputeOrder computeOrder = Mockito.spy(new ComputeOrder());
 		computeOrder.setLocalToken(token);
 		computeOrder.setFederationToken(token);
 		computeOrder.setOrderState(orderState);
-		
+
 		orderId = computeOrder.getId();
-		
+
 		this.activeOrdersMap.put(orderId, computeOrder);
-		
+
 		switch (orderState) {
 		case OPEN:
 			this.openOrdersList.addItem(computeOrder);
@@ -207,7 +208,7 @@ public class ComputeOrdersServiceTest extends BaseUnitTests {
 		case CLOSED:
 			this.closedOrdersList.addItem(computeOrder);
 		}
-		
+
 		return orderId;
 	}
 }
