5d4865cc180404fc800b79c3c29b3e2eba947f5f
Gustavo <gustavo.monteiro@ccc.ufcg.edu.br>
2018-05-17 14:39:21 -0300

    Fix tests for get by id and get all.

--- src/test/java/org/fogbowcloud/manager/core/rest/TestExceptionHandlerController.java
+++ src/test/java/org/fogbowcloud/manager/core/rest/TestExceptionHandlerController.java
@@ -7,6 +7,7 @@ import org.fogbowcloud.manager.core.plugins.identity.exceptions.InvalidCredentia
 import org.fogbowcloud.manager.core.plugins.identity.exceptions.InvalidTokenException;
 import org.fogbowcloud.manager.core.plugins.identity.exceptions.TokenCreationException;
 import org.fogbowcloud.manager.core.rest.controllers.ComputeOrdersController;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -16,7 +17,6 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import net.minidev.json.JSONObject;
 import net.minidev.json.JSONValue;
 
 public class TestExceptionHandlerController {
@@ -39,10 +39,11 @@ public class TestExceptionHandlerController {
 		Mockito.when(computeOrdersController.getAllCompute(fakeAccessId)).thenThrow(new InvalidCredentialsException());
 		
 		MockHttpServletResponse response = mockMvc.perform(get("/compute/")
-				.accept(MediaType.APPLICATION_JSON))
+				.accept(MediaType.APPLICATION_JSON)
+				.header("accessId", fakeAccessId))
         		.andReturn().getResponse();
 		
-		JSONObject jsonObject = (JSONObject) JSONValue.parse(response.getContentAsString());
+		JSONObject jsonObject = new JSONObject(response.getContentAsString());
 
 		assertEquals(jsonObject.get("details"), "uri=/compute/");
 		assertEquals(jsonObject.get("message"), "Invalid Credentials");
@@ -56,10 +57,11 @@ public class TestExceptionHandlerController {
 		Mockito.when(computeOrdersController.getAllCompute(fakeAccessId)).thenThrow(new InvalidTokenException());
 		
 		MockHttpServletResponse response = mockMvc.perform(get("/compute/")
-				.accept(MediaType.APPLICATION_JSON))
+				.accept(MediaType.APPLICATION_JSON)
+				.header("accessId", fakeAccessId))
         		.andReturn().getResponse();
 		
-		JSONObject jsonObject = (JSONObject) JSONValue.parse(response.getContentAsString());
+		JSONObject jsonObject = new JSONObject(response.getContentAsString());
 
 		assertEquals(jsonObject.get("details"), "uri=/compute/");
 		assertEquals(jsonObject.get("message"), "Invalid token");
@@ -73,10 +75,11 @@ public class TestExceptionHandlerController {
 		Mockito.when(computeOrdersController.getAllCompute(fakeAccessId)).thenThrow(new TokenCreationException());
 		
 		MockHttpServletResponse response = mockMvc.perform(get("/compute/")
-				.accept(MediaType.APPLICATION_JSON))
+				.accept(MediaType.APPLICATION_JSON)
+				.header("accessId", fakeAccessId))
         		.andReturn().getResponse();
 		
-		JSONObject jsonObject = (JSONObject) JSONValue.parse(response.getContentAsString());
+		JSONObject jsonObject = new JSONObject(response.getContentAsString());
 
 		assertEquals(jsonObject.get("details"), "uri=/compute/");
 		assertEquals(jsonObject.get("message"), "Token Creation Exception");
@@ -90,10 +93,11 @@ public class TestExceptionHandlerController {
 		Mockito.when(computeOrdersController.getAllCompute(fakeAccessId)).thenThrow(new RuntimeException());
 		
 		MockHttpServletResponse response = mockMvc.perform(get("/compute/")
-				.accept(MediaType.APPLICATION_JSON))
+				.accept(MediaType.APPLICATION_JSON)
+				.header("accessId", fakeAccessId))
         		.andReturn().getResponse();
 		
-		JSONObject jsonObject = (JSONObject) JSONValue.parse(response.getContentAsString());
+		JSONObject jsonObject = new JSONObject(response.getContentAsString());
 
 		assertEquals(jsonObject.get("details"), "uri=/compute/");
 		assertEquals(jsonObject.get("statusCode"), HttpStatus.BAD_REQUEST.name());
