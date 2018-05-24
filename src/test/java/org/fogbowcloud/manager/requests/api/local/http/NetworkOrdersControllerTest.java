ef6309f37507f675af79c59494c90cd826fad570
wassoares <wlfsoares@gmail.com>
2018-05-24 13:51:56 -0300

    Added more tests methods in 'NetworkOrdersControllerTest' and 'VolumeOrdersControllerTest' classes

--- src/test/java/org/fogbowcloud/manager/requests/api/local/http/NetworkOrdersControllerTest.java
+++ src/test/java/org/fogbowcloud/manager/requests/api/local/http/NetworkOrdersControllerTest.java
@@ -5,16 +5,26 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.spy;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.fogbowcloud.manager.api.local.http.NetworkOrdersController;
 import org.fogbowcloud.manager.core.ApplicationFacade;
+import org.fogbowcloud.manager.core.SharedOrderHolders;
 import org.fogbowcloud.manager.core.exceptions.OrderManagementException;
-import org.fogbowcloud.manager.core.exceptions.UnauthenticatedException;
 import org.fogbowcloud.manager.core.manager.plugins.identity.exceptions.UnauthorizedException;
+import org.fogbowcloud.manager.core.models.linkedlist.ChainedList;
 import org.fogbowcloud.manager.core.models.orders.NetworkOrder;
+import org.fogbowcloud.manager.core.models.orders.Order;
+import org.fogbowcloud.manager.core.models.token.Token;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -27,7 +37,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @RunWith(PowerMockRunner.class)
@@ -55,30 +64,99 @@ public class NetworkOrdersControllerTest {
 
     @Test
     public void createdNetworkTest() throws Exception {
-        mockApplicationContrllerToCreateNetwork();
+        PowerMockito.mockStatic(ApplicationFacade.class);
+        given(ApplicationFacade.getInstance()).willReturn(this.facade);
+        doNothing().when(this.facade).createNetwork(any(NetworkOrder.class), anyString());
 
         HttpHeaders headers = getHttpHeaders();
 
-        RequestBuilder requestBuilder = createRequestBuilder(headers, CORRECT_BODY);
-
-        MvcResult result = this.mockMvc.perform(requestBuilder).andReturn();
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.post(NETWORK_END_POINT)
+                .headers(headers)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(CORRECT_BODY)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
 
         int expectedStatus = HttpStatus.CREATED.value();
 
         assertEquals(expectedStatus, result.getResponse().getStatus());
     }
+    
+    @Test
+    public void getAllNetworksTest() throws Exception {
+        NetworkOrder networkOrder = createNetworkOrder();
+        
+        String federationTokenValue = networkOrder.getFederationToken().getAccessId();
+        
+        Map<String, Order> activeOrdersMap = SharedOrderHolders.getInstance().getActiveOrdersMap();
+        List<NetworkOrder> networkOrders = new ArrayList<NetworkOrder>();
 
-    private RequestBuilder createRequestBuilder(HttpHeaders headers, String body) {
-        return MockMvcRequestBuilders.post(NETWORK_END_POINT).headers(headers)
-                .accept(MediaType.APPLICATION_JSON).content(body)
-                .contentType(MediaType.APPLICATION_JSON);
-    }
-
-    private void mockApplicationContrllerToCreateNetwork()
-            throws OrderManagementException, UnauthorizedException, UnauthenticatedException {
+        for (Order order : activeOrdersMap.values()) {
+            networkOrders.add((NetworkOrder) order);
+        }        
+        
         PowerMockito.mockStatic(ApplicationFacade.class);
         given(ApplicationFacade.getInstance()).willReturn(this.facade);
-        doNothing().when(this.facade).createNetwork(any(NetworkOrder.class), anyString());
+        doReturn(networkOrders).when(this.facade).getAllVolumes(federationTokenValue);
+        
+        HttpHeaders headers = getHttpHeaders();
+        
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.get(NETWORK_END_POINT)
+                .headers(headers)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+        
+        int expectedStatus = HttpStatus.OK.value();
+
+        assertEquals(expectedStatus, result.getResponse().getStatus());
+    }
+    
+    @Test
+    public void getNetworkTest() throws Exception {
+        NetworkOrder networkOrder = createNetworkOrder();
+        
+        String networkId = networkOrder.getId();
+        String federationTokenValue = networkOrder.getFederationToken().getAccessId();     
+        
+        PowerMockito.mockStatic(ApplicationFacade.class);
+        given(ApplicationFacade.getInstance()).willReturn(this.facade);
+        doReturn(networkOrder).when(this.facade).getNetwork(networkId, federationTokenValue);
+        
+        HttpHeaders headers = getHttpHeaders();
+        
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.get(NETWORK_END_POINT)
+                .param("networkId", networkId)
+                .headers(headers)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+        
+        int expectedStatus = HttpStatus.OK.value();
+
+        assertEquals(expectedStatus, result.getResponse().getStatus());
+    }
+    
+    @Test
+    public void deleteNetworkTest() throws Exception {
+        NetworkOrder networkOrder = createNetworkOrder();
+
+        String volumeId = networkOrder.getId();
+        String federationTokenValue = networkOrder.getFederationToken().getAccessId();
+
+        PowerMockito.mockStatic(ApplicationFacade.class);
+        given(ApplicationFacade.getInstance()).willReturn(this.facade);
+        doNothing().when(this.facade).deleteVolume(volumeId, federationTokenValue);
+
+        HttpHeaders headers = getHttpHeaders();
+
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.get(NETWORK_END_POINT)
+                .param("networkId", volumeId)
+                .headers(headers)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        int expectedStatus = HttpStatus.OK.value();
+
+        assertEquals(expectedStatus, result.getResponse().getStatus());
     }
 
     private HttpHeaders getHttpHeaders() {
@@ -88,4 +166,35 @@ public class NetworkOrdersControllerTest {
         return headers;
     }
 
+    private Token createToken() {
+        String accessId = "fake-access-id";
+        Token.User tokenUser = this.createTokenUser();
+        Date expirationTime = new Date();
+        Map<String, String> attributesMap = new HashMap<>();
+        return new Token(accessId, tokenUser, expirationTime, attributesMap);
+    }
+
+    private Token.User createTokenUser() {
+        String tokenUserId = "fake-user-id";
+        String tokenUserName = "fake-user-name";
+        Token.User tokenUser = new Token.User(tokenUserId, tokenUserName);
+        return tokenUser;
+    }
+
+    private NetworkOrder createNetworkOrder() {
+        Token token = this.createToken();
+        
+        NetworkOrder networkOrder = Mockito.spy(new NetworkOrder());
+        networkOrder.setFederationToken(token);
+
+        String orderId = networkOrder.getId();
+
+        Map<String, Order> activeOrdersMap = SharedOrderHolders.getInstance().getActiveOrdersMap();
+        activeOrdersMap.put(orderId, networkOrder);
+        
+        ChainedList openOrdersList = SharedOrderHolders.getInstance().getOpenOrdersList();
+        openOrdersList.addItem(networkOrder);
+        
+        return networkOrder;
+    }
 }
--- src/test/java/org/fogbowcloud/manager/requests/api/local/http/VolumeOrdersControllerTest.java
+++ src/test/java/org/fogbowcloud/manager/requests/api/local/http/VolumeOrdersControllerTest.java
@@ -5,16 +5,26 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.spy;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.fogbowcloud.manager.api.local.http.VolumeOrdersController;
 import org.fogbowcloud.manager.core.ApplicationFacade;
+import org.fogbowcloud.manager.core.SharedOrderHolders;
 import org.fogbowcloud.manager.core.exceptions.OrderManagementException;
-import org.fogbowcloud.manager.core.exceptions.UnauthenticatedException;
 import org.fogbowcloud.manager.core.manager.plugins.identity.exceptions.UnauthorizedException;
+import org.fogbowcloud.manager.core.models.linkedlist.ChainedList;
+import org.fogbowcloud.manager.core.models.orders.Order;
 import org.fogbowcloud.manager.core.models.orders.VolumeOrder;
+import org.fogbowcloud.manager.core.models.token.Token;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -27,7 +37,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @RunWith(PowerMockRunner.class)
@@ -55,30 +64,98 @@ public class VolumeOrdersControllerTest {
 
     @Test
     public void createdVolumeTest() throws Exception {
-        mockApplicationContrllerToCreateVolume();
+        PowerMockito.mockStatic(ApplicationFacade.class);
+        given(ApplicationFacade.getInstance()).willReturn(this.facade);
+        doNothing().when(this.facade).createVolume(any(VolumeOrder.class), anyString());
 
         HttpHeaders headers = getHttpHeaders();
-
-        RequestBuilder requestBuilder = createRequestBuilder(headers, CORRECT_BODY);
-
-        MvcResult result = this.mockMvc.perform(requestBuilder).andReturn();
+        
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.post(VOLUME_END_POINT)
+                .headers(headers)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(CORRECT_BODY)
+                .contentType(MediaType.APPLICATION_JSON)).andReturn();
 
         int expectedStatus = HttpStatus.CREATED.value();
 
         assertEquals(expectedStatus, result.getResponse().getStatus());
     }
+    
+    @Test
+    public void getAllVolumesTest() throws Exception {
+        VolumeOrder volumeOrder = createVolumeOrder();
+        
+        String federationTokenValue = volumeOrder.getFederationToken().getAccessId();
+        
+        Map<String, Order> activeOrdersMap = SharedOrderHolders.getInstance().getActiveOrdersMap();
+        List<VolumeOrder> volumeOrders = new ArrayList<VolumeOrder>();
 
-    private RequestBuilder createRequestBuilder(HttpHeaders headers, String body) {
-        return MockMvcRequestBuilders.post(VOLUME_END_POINT).headers(headers)
-                .accept(MediaType.APPLICATION_JSON).content(body)
-                .contentType(MediaType.APPLICATION_JSON);
-    }
-
-    private void mockApplicationContrllerToCreateVolume()
-            throws OrderManagementException, UnauthorizedException, UnauthenticatedException {
+        for (Order order : activeOrdersMap.values()) {
+            volumeOrders.add((VolumeOrder) order);
+        }        
+        
         PowerMockito.mockStatic(ApplicationFacade.class);
         given(ApplicationFacade.getInstance()).willReturn(this.facade);
-        doNothing().when(this.facade).createVolume(any(VolumeOrder.class), anyString());
+        doReturn(volumeOrders).when(this.facade).getAllVolumes(federationTokenValue);
+        
+        HttpHeaders headers = getHttpHeaders();
+        
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.get(VOLUME_END_POINT)
+                .headers(headers)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+        
+        int expectedStatus = HttpStatus.OK.value();
+
+        assertEquals(expectedStatus, result.getResponse().getStatus());
+    }
+    
+    @Test
+    public void getVolumeTest() throws Exception {
+        VolumeOrder volumeOrder = createVolumeOrder();
+        
+        String volumeId = volumeOrder.getId();
+        String federationTokenValue = volumeOrder.getFederationToken().getAccessId();     
+        
+        PowerMockito.mockStatic(ApplicationFacade.class);
+        given(ApplicationFacade.getInstance()).willReturn(this.facade);
+        doReturn(volumeOrder).when(this.facade).getVolume(volumeId, federationTokenValue);
+        
+        HttpHeaders headers = getHttpHeaders();
+        
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.get(VOLUME_END_POINT)
+                .param("volumeId", volumeId)
+                .headers(headers)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+        
+        int expectedStatus = HttpStatus.OK.value();
+
+        assertEquals(expectedStatus, result.getResponse().getStatus());
+    }
+    
+    @Test
+    public void deleteVolumeTest() throws Exception {
+        VolumeOrder volumeOrder = createVolumeOrder();
+
+        String volumeId = volumeOrder.getId();
+        String federationTokenValue = volumeOrder.getFederationToken().getAccessId();
+
+        PowerMockito.mockStatic(ApplicationFacade.class);
+        given(ApplicationFacade.getInstance()).willReturn(this.facade);
+        doNothing().when(this.facade).deleteVolume(volumeId, federationTokenValue);
+
+        HttpHeaders headers = getHttpHeaders();
+
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.get(VOLUME_END_POINT)
+                .param("volumeId", volumeId)
+                .headers(headers)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        int expectedStatus = HttpStatus.OK.value();
+
+        assertEquals(expectedStatus, result.getResponse().getStatus());
     }
 
     private HttpHeaders getHttpHeaders() {
@@ -88,4 +165,35 @@ public class VolumeOrdersControllerTest {
         return headers;
     }
 
+    private Token createToken() {
+        String accessId = "fake-access-id";
+        Token.User tokenUser = this.createTokenUser();
+        Date expirationTime = new Date();
+        Map<String, String> attributesMap = new HashMap<>();
+        return new Token(accessId, tokenUser, expirationTime, attributesMap);
+    }
+
+    private Token.User createTokenUser() {
+        String tokenUserId = "fake-user-id";
+        String tokenUserName = "fake-user-name";
+        Token.User tokenUser = new Token.User(tokenUserId, tokenUserName);
+        return tokenUser;
+    }
+
+    private VolumeOrder createVolumeOrder() {
+        Token token = this.createToken();
+        
+        VolumeOrder volumeOrder = Mockito.spy(new VolumeOrder());
+        volumeOrder.setFederationToken(token);
+
+        String orderId = volumeOrder.getId();
+
+        Map<String, Order> activeOrdersMap = SharedOrderHolders.getInstance().getActiveOrdersMap();
+        activeOrdersMap.put(orderId, volumeOrder);
+        
+        ChainedList openOrdersList = SharedOrderHolders.getInstance().getOpenOrdersList();
+        openOrdersList.addItem(volumeOrder);
+        
+        return volumeOrder;
+    }
 }
