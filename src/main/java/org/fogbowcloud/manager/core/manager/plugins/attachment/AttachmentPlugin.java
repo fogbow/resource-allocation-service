55449d93c2600f0b790b38953329df25e8f8dbdd
wassoares <wlfsoares@gmail.com>
2018-05-30 10:17:58 -0300

    Modified calls to method 'detachVolume' and 'getAttachment' in 'AttachmentPlugin' interface

--- src/main/java/org/fogbowcloud/manager/core/manager/plugins/attachment/AttachmentPlugin.java
+++ src/main/java/org/fogbowcloud/manager/core/manager/plugins/attachment/AttachmentPlugin.java
@@ -2,14 +2,15 @@ package org.fogbowcloud.manager.core.manager.plugins.attachment;
 
 import org.fogbowcloud.manager.core.exceptions.RequestException;
 import org.fogbowcloud.manager.core.models.orders.AttachmentOrder;
-import org.fogbowcloud.manager.core.models.orders.instances.VolumeAttachment;
+import org.fogbowcloud.manager.core.models.orders.Order;
+import org.fogbowcloud.manager.core.models.orders.instances.AttachmentInstance;
 import org.fogbowcloud.manager.core.models.token.Token;
 
 public interface AttachmentPlugin {
 
 	public String attachVolume(Token localToken, AttachmentOrder attachmentOrder) throws RequestException;
 
-    public void detachVolume(Token localToken, String instanceId, String attachmentId) throws RequestException;
+    public void detachVolume(Token localToken, Order order) throws RequestException;
     
-    public VolumeAttachment getInstance(Token localToken, String instanceId, String attachmentId) throws RequestException;
+    public AttachmentInstance getAttachment(Token localToken, Order order) throws RequestException;
 }
