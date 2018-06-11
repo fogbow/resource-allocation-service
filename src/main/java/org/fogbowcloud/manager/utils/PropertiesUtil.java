d9bc4ae79cd1a9d4c5297ca7f042ee24872362de
Thiago Manel <thiago.manel@gmail.com>
2018-06-11 08:21:56 -0300

    make PropertiesUtil less error prone

--- src/main/java/org/fogbowcloud/manager/utils/PropertiesUtil.java
+++ src/main/java/org/fogbowcloud/manager/utils/PropertiesUtil.java
@@ -17,32 +17,46 @@ public class PropertiesUtil {
         List<String> configFilesNames = new ArrayList<>();
         configFilesNames.add(configFileName);
         return readProperties(configFilesNames);
-
     }
 
     public static Properties readProperties(List<String> configFilesNames) {
 
         Properties properties = new Properties();
 
-        try {
-            for (String fileName : configFilesNames) {
-                Properties mProperties = new Properties();
-                FileInputStream mInput = new FileInputStream(fileName);
-                mProperties.load(mInput);
-                properties.putAll(mProperties);
-            }
-        } catch (FileNotFoundException e) {
-            String[] msgSplitted = e.getMessage().split(" ");
-            String fileName = msgSplitted[0];
-
-            String msg = "No " + fileName + " file was found at resources.";
-            LOGGER.fatal(msg);
-            System.exit(EXIT_ERROR_CODE);
-        } catch (IOException e) {
-            LOGGER.fatal(e.getMessage());
+        for (String fileName : configFilesNames) {
+            Properties mProperties = loadProperties(fileName);
+            properties.putAll(mProperties);
         }
 
         return properties;
+    }
+
+    private static Properties loadProperties(String fileName) {
+
+        Properties prop = new Properties();
+        FileInputStream fileInputStream = null;
+
+        try {
+            fileInputStream = new FileInputStream(fileName);
+            prop.load(fileInputStream);
+        } catch (FileNotFoundException e) {
+            LOGGER.fatal("No " + fileName + " file was found at resources", e);
+            System.exit(EXIT_ERROR_CODE);
+        } catch (IOException e) {
+            LOGGER.fatal(e.getMessage(), e);
+            System.exit(EXIT_ERROR_CODE);
+        } finally {
+            if (fileInputStream != null) {
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                   LOGGER.error("Could not cloud file " + fileName, e);
+                }
+            }
+        }
+
+        return prop;
+
     }
 }
 
