bb55cd9b14f5e7f8ee47f2134db865b32b3ff8bf
wassoares <wlfsoares@gmail.com>
2018-06-12 10:37:23 -0300

    Ignored tests in 'OpenStackImagePluginTest' and 'OpenStackComputeQuotaPluginTest' classes

--- src/test/java/org/fogbowcloud/manager/core/plugins/cloud/quota/compute/OpenStackComputeQuotaPluginTest.java
+++ src/test/java/org/fogbowcloud/manager/core/plugins/cloud/quota/compute/OpenStackComputeQuotaPluginTest.java
@@ -2,29 +2,41 @@ package org.fogbowcloud.manager.core.plugins.cloud.quota.compute;
 
 import java.util.HashMap;
 import java.util.Map;
-
+import java.util.Properties;
+import org.fogbowcloud.manager.core.HomeDir;
+import org.fogbowcloud.manager.core.PropertiesHolder;
+import org.fogbowcloud.manager.core.exceptions.ImageException;
 import org.fogbowcloud.manager.core.exceptions.QuotaException;
 import org.fogbowcloud.manager.core.models.quotas.ComputeQuota;
 import org.fogbowcloud.manager.core.models.token.Token;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.google.gson.Gson;
 
 public class OpenStackComputeQuotaPluginTest {
 	
+    /**
+     * TODO The test must be redone using mock.
+     * @throws ImageException
+     */
+    @Ignore
 	@Test
 	public void testGetUserQuota() throws QuotaException {
-		//Properties properties = new Properties();
-		
-		//properties.put(OpenStackConfigurationConstants.COMPUTE_NOVAV2_URL_KEY, "https://cloud.lsd.ufcg.edu.br:8774");
-		OpenStackComputeQuotaPlugin plugin = new OpenStackComputeQuotaPlugin();
+        HomeDir.getInstance().setPath("src/test/resources/private");
+
+        PropertiesHolder propertiesHolder = PropertiesHolder.getInstance();
+        Properties properties = propertiesHolder.getProperties();
+		properties.put("", "");
+
+        OpenStackComputeQuotaPlugin plugin = new OpenStackComputeQuotaPlugin();
 		
 		Token token = new Token();
 		
 		Map<String, String> attributes = new HashMap<String, String>();
-		attributes.put("tenantId", "3324431f606d4a74a060cf78c16fcb21");
+		attributes.put("tenantId", "");
 		token.setAttributes(attributes);
-		token.setAccessId("gAAAAABbGtM0Eqd3aINFw6lX1Q91k4RDOS4nTd3hkUhymh81JfYa5UGJjkNfbN3WWLHPa-1oSofJG75H9RCMv3Mm1kLzrfdZLErL-rF2ZTOnAxT_iI6qzPyU2A7GmXl9JP12_WHuhVdxLZysxO3UTHryTCJjy-jOWNdvbXV5E6JfWp-SW49rmrH5WXvSLNxEK80Sn7oJgFMjZ3VmyiNh8-skfM01Uo0_8B8dCRfkFskpdu6QV2LzeZg");
+		token.setAccessId("");
 		
 		ComputeQuota quota = plugin.getUserQuota(token);
 		Gson gson = new Gson();
