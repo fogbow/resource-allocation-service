586e00841dcb6068ed1fd851dde23d8948d11c01
Lucas Vieira <lima.lucaslv@gmail.com>
2018-06-21 11:39:14 -0300

    Changed DB to PostgreSQL

--- src/main/java/org/fogbowcloud/manager/core/datastore/DatabaseManager.java
+++ src/main/java/org/fogbowcloud/manager/core/datastore/DatabaseManager.java
@@ -1,33 +1,34 @@
 package org.fogbowcloud.manager.core.datastore;
 
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import org.bson.codecs.configuration.CodecRegistry;
-import org.bson.codecs.pojo.PojoCodecProvider;
-import org.fogbowcloud.manager.core.PropertiesHolder;
+import org.apache.log4j.Logger;
 import org.fogbowcloud.manager.core.models.linkedlist.SynchronizedDoublyLinkedList;
 import org.fogbowcloud.manager.core.models.orders.Order;
 import org.fogbowcloud.manager.core.models.orders.OrderState;
 
-import static com.mongodb.client.model.Filters.eq;
-import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
-import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class DatabaseManager implements StableStorage {
 
-    private static final String HOST = "localhost";
-    private static final int PORT = 27017;
-    private static final String DB_NAME = "fogbow";
+    private static final Logger LOGGER = Logger.getLogger(DatabaseManager.class);
+
+    private static final String URL = "jdbc:postgresql://localhost:21700/recovery";
+    private static final String USERNAME = "fogbow";
+    private static final String PASSWORD = "fogbow";
+
+    private static final String ORDER_TABLE_NAME = "t_order";
+    private static final String ORDER_ID = "order_id";
+    private static final String INSTANCE_ID = "instance_id";
+
+    private static final String INSERT_ORDER_SQL = "INSERT INTO " + ORDER_TABLE_NAME
+            + " (" + ORDER_ID + "," + INSTANCE_ID + ")" + " VALUES (?,?)";
 
     private static DatabaseManager instance;
 
-    private MongoDatabase mongoDatabase;
-
     private DatabaseManager() {
-        PropertiesHolder propertiesHolder = PropertiesHolder.getInstance();
-        // propertiesHolder.getProperty();
-        this.mongoDatabase = initiateDatabase();
+        // Database configuration must be in a propertie file
     }
 
     public static DatabaseManager getInstance() {
@@ -40,14 +41,37 @@ public class DatabaseManager implements StableStorage {
 
     @Override
     public void add(Order order) {
-        MongoCollection<Order> orderCollection = this.mongoDatabase.getCollection("order", Order.class);
-        orderCollection.insertOne(order);
+        Connection connection = null;
+        PreparedStatement orderStatement = null;
+
+        try {
+            connection = getConnection();
+            connection.setAutoCommit(false);
+
+            orderStatement = connection.prepareStatement(INSERT_ORDER_SQL);
+
+            orderStatement.setString(1, order.getId());
+            orderStatement.setString(2, order.getInstanceId());
+
+            orderStatement.executeUpdate();
+
+            connection.commit();
+        } catch (SQLException e) {
+            LOGGER.error("Couldn't create order.", e);
+            try {
+                if (connection != null) {
+                    connection.rollback();
+                }
+            } catch (SQLException e1) {
+                LOGGER.error("Couldn't rollback transaction.", e1);
+            }
+        } finally {
+            closeConnection(orderStatement, connection);
+        }
     }
 
     @Override
     public void update(Order order) {
-        MongoCollection<Order> orderCollection = this.mongoDatabase.getCollection("order", Order.class);
-        orderCollection.replaceOne(eq("_id", order.getId()), order);
     }
 
     @Override
@@ -59,12 +83,34 @@ public class DatabaseManager implements StableStorage {
         return new SynchronizedDoublyLinkedList();
     }
 
-    private MongoDatabase initiateDatabase() {
-        MongoClient mongoClient = new MongoClient(HOST, PORT);
+    private Connection getConnection() throws SQLException {
+        try {
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            LOGGER.error("Error while getting a new connection from the connection pool.", e);
+            throw e;
+        }
+    }
 
-        CodecRegistry pojoCodecRegistry = fromRegistries(MongoClient.getDefaultCodecRegistry(),
-                fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+    private void closeConnection(PreparedStatement statement, Connection connection) {
+        if (statement != null) {
+            try {
+                if (!statement.isClosed()) {
+                    statement.close();
+                }
+            } catch (SQLException e) {
+                LOGGER.error("Couldn't close statement");
+            }
+        }
 
-        return mongoClient.getDatabase(DB_NAME).withCodecRegistry(pojoCodecRegistry);
+        if (connection != null) {
+            try {
+                if (!connection.isClosed()) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                LOGGER.error("Couldn't close connection");
+            }
+        }
     }
 }
