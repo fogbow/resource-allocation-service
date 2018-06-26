8c7fdadb085430a59b9f8ecbed671e8501692a24
wassoares <wlfsoares@gmail.com>
2018-06-26 15:42:12 -0300

    Fixes made in the 'Flavor' and 'OpenStackNovaV2ComputePlugin' classes

--- src/main/java/org/fogbowcloud/manager/core/plugins/cloud/openstack/OpenStackNovaV2ComputePlugin.java
+++ src/main/java/org/fogbowcloud/manager/core/plugins/cloud/openstack/OpenStackNovaV2ComputePlugin.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
