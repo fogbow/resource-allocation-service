da86296eac271854746f263fd0ddc0363295bce8
Ordan <ordan.santos@gmail.com>
2018-07-11 14:50:43 -0300

    OpenStackImagePlugin completed 98,3% coverage

--- src/main/java/org/fogbowcloud/manager/core/models/images/Image.java
+++ src/main/java/org/fogbowcloud/manager/core/models/images/Image.java
@@ -2,7 +2,7 @@ package org.fogbowcloud.manager.core.models.images;
 
 public class Image {
 
-    private String id;
+	private String id;
     private String name;
     private long size; // in bytes
     private long minDisk; // in GB
@@ -29,4 +29,62 @@ public class Image {
     public String getStatus() {
         return status;
     }
+    
+    public long getSize() {
+		return size;
+	}
+
+	public long getMinDisk() {
+		return minDisk;
+	}
+
+	public long getMinRam() {
+		return minRam;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + (int) (minDisk ^ (minDisk >>> 32));
+		result = prime * result + (int) (minRam ^ (minRam >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + (int) (size ^ (size >>> 32));
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Image other = (Image) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (minDisk != other.minDisk)
+			return false;
+		if (minRam != other.minRam)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (size != other.size)
+			return false;
+		if (status == null) {
+			if (other.status != null)
+				return false;
+		} else if (!status.equals(other.status))
+			return false;
+		return true;
+	}
 }
