5d0a7d456da18b37d3cc05c987ab27b05a86f53a
jvmafra <joao.mafra@ccc.ufcg.edu.br>
2018-07-17 11:35:58 -0300

    Refactoring, improving and adding new tests of ComputeOrdersController class

--- src/test/java/org/fogbowcloud/manager/requests/api/local/http/ComputeOrdersControllerTest.java
+++ src/test/java/org/fogbowcloud/manager/requests/api/local/http/ComputeOrdersControllerTest.java
@@ -18,7 +18,10 @@ import org.fogbowcloud.manager.api.http.ComputeOrdersController;
 import org.fogbowcloud.manager.core.ApplicationFacade;
 import org.fogbowcloud.manager.core.exceptions.FogbowManagerException;
 import org.fogbowcloud.manager.core.exceptions.InstanceNotFoundException;
+import org.fogbowcloud.manager.core.models.InstanceStatus;
 import org.fogbowcloud.manager.core.models.instances.ComputeInstance;
+import org.fogbowcloud.manager.core.models.instances.InstanceState;
+import org.fogbowcloud.manager.core.models.instances.InstanceType;
 import org.fogbowcloud.manager.core.models.orders.ComputeOrder;
 import org.junit.Before;
 import org.junit.Test;
@@ -48,19 +51,20 @@ import com.google.gson.reflect.TypeToken;
 @PrepareForTest(ApplicationFacade.class)
 public class ComputeOrdersControllerTest {
 
-    private final String CORRECT_BODY =
+    private static final String CORRECT_BODY =
             "{\"requestingMember\":\"req-member\", \"providingMember\":\"prov-member\", "
                     + "\"publicKey\":\"pub-key\", \"vCPU\":\"2\", \"memory\":\"1024\", \"disk\":\"20\", "
                     + "\"imageName\":\"ubuntu\"}";
 
-    private final String WRONG_BODY = "";
+    private static final String WRONG_BODY = "";
+    private static final String FAKE_ORDER_ID = "fake-order-id";
 
     @Autowired
     private MockMvc mockMvc;
 
     private ApplicationFacade facade;
 
-    private final String COMPUTE_ENDPOINT = "/" + ComputeOrdersController.COMPUTE_ENDPOINT;
+    private static final String COMPUTE_ENDPOINT = "/" + ComputeOrdersController.COMPUTE_ENDPOINT;
 
     @Before
     public void setUp() throws FogbowManagerException {
@@ -70,10 +74,9 @@ public class ComputeOrdersControllerTest {
     }
 
     @Test
-    public void createdComputeTest() throws Exception {
-        String orderId = "fake-id";
+    public void testCreateCompute() throws Exception {
 
-        doReturn(orderId).when(this.facade).createCompute(any(ComputeOrder.class), anyString());
+        doReturn(FAKE_ORDER_ID).when(this.facade).createCompute(any(ComputeOrder.class), anyString());
 
         // Need to make a method to create a body based on parameters, also change the mock above
         RequestBuilder requestBuilder = createRequestBuilder(HttpMethod.POST, COMPUTE_ENDPOINT, getHttpHeaders(), CORRECT_BODY);
@@ -82,13 +85,14 @@ public class ComputeOrdersControllerTest {
 
         int expectedStatus = HttpStatus.CREATED.value();
         assertEquals(expectedStatus, result.getResponse().getStatus());
+        assertEquals(FAKE_ORDER_ID, result.getResponse().getContentAsString());
     }
 
     // There's tests missing, that we need to implement, forcing Application Controller to throw
     // different exceptionsOLD.
 
     @Test
-    public void wrongBodyToPostComputeTest() throws Exception {
+    public void testCreateComputeBadRequest() throws Exception {
         // Need to make a method to create a body based on parameters, also change the mock above
         RequestBuilder requestBuilder = createRequestBuilder(HttpMethod.POST, COMPUTE_ENDPOINT, getHttpHeaders(), WRONG_BODY);
 
@@ -96,10 +100,11 @@ public class ComputeOrdersControllerTest {
 
         int expectedStatus = HttpStatus.BAD_REQUEST.value();
         assertEquals(expectedStatus, result.getResponse().getStatus());
+        assertEquals("", result.getResponse().getContentAsString());
     }
 
     @Test
-    public void getAllComputeWhenHasNoData() throws Exception {
+    public void testGetAllComputesEmptyList() throws Exception {
         List<ComputeInstance> computeInstanceList = new ArrayList<>();
         doReturn(computeInstanceList).when(this.facade).getAllComputes(anyString());
 
@@ -108,14 +113,21 @@ public class ComputeOrdersControllerTest {
         MvcResult result = this.mockMvc.perform(requestBuilder).andReturn();
 
         int expectedStatus = HttpStatus.OK.value();
+        String expectedResult = "[]";
+        
         assertEquals(expectedStatus, result.getResponse().getStatus());
+        assertEquals(expectedResult, result.getResponse().getContentAsString());
     }
 
     @Test
-    public void getAllComputeWhenHasData() throws Exception {
-        ComputeInstance computeInstance1 = new ComputeInstance("fake-Id-1");
-        ComputeInstance computeInstance2 = new ComputeInstance("fake-Id-2");
-        ComputeInstance computeInstance3 = new ComputeInstance("fake-Id-3");
+    public void testGetAllComputesWhenHasData() throws Exception {
+    	final String FAKE_ID_1 = "fake-Id-1";
+    	final String FAKE_ID_2 = "fake-Id-2";
+    	final String FAKE_ID_3 = "fake-Id-3";
+    	
+        ComputeInstance computeInstance1 = new ComputeInstance(FAKE_ID_1);
+        ComputeInstance computeInstance2 = new ComputeInstance(FAKE_ID_2);
+        ComputeInstance computeInstance3 = new ComputeInstance(FAKE_ID_3);
 
         List<ComputeInstance> computeInstanceList = Arrays.asList(new ComputeInstance[] {computeInstance1, computeInstance2, computeInstance3});
         doReturn(computeInstanceList).when(this.facade).getAllComputes(anyString());
@@ -125,17 +137,68 @@ public class ComputeOrdersControllerTest {
 
         int expectedStatus = HttpStatus.OK.value();
         assertEquals(expectedStatus, result.getResponse().getStatus());
-
+        
         TypeToken<List<ComputeInstance>> token = new TypeToken<List<ComputeInstance>>(){};
         List<ComputeInstance> resultList = new Gson().fromJson(result.getResponse().getContentAsString(), token.getType());
         assertTrue(resultList.size() == 3);
+        assertEquals(FAKE_ID_1, resultList.get(0).getId());
+        assertEquals(FAKE_ID_2, resultList.get(1).getId());
+        assertEquals(FAKE_ID_3, resultList.get(2).getId());
+    }
+    
+    @Test
+    public void testGetAllComputeStatusEmptyList() throws Exception {
+    	
+        doReturn(new ArrayList<InstanceState>()).when(this.facade).getAllInstancesStatus(anyString(), any(InstanceType.class));
+        
+        String COMPUTE_STATUS_ENDPOINT = COMPUTE_ENDPOINT + "/" + ComputeOrdersController.STATUS_ENDPOINT;
+
+        RequestBuilder requestBuilder = createRequestBuilder(HttpMethod.GET, COMPUTE_STATUS_ENDPOINT, getHttpHeaders(), "");
+        MvcResult result = this.mockMvc.perform(requestBuilder).andReturn();
+
+        int expectedStatus = HttpStatus.OK.value();
+        assertEquals(expectedStatus, result.getResponse().getStatus());
+        
+        String expectedResult = "[]";
+        assertEquals(expectedResult, result.getResponse().getContentAsString());
+       
+    }
+    
+    @Test
+    public void testGetAllComputeStatusWhenHasData() throws Exception {
+    	final String FAKE_ID_1 = "fake-Id-1";
+    	final String FAKE_ID_2 = "fake-Id-2";
+    	final String FAKE_ID_3 = "fake-Id-3";
+    	
+        InstanceStatus instanceStatus1 = new InstanceStatus(FAKE_ID_1, InstanceState.READY);
+        InstanceStatus instanceStatus2 = new InstanceStatus(FAKE_ID_2, InstanceState.READY);
+        InstanceStatus instanceStatus3 = new InstanceStatus(FAKE_ID_3, InstanceState.READY);
+        
+        List<InstanceStatus> computeStatusList = Arrays.asList(new InstanceStatus[] {instanceStatus1, instanceStatus2, instanceStatus3});
+        doReturn(computeStatusList).when(this.facade).getAllInstancesStatus(anyString(), any(InstanceType.class));
+        
+        String COMPUTE_STATUS_ENDPOINT = COMPUTE_ENDPOINT + "/" + ComputeOrdersController.STATUS_ENDPOINT;
+        RequestBuilder requestBuilder = createRequestBuilder(HttpMethod.GET, COMPUTE_STATUS_ENDPOINT, getHttpHeaders(), "");
+        MvcResult result = this.mockMvc.perform(requestBuilder).andReturn();
+
+        int expectedStatus = HttpStatus.OK.value();
+        assertEquals(expectedStatus, result.getResponse().getStatus());
+        
+        TypeToken<List<InstanceStatus>> token = new TypeToken<List<InstanceStatus>>(){};
+        List<InstanceStatus> resultList = new Gson().fromJson(result.getResponse().getContentAsString(), token.getType());
+        assertTrue(resultList.size() == 3);
+        assertEquals(FAKE_ID_1, resultList.get(0).getInstanceId());
+        assertEquals(FAKE_ID_2, resultList.get(1).getInstanceId());
+        assertEquals(FAKE_ID_3, resultList.get(2).getInstanceId());
     }
 
     @Test
-    public void getComputeById() throws Exception {
-        final String fakeId = "fake-Id-1";
-        String computeIdEndpoint = COMPUTE_ENDPOINT + "/" + fakeId;
-        ComputeInstance computeInstance = new ComputeInstance(fakeId);
+    public void testGetComputeById() throws Exception {
+        final String FAKE_ID = "fake-Id-1";
+        
+        String computeIdEndpoint = COMPUTE_ENDPOINT + "/" + FAKE_ID;
+        ComputeInstance computeInstance = new ComputeInstance(FAKE_ID);
+        
         doReturn(computeInstance).when(this.facade).getCompute(anyString(), anyString());
 
         RequestBuilder requestBuilder = createRequestBuilder(HttpMethod.GET, computeIdEndpoint, getHttpHeaders(), "");
@@ -147,12 +210,13 @@ public class ComputeOrdersControllerTest {
 
         ComputeInstance resultComputeInstance = new Gson().fromJson(result.getResponse().getContentAsString(), ComputeInstance.class);
         assertTrue(resultComputeInstance != null);
+        assertEquals(computeInstance.getId(), resultComputeInstance.getId());
     }
 
     @Test
-    public void getNotFoundComputeById() throws Exception {
-        final String fakeId = "fake-Id-1";
-        String computeIdEndpoint = COMPUTE_ENDPOINT + "/" + fakeId;
+    public void testGetNotFoundComputeById() throws Exception {
+    	final String FAKE_ID = "fake-Id-1";
+        String computeIdEndpoint = COMPUTE_ENDPOINT + "/" + FAKE_ID;
         doThrow(new InstanceNotFoundException()).when(this.facade).getCompute(anyString(), anyString());
         RequestBuilder requestBuilder = createRequestBuilder(HttpMethod.GET, computeIdEndpoint, getHttpHeaders(), "");
 
