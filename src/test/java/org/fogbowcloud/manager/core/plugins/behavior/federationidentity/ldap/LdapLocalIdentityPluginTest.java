b0104db045796eb78191a75dcbe18a7f999a76f2
Ordan <ordan.santos@gmail.com>
2018-07-23 16:47:31 -0300

    fixing tests according to arnett's design pattern

--- src/test/java/org/fogbowcloud/manager/core/plugins/behavior/federationidentity/ldap/LdapLocalIdentityPluginTest.java
+++ src/test/java/org/fogbowcloud/manager/core/plugins/behavior/federationidentity/ldap/LdapLocalIdentityPluginTest.java
@@ -1,9 +1,11 @@
 package org.fogbowcloud.manager.core.plugins.behavior.federationidentity.ldap;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.codec.binary.Base64;
 import org.fogbowcloud.manager.core.HomeDir;
@@ -24,121 +26,83 @@ public class LdapLocalIdentityPluginTest {
     private final String MOCK_SIGNATURE = "mock_signature";
 
     private LdapIdentityPlugin ldapStoneIdentity;
-
+    private final String name = "ldapUser";
+    private final String password = "ldapUserPass";
+    private final String userName = "User Full Name";
+    Map<String, String> userCredentials = new HashMap<String, String>();
+    
     @Before
     public void setUp() throws Exception {
-
         HomeDir.getInstance().setPath("src/test/resources/private");
         PropertiesHolder propertiesHolder = PropertiesHolder.getInstance();
         Properties properties = propertiesHolder.getProperties();
-        properties.put(IDENTITY_URL_KEY, KEYSTONE_URL);
-
+        properties.put(IDENTITY_URL_KEY, this.KEYSTONE_URL);
         this.ldapStoneIdentity = Mockito.spy(new LdapIdentityPlugin());
-        Mockito.doReturn(MOCK_SIGNATURE)
-                .when(ldapStoneIdentity)
+        Mockito.doReturn(this.MOCK_SIGNATURE)
+                .when(this.ldapStoneIdentity)
                 .createSignature(Mockito.any(JSONObject.class));
-    }
 
+        this.userCredentials.put(LdapIdentityPlugin.CRED_USERNAME, this.name);
+        this.userCredentials.put(LdapIdentityPlugin.CRED_PASSWORD, this.password);
+        this.userCredentials.put(LdapIdentityPlugin.CRED_AUTH_URL, "ldapUrl");
+        this.userCredentials.put(LdapIdentityPlugin.CRED_LDAP_BASE, "ldapBase");
+        this.userCredentials.put(LdapIdentityPlugin.CRED_LDAP_ENCRYPT, "");
+        this.userCredentials.put(LdapIdentityPlugin.CRED_PRIVATE_KEY, "private_key_path");
+        this.userCredentials.put(LdapIdentityPlugin.CRED_PUBLIC_KEY, "public_key_path");
+    }
+    
+    //test case: check if the access id information is correct when the user credentials are correct.
     @Test
     public void testCreateToken() throws Exception {
-
-        String name = "ldapUser";
-        String password = "ldapUserPass";
-        String userName = "User Full Name";
-
-        Map<String, String> userCredentials = new HashMap<String, String>();
-        userCredentials.put(LdapIdentityPlugin.CRED_USERNAME, name);
-        userCredentials.put(LdapIdentityPlugin.CRED_PASSWORD, password);
-        userCredentials.put(LdapIdentityPlugin.CRED_AUTH_URL, "ldapUrl");
-        userCredentials.put(LdapIdentityPlugin.CRED_LDAP_BASE, "ldapBase");
-        userCredentials.put(LdapIdentityPlugin.CRED_LDAP_ENCRYPT, "");
-        userCredentials.put(LdapIdentityPlugin.CRED_PRIVATE_KEY, "private_key_path");
-        userCredentials.put(LdapIdentityPlugin.CRED_PUBLIC_KEY, "public_key_path");
-
-        Mockito.doReturn(userName)
-                .when(ldapStoneIdentity)
-                .ldapAuthenticate(Mockito.eq(name), Mockito.eq(password));
-
-        String federationTokenValue = ldapStoneIdentity.createFederationTokenValue(userCredentials);
+    	//set up
+        Mockito.doReturn(this.userName)
+                .when(this.ldapStoneIdentity)
+                .ldapAuthenticate(Mockito.eq(this.name), Mockito.eq(this.password));
+        
+        //exercise
+        String federationTokenValue = this.ldapStoneIdentity.createFederationTokenValue(userCredentials);
+        
+        //verify
         String decodedAccessId = decodeAccessId(federationTokenValue);
-
-        Assert.assertTrue(decodedAccessId.contains(name));
-        Assert.assertTrue(decodedAccessId.contains(userName));
-        Assert.assertTrue(decodedAccessId.contains(MOCK_SIGNATURE));
+        Assert.assertTrue(decodedAccessId.contains(this.name));
+        Assert.assertTrue(decodedAccessId.contains(this.userName));
+        Assert.assertTrue(decodedAccessId.contains(this.MOCK_SIGNATURE));
     }
-
-    @Test
+    
+    //test case: check if createFederationTokenValue throws UnauthenticatedUserException when the user credentials are invalid.
+    @Test (expected = UnauthenticatedUserException.class)
     public void testCreateTokenFail() throws Exception {
-
-        String name = "ldapUser";
-        String password = "ldapUserPass";
-
-        Map<String, String> userCredentials = new HashMap<String, String>();
-        userCredentials.put(LdapIdentityPlugin.CRED_USERNAME, name);
-        userCredentials.put(LdapIdentityPlugin.CRED_PASSWORD, password);
-        userCredentials.put(LdapIdentityPlugin.CRED_AUTH_URL, "ldapUrl");
-        userCredentials.put(LdapIdentityPlugin.CRED_LDAP_BASE, "ldapBase");
-        userCredentials.put(LdapIdentityPlugin.CRED_LDAP_ENCRYPT, "");
-        userCredentials.put(LdapIdentityPlugin.CRED_PRIVATE_KEY, "private_key_path");
-        userCredentials.put(LdapIdentityPlugin.CRED_PUBLIC_KEY, "public_key_path");
-
+    	//set up
         Mockito.doThrow(new FogbowManagerException("Invalid User"))
-                .when(ldapStoneIdentity)
-                .ldapAuthenticate(Mockito.eq(name), Mockito.eq(password));
-        try {
-            ldapStoneIdentity.createFederationTokenValue(userCredentials);
-            Assert.fail();
-        } catch (UnauthenticatedUserException e) {
-            Assert.assertEquals("Couldn't load account summary from LDAP Server.", e.getMessage());
-        }
+                .when(this.ldapStoneIdentity)
+                .ldapAuthenticate(Mockito.eq(name), Mockito.eq(this.password));
+        
+        //exercise/verify
+        this.ldapStoneIdentity.createFederationTokenValue(this.userCredentials);
     }
-
+    
+    //test case: check if isValid returns false when the accessId is invalid.
     @Test
     public void testGetTokenInvalidAccessId() throws Exception {
-
-        String name = "ldapUser";
-        String password = "ldapUserPass";
-        String userName = "User Full Name";
-
-        Map<String, String> userCredentials = new HashMap<String, String>();
-        userCredentials.put(LdapIdentityPlugin.CRED_USERNAME, name);
-        userCredentials.put(LdapIdentityPlugin.CRED_PASSWORD, password);
-        userCredentials.put(LdapIdentityPlugin.CRED_AUTH_URL, "ldapUrl");
-        userCredentials.put(LdapIdentityPlugin.CRED_LDAP_BASE, "ldapBase");
-        userCredentials.put(LdapIdentityPlugin.CRED_LDAP_ENCRYPT, "");
-        userCredentials.put(LdapIdentityPlugin.CRED_PRIVATE_KEY, "private_key_path");
-        userCredentials.put(LdapIdentityPlugin.CRED_PUBLIC_KEY, "public_key_path");
-
-        Mockito.doReturn(userName)
+    	//set up
+        Mockito.doReturn(this.userName)
                 .when(this.ldapStoneIdentity)
-                .ldapAuthenticate(Mockito.eq(name), Mockito.eq(password));
-
-        String tokenValueA = this.ldapStoneIdentity.createFederationTokenValue(userCredentials);
-
+                .ldapAuthenticate(Mockito.eq(this.name), Mockito.eq(this.password));
+        String tokenValueA = this.ldapStoneIdentity.createFederationTokenValue(this.userCredentials);
         String decodedAccessId = decodeAccessId(tokenValueA);
-
         String split[] = decodedAccessId.split(LdapIdentityPlugin.ACCESSID_SEPARATOR);
-        String tokenMessage = split[0];
         String signature = split[1];
-
         String newAccessId =
-                "{name:\"nome\", expirationDate:\"123421\"}"
+                "{name:\"nome\", expirationDate:\"" + (new Date(new Date().getTime() + TimeUnit.DAYS.toMillis(365))).getTime() + "\"}"
                         + LdapIdentityPlugin.ACCESSID_SEPARATOR
                         + signature;
-
         newAccessId =
                 new String(
                         Base64.encodeBase64(
                                 newAccessId.getBytes(StandardCharsets.UTF_8), false, false),
                         StandardCharsets.UTF_8);
 
-        Mockito.doReturn(true)
-                .when(this.ldapStoneIdentity)
-                .verifySign(Mockito.eq(tokenMessage), Mockito.eq(signature));
-        Mockito.doReturn(false)
-                .when(this.ldapStoneIdentity)
-                .verifySign(Mockito.eq(newAccessId), Mockito.eq(signature));
-
+        //exercise/verify
         Assert.assertFalse(this.ldapStoneIdentity.isValid(newAccessId));
     }
 
