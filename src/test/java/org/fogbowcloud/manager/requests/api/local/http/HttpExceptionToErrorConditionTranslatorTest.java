cb6bbc3c6d5031aa5c808bc941149a7cf6ef38d3
Gustavo Yamaguchi <gustavoyama@gmail.com>
2018-07-26 11:05:15 -0300

    Adding comments to test cases

--- src/test/java/org/fogbowcloud/manager/requests/api/local/http/HttpExceptionToErrorConditionTranslatorTest.java
+++ src/test/java/org/fogbowcloud/manager/requests/api/local/http/HttpExceptionToErrorConditionTranslatorTest.java
@@ -46,101 +46,119 @@ public class HttpExceptionToErrorConditionTranslatorTest {
 		given(ApplicationFacade.getInstance()).willReturn(this.facade);
 	}
 
+	//test case: Tests if an UnauthorizedRequestException will be mapped to a FORBIDDEN http status
 	@Test
 	public void testUnauthorizedRequestExceptionMapping() throws Exception {
+		//set up
 		doThrow(new UnauthorizedRequestException()).when(this.facade).getAllComputes(anyString());
-
 		RequestBuilder requestBuilder = createRequestBuilder(computeEndpoint, getHttpHeaders());
+		//exercise
 		MvcResult result = this.mockMvc.perform(requestBuilder).andReturn();
-
+		//verify
 		int expectedStatus = HttpStatus.FORBIDDEN.value();
 		assertEquals(expectedStatus, result.getResponse().getStatus());
 	}
 
+	//test case: Tests if an UnauthenticatedUserException will be mapped to an UNAUTHORIZED http status
 	@Test
 	public void testUnauthenticatedUserExceptionMapping() throws Exception {
+		//set up
 		doThrow(new UnauthenticatedUserException()).when(this.facade).getAllComputes(anyString());
-
 		RequestBuilder requestBuilder = createRequestBuilder(computeEndpoint, getHttpHeaders());
+		//exercise
 		MvcResult result = this.mockMvc.perform(requestBuilder).andReturn();
-
+		//verify
 		int expectedStatus = HttpStatus.UNAUTHORIZED.value();
 		assertEquals(expectedStatus, result.getResponse().getStatus());
 	}
 
+	//test case: Tests if an InvalidParameterException will be mapped to a BAD_REQUEST http status
 	@Test
 	public void testInvalidParameterExceptionMapping() throws Exception {
+		//set up
 		doThrow(new InvalidParameterException()).when(this.facade).getAllComputes(anyString());
-
 		RequestBuilder requestBuilder = createRequestBuilder(computeEndpoint, getHttpHeaders());
+		//exercise
 		MvcResult result = this.mockMvc.perform(requestBuilder).andReturn();
-
+		//verify
 		int expectedStatus = HttpStatus.BAD_REQUEST.value();
 		assertEquals(expectedStatus, result.getResponse().getStatus());
 	}
 
+	//test case: Tests if an InstanceNotFoundException will be mapped to a NOT_FOUND http status
 	@Test
 	public void testInstanceNotFoundExceptionMapping() throws Exception {
+		//set up
 		doThrow(new InstanceNotFoundException()).when(this.facade).getAllComputes(anyString());
-
 		RequestBuilder requestBuilder = createRequestBuilder(computeEndpoint, getHttpHeaders());
+		//exercise
 		MvcResult result = this.mockMvc.perform(requestBuilder).andReturn();
-
+		//verify
 		int expectedStatus = HttpStatus.NOT_FOUND.value();
 		assertEquals(expectedStatus, result.getResponse().getStatus());
 	}
 
+	//test case: Tests if a QuotaExceededException will be mapped to a CONFLICT http status
 	@Test
 	public void testQuotaExceededExceptionMapping() throws Exception {
+		//set up
 		doThrow(new QuotaExceededException()).when(this.facade).getAllComputes(anyString());
-
 		RequestBuilder requestBuilder = createRequestBuilder(computeEndpoint, getHttpHeaders());
+		//exercise
 		MvcResult result = this.mockMvc.perform(requestBuilder).andReturn();
-
+		//verify
 		int expectedStatus = HttpStatus.CONFLICT.value();
 		assertEquals(expectedStatus, result.getResponse().getStatus());
 	}
 
+	//test case: Tests if a NoAvailableResourcesException will be mapped to a NOT_ACCEPTABLE http status
 	@Test
 	public void testNoAvailableResourcesExceptionMapping() throws Exception {
+		//set up
 		doThrow(new NoAvailableResourcesException()).when(this.facade).getAllComputes(anyString());
-
 		RequestBuilder requestBuilder = createRequestBuilder(computeEndpoint, getHttpHeaders());
+		//exercise
 		MvcResult result = this.mockMvc.perform(requestBuilder).andReturn();
-
+		//verify
 		int expectedStatus = HttpStatus.NOT_ACCEPTABLE.value();
 		assertEquals(expectedStatus, result.getResponse().getStatus());
 	}
 
+	//test case: Tests if an UnavailableProviderException will be mapped to a GATEWAY_TIMEOUT http status
 	@Test
 	public void testUnavailableProviderExceptionMapping() throws Exception {
+		//set up
 		doThrow(new UnavailableProviderException()).when(this.facade).getAllComputes(anyString());
-
 		RequestBuilder requestBuilder = createRequestBuilder(computeEndpoint, getHttpHeaders());
+		//exercise
 		MvcResult result = this.mockMvc.perform(requestBuilder).andReturn();
-
+		//verify
 		int expectedStatus = HttpStatus.GATEWAY_TIMEOUT.value();
 		assertEquals(expectedStatus, result.getResponse().getStatus());
 	}
 
+	//test case: Tests if an UnexpectedException will be mapped to an INTERNAL_SERVER_ERROR http status
 	@Test
 	public void testUnexpectedExceptionMapping() throws Exception {
+		//set up
 		doThrow(new UnexpectedException()).when(this.facade).getAllComputes(anyString());
-
 		RequestBuilder requestBuilder = createRequestBuilder(computeEndpoint, getHttpHeaders());
+		//exercise
 		MvcResult result = this.mockMvc.perform(requestBuilder).andReturn();
-
+		//verify
 		int expectedStatus = HttpStatus.INTERNAL_SERVER_ERROR.value();
 		assertEquals(expectedStatus, result.getResponse().getStatus());
 	}
 
+	//test case: Tests if an Exception will be mapped to an UNSUPPORTED_MEDIA_TYPE http status
 	@Test
 	public void testExceptionMapping() throws Exception {
+		//set up
 		doThrow(new Exception()).when(this.facade).getAllComputes(anyString());
-
 		RequestBuilder requestBuilder = createRequestBuilder(computeEndpoint, getHttpHeaders());
+		//exercise
 		MvcResult result = this.mockMvc.perform(requestBuilder).andReturn();
-
+		//verify
 		int expectedStatus = HttpStatus.UNSUPPORTED_MEDIA_TYPE.value();
 		assertEquals(expectedStatus, result.getResponse().getStatus());
 	}
