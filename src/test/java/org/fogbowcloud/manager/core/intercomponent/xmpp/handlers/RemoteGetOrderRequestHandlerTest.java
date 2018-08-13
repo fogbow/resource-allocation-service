ec728c3dc50d151878e63d34e13a18a9e725fe23
Arnett Rufino <arn3tt@gmail.com>
2018-08-13 17:59:54 -0300

    Refactor RemoteGetImage and RemoteGetAllImages

--- src/test/java/org/fogbowcloud/manager/core/intercomponent/xmpp/handlers/RemoteGetOrderRequestHandlerTest.java
+++ src/test/java/org/fogbowcloud/manager/core/intercomponent/xmpp/handlers/RemoteGetOrderRequestHandlerTest.java
@@ -25,106 +25,111 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import org.xmpp.packet.IQ;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ RemoteFacade.class, PacketSenderHolder.class })
+@PrepareForTest({RemoteFacade.class, PacketSenderHolder.class})
 public class RemoteGetOrderRequestHandlerTest {
 
-	private static final String IQ_RESULT = "\n<iq type=\"result\" id=\"%s\" from=\"%s\">\n"
-			+ "  <query xmlns=\"remoteGetOrder\">\n" + "    <instance>{\"id\":\"fake-instance-id\"}</instance>\n"
-			+ "    <instanceClassName>org.fogbowcloud.manager.core.models.instances.Instance</instanceClassName>\n"
-			+ "  </query>\n" + "</iq>";
+    private static final String IQ_RESULT = "\n<iq type=\"result\" id=\"%s\" from=\"%s\">\n"
+        + "  <query xmlns=\"remoteGetOrder\">\n"
+        + "    <instance>{\"id\":\"fake-instance-id\"}</instance>\n"
+        + "    <instanceClassName>org.fogbowcloud.manager.core.models.instances.Instance</instanceClassName>\n"
+        + "  </query>\n" + "</iq>";
 
-	private static final String IQ_ERROR_RESULT = "\n<iq type=\"error\" id=\"%s\" from=\"%s\">\n"
-			+ "  <error code=\"500\" type=\"wait\">\n"
-			+ "    <undefined-condition xmlns=\"urn:ietf:params:xml:ns:xmpp-stanzas\"/>\n"
-			+ "    <text xmlns=\"urn:ietf:params:xml:ns:xmpp-stanzas\">Unexpected exceptionjava.lang.Exception</text>\n"
-			+ "  </error>\n" + "</iq>";
+    private static final String IQ_ERROR_RESULT = "\n<iq type=\"error\" id=\"%s\" from=\"%s\">\n"
+        + "  <error code=\"500\" type=\"wait\">\n"
+        + "    <undefined-condition xmlns=\"urn:ietf:params:xml:ns:xmpp-stanzas\"/>\n"
+        + "    <text xmlns=\"urn:ietf:params:xml:ns:xmpp-stanzas\">Unexpected exception: java.lang.Exception</text>\n"
+        + "  </error>\n" + "</iq>";
 
-	private static final String FAKE_INSTANCE_ID = "fake-instance-id";
+    private static final String FAKE_INSTANCE_ID = "fake-instance-id";
 
-	private RemoteGetOrderRequestHandler remoteGetOrderRequestHandler;
-	private RemoteFacade remoteFacade;
-	private PacketSender packetSender;
+    private RemoteGetOrderRequestHandler remoteGetOrderRequestHandler;
+    private RemoteFacade remoteFacade;
 
-	@Before
-	public void setUp() {
-		this.remoteGetOrderRequestHandler = new RemoteGetOrderRequestHandler();
+    private PacketSender packetSender;
 
-		this.remoteFacade = Mockito.mock(RemoteFacade.class);
-		PowerMockito.mockStatic(RemoteFacade.class);
-		BDDMockito.given(RemoteFacade.getInstance()).willReturn(this.remoteFacade);
+    @Before
+    public void setUp() {
+        this.remoteGetOrderRequestHandler = new RemoteGetOrderRequestHandler();
 
-		this.packetSender = Mockito.mock(PacketSender.class);
-		PowerMockito.mockStatic(PacketSenderHolder.class);
-		BDDMockito.given(PacketSenderHolder.getPacketSender()).willReturn(this.packetSender);
-	}
+        this.remoteFacade = Mockito.mock(RemoteFacade.class);
+        PowerMockito.mockStatic(RemoteFacade.class);
+        BDDMockito.given(RemoteFacade.getInstance()).willReturn(this.remoteFacade);
 
-	// test case: When the handle method is called passing an IQ request, it must
-	// return the Order from that.
-	@Test
-	public void testHandleWithValidIQ() throws Exception {
-		// set up
-		FederationUser federationUser = createFederationUser();
-		Order order = createOrder(federationUser);
-		String orderId = order.getId();
-		Instance instance = new Instance(FAKE_INSTANCE_ID);
+        this.packetSender = Mockito.mock(PacketSender.class);
+        PowerMockito.mockStatic(PacketSenderHolder.class);
+        BDDMockito.given(PacketSenderHolder.getPacketSender()).willReturn(this.packetSender);
+    }
 
-		Mockito.when(this.remoteFacade.getResourceInstance(Mockito.eq(orderId), Mockito.eq(federationUser),
-				Mockito.eq(ResourceType.COMPUTE))).thenReturn(instance);
+    // test case: When the handle method is called passing an IQ request, it must
+    // return the Order from that.
+    @Test
+    public void testHandleWithValidIQ() throws Exception {
+        // set up
+        FederationUser federationUser = createFederationUser();
+        Order order = createOrder(federationUser);
+        String orderId = order.getId();
+        Instance instance = new Instance(FAKE_INSTANCE_ID);
 
-		IQ iq = RemoteGetOrderRequest.marshal(order);
+        Mockito.when(
+            this.remoteFacade.getResourceInstance(Mockito.eq(orderId), Mockito.eq(federationUser),
+                Mockito.eq(ResourceType.COMPUTE))).thenReturn(instance);
 
-		// exercise
-		IQ result = this.remoteGetOrderRequestHandler.handle(iq);
+        IQ iq = RemoteGetOrderRequest.marshal(order);
 
-		// verify
-		Mockito.verify(this.remoteFacade, Mockito.times(1)).getResourceInstance(Mockito.eq(orderId),
-				Mockito.eq(federationUser), Mockito.eq(ResourceType.COMPUTE));
+        // exercise
+        IQ result = this.remoteGetOrderRequestHandler.handle(iq);
 
-		String iqId = iq.getID();
-		String providingMember = order.getProvidingMember();
-		String expected = String.format(IQ_RESULT, iqId, providingMember);
+        // verify
+        Mockito.verify(this.remoteFacade, Mockito.times(1)).getResourceInstance(Mockito.eq(orderId),
+            Mockito.eq(federationUser), Mockito.eq(ResourceType.COMPUTE));
 
-		Assert.assertEquals(expected, result.toString());
+        String iqId = iq.getID();
+        String providingMember = order.getProvidingMember();
+        String expected = String.format(IQ_RESULT, iqId, providingMember);
 
-	}
+        Assert.assertEquals(expected, result.toString());
 
-	// test case: When an Exception occurs, the handle method must return a response
-	// error.
-	@Test
-	public void testHandleWhenThrowsException() throws Exception {
-		// set up
-		FederationUser federationUser = null;
-		Order order = createOrder(federationUser);
-		String orderId = order.getId();
+    }
 
-		Mockito.when(this.remoteFacade.getResourceInstance(Mockito.eq(orderId), Mockito.eq(federationUser),
-				Mockito.eq(ResourceType.COMPUTE))).thenThrow(new Exception());
+    // test case: When an Exception occurs, the handle method must return a response
+    // error.
+    @Test
+    public void testHandleWhenThrowsException() throws Exception {
+        // set up
+        FederationUser federationUser = null;
+        Order order = createOrder(federationUser);
+        String orderId = order.getId();
 
-		IQ iq = RemoteGetOrderRequest.marshal(order);
+        Mockito.when(
+            this.remoteFacade.getResourceInstance(Mockito.eq(orderId), Mockito.eq(federationUser),
+                Mockito.eq(ResourceType.COMPUTE))).thenThrow(new Exception());
 
-		// exercise
-		IQ result = this.remoteGetOrderRequestHandler.handle(iq);
+        IQ iq = RemoteGetOrderRequest.marshal(order);
 
-		// verify
-		Mockito.verify(this.remoteFacade, Mockito.times(1)).getResourceInstance(Mockito.eq(orderId),
-				Mockito.eq(federationUser), Mockito.eq(ResourceType.COMPUTE));
+        // exercise
+        IQ result = this.remoteGetOrderRequestHandler.handle(iq);
 
-		String iqId = iq.getID();
-		String providingMember = order.getProvidingMember();
-		String expected = String.format(IQ_ERROR_RESULT, iqId, providingMember);
+        // verify
+        Mockito.verify(this.remoteFacade, Mockito.times(1)).getResourceInstance(Mockito.eq(orderId),
+            Mockito.eq(federationUser), Mockito.eq(ResourceType.COMPUTE));
 
-		Assert.assertEquals(expected, result.toString());
-	}
+        String iqId = iq.getID();
+        String providingMember = order.getProvidingMember();
+        String expected = String.format(IQ_ERROR_RESULT, iqId, providingMember);
 
-	private Order createOrder(FederationUser federationUser) throws InvalidParameterException {
-		return new ComputeOrder(federationUser, "requestingMember", "providingmember", 1, 2, 3, "imageId", null,
-				"publicKey", new ArrayList<>());
-	}
+        Assert.assertEquals(expected, result.toString());
+    }
 
-	private FederationUser createFederationUser() throws InvalidParameterException {
-		Map<String, String> attributes = new HashMap<>();
-		attributes.put("user-name", "fogbow");
-		return new FederationUser("fake-id", attributes);
-	}
+    private Order createOrder(FederationUser federationUser) throws InvalidParameterException {
+        return new ComputeOrder(federationUser, "requestingMember", "providingmember", 1, 2, 3,
+            "imageId", null,
+            "publicKey", new ArrayList<>());
+    }
+
+    private FederationUser createFederationUser() throws InvalidParameterException {
+        Map<String, String> attributes = new HashMap<>();
+        attributes.put("user-name", "fogbow");
+        return new FederationUser("fake-id", attributes);
+    }
 
 }
--- src/test/java/org/fogbowcloud/manager/core/intercomponent/xmpp/requesters/RemoteGetAllImagesRequestTest.java
+++ src/test/java/org/fogbowcloud/manager/core/intercomponent/xmpp/requesters/RemoteGetAllImagesRequestTest.java
@@ -1,19 +1,19 @@
 package org.fogbowcloud.manager.core.intercomponent.xmpp.requesters;
 
+import com.google.gson.Gson;
 import java.util.HashMap;
 import java.util.Map;
-
 import org.dom4j.Element;
 import org.fogbowcloud.manager.core.exceptions.InvalidParameterException;
 import org.fogbowcloud.manager.core.exceptions.UnauthorizedRequestException;
 import org.fogbowcloud.manager.core.exceptions.UnavailableProviderException;
 import org.fogbowcloud.manager.core.exceptions.UnexpectedException;
+import org.fogbowcloud.manager.core.intercomponent.xmpp.IQMatcher;
 import org.fogbowcloud.manager.core.intercomponent.xmpp.IqElement;
 import org.fogbowcloud.manager.core.intercomponent.xmpp.PacketSenderHolder;
 import org.fogbowcloud.manager.core.intercomponent.xmpp.RemoteMethod;
 import org.fogbowcloud.manager.core.models.tokens.FederationUser;
 import org.jamppa.component.PacketSender;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
@@ -21,120 +21,105 @@ import org.mockito.Mockito;
 import org.xmpp.packet.IQ;
 import org.xmpp.packet.PacketError;
 
-import com.google.gson.Gson;
-
 public class RemoteGetAllImagesRequestTest {
-	
- 	private RemoteGetAllImagesRequest remoteGetAllImagesRequest;
-	private PacketSender packetSender;
-	private ArgumentCaptor<IQ> argIQ = ArgumentCaptor.forClass(IQ.class);
-	private FederationUser federationUser;
-	HashMap<String, String> imagesMap;
-	private final String provider = "provider";
-	
- 	@Before
-	public void setUp() throws InvalidParameterException {
- 		Map<String, String> attributes = new HashMap<String, String>();
- 		attributes.put("user-name", "user-name");
- 		this.federationUser = new FederationUser("federation-user-id", attributes);
- 		
-		this.remoteGetAllImagesRequest = new RemoteGetAllImagesRequest(provider, federationUser);
-		this.packetSender = Mockito.mock(PacketSender.class);
-		PacketSenderHolder.init(packetSender);
-		
-		this.imagesMap = new HashMap<String, String>();
-		this.imagesMap.put("key-1", "value-1");
-		this.imagesMap.put("key-2", "value-2");
-		this.imagesMap.put("key-3", "value-3");
-	}
- 	
- 	//test case: checks if IQ attributes is according to both RemoteGetAllImagesRequestTest constructor parameters 
- 	//and remote get all images request rules. In addition, it checks if the image map from a possible response is 
- 	//properly created and returned by the "send" method
-	@Test
-	public void testSend() throws Exception {
-		//set up
-		IQ iqResponse = getImagesMapIQResponse(this.imagesMap);
-		Mockito.doReturn(iqResponse).when(this.packetSender).syncSendPacket(argIQ.capture());
- 		String federationUserJson = new Gson().toJson(this.federationUser);
 
- 		//exercise
-		HashMap<String, String> responseImagesMap = this.remoteGetAllImagesRequest.send();
-		
- 		//verify
-		IQ iq = argIQ.getValue();
-		Assert.assertEquals(IQ.Type.get.toString(), iq.getType().toString());
-		Assert.assertEquals(this.provider, iq.getTo().toString());
-		
-		Element iqElementQuery = iq.getElement().element(IqElement.QUERY.toString());
-		Assert.assertEquals(RemoteMethod.REMOTE_GET_ALL_IMAGES.toString(), iqElementQuery.getNamespaceURI());
-		
-		String iqQueryMemberId = iqElementQuery.element(IqElement.MEMBER_ID.toString()).getText();
-		Assert.assertEquals(this.provider, iqQueryMemberId);
-		
-		String iqQueryUser = iqElementQuery.element(IqElement.FEDERATION_USER.toString()).getText();
-		Assert.assertEquals(federationUserJson, iqQueryUser);
-		
-		Assert.assertEquals(this.imagesMap, responseImagesMap);
-	}
-	
-	//test case: checks if "send" is properly forwading UnavailableProviderException thrown by 
-	//"XmppErrorConditionToExceptionTranslator.handleError" when the IQ response is null
-	@Test (expected = UnavailableProviderException.class)
-	public void testSendWhenResponseIsNull() throws Exception {
-		//set up
-		Mockito.doReturn(null).when(this.packetSender).syncSendPacket(this.argIQ.capture());
- 		
-		//exercise/verify
-		this.remoteGetAllImagesRequest.send();
-	}
-	
-	//test case: checks if "send" is properly forwading UnauthorizedRequestException thrown by 
-	//"XmppErrorConditionToExceptionTranslator.handleError" when the IQ response status is forbidden
-	@Test (expected = UnauthorizedRequestException.class)
-	public void testSendWhenResponseReturnsForbidden() throws Exception {
-		//set up
-		IQ iqResponse = new IQ();
-		Mockito.doReturn(iqResponse).when(this.packetSender).syncSendPacket(this.argIQ.capture());
-		iqResponse.setError(new PacketError(PacketError.Condition.forbidden));
-		
-		//exercise/verify
-		this.remoteGetAllImagesRequest.send();
-	}
-	
-	//test case: checks if "send" is properly forwading UnexpectedException thrown by 
-	//"getImageFromResponse" when the images map class name from the IQ response is undefined (wrong or not found)
-	@Test(expected = UnexpectedException.class)
-	public void testSendWhenImageClassIsUndefined() throws Exception {
-		//set up
-		IQ iqResponse = getImagesMapIQResponseWithWrongClass(this.imagesMap);
-		Mockito.doReturn(iqResponse).when(this.packetSender).syncSendPacket(argIQ.capture());
+    private final String PROVIDER = "provider";
 
- 		//exercise/verify
-		this.remoteGetAllImagesRequest.send();
-	}
-	
-	private IQ getImagesMapIQResponse(Map<String, String> imagesMap) {
+    private PacketSender packetSender;
+    private RemoteGetAllImagesRequest remoteGetAllImagesRequest;
+
+    private HashMap<String, String> imagesMap;
+    private FederationUser federationUser;
+
+    private ArgumentCaptor<IQ> argIQ = ArgumentCaptor.forClass(IQ.class);
+
+    @Before
+    public void setUp() throws InvalidParameterException {
+        Map<String, String> attributes = new HashMap<String, String>();
+        attributes.put("user-name", "user-name");
+        this.federationUser = new FederationUser("federation-user-id", attributes);
+
+        this.remoteGetAllImagesRequest = new RemoteGetAllImagesRequest(PROVIDER, federationUser);
+        this.packetSender = Mockito.mock(PacketSender.class);
+        PacketSenderHolder.init(packetSender);
+
+        this.imagesMap = new HashMap<String, String>();
+        this.imagesMap.put("key-1", "value-1");
+        this.imagesMap.put("key-2", "value-2");
+        this.imagesMap.put("key-3", "value-3");
+    }
+
+    // test case: checks if IQ attributes is according to both RemoteGetAllImagesRequestTest constructor parameters
+    // and remote get all images request rules. In addition, it checks if the image map from a possible response is
+    // properly created and returned by the "send" method
+    @Test
+    public void testSend() throws Exception {
+        // set up
+        IQ response = getImagesResponse(this.imagesMap, this.imagesMap.getClass().getName());
+        Mockito.doReturn(response).when(this.packetSender).syncSendPacket(Mockito.any(IQ.class));
+
+        // exercise
+        this.remoteGetAllImagesRequest.send();
+
+        // verify
+        // as IQ does not implement equals we need a matcher
+        IQ expectedIQ = RemoteGetAllImagesRequest.marshal(PROVIDER, federationUser);
+        IQMatcher matcher = new IQMatcher(expectedIQ);
+        Mockito.verify(this.packetSender).syncSendPacket(Mockito.argThat(matcher));
+    }
+
+    // test case: checks if "send" is properly forwading UnavailableProviderException thrown by
+    // "XmppErrorConditionToExceptionTranslator.handleError" when the IQ response is null
+    @Test(expected = UnavailableProviderException.class)
+    public void testSendWhenResponseIsNull() throws Exception {
+        // set up
+        Mockito.doReturn(null).when(this.packetSender).syncSendPacket(Mockito.any(IQ.class));
+
+        // exercise/verify
+        this.remoteGetAllImagesRequest.send();
+    }
+
+    // test case: checks if "send" is properly forwading UnauthorizedRequestException thrown by
+    // "XmppErrorConditionToExceptionTranslator.handleError" when the IQ response status is forbidden
+    @Test(expected = UnauthorizedRequestException.class)
+    public void testSendWhenResponseReturnsForbidden() throws Exception {
+        // set up
         IQ iqResponse = new IQ();
-        Element queryEl = iqResponse.getElement().addElement(IqElement.QUERY.toString(), RemoteMethod.REMOTE_GET_ALL_IMAGES.toString());
+        Mockito.doReturn(iqResponse).when(this.packetSender).syncSendPacket(Mockito.any(IQ.class));
+        iqResponse.setError(new PacketError(PacketError.Condition.forbidden));
+
+        // exercise/verify
+        this.remoteGetAllImagesRequest.send();
+    }
+
+    // test case: checks if "send" is properly forwading UnexpectedException thrown by
+    // "getImageFromResponse" when the images map class name from the IQ response is undefined (wrong or not found)
+    @Test(expected = UnexpectedException.class)
+    public void testSendWhenImageClassIsUndefined() throws Exception {
+        // set up
+        IQ iqResponse = getImagesMapIQResponseWithWrongClass(this.imagesMap);
+        Mockito.doReturn(iqResponse).when(this.packetSender).syncSendPacket(Mockito.any(IQ.class));
+
+        // exercise/verify
+        this.remoteGetAllImagesRequest.send();
+    }
+
+    private IQ getImagesResponse(Map<String, String> imagesMap,
+        String className) {
+        IQ iqResponse = new IQ();
+        Element queryEl = iqResponse.getElement()
+            .addElement(IqElement.QUERY.toString(), RemoteMethod.REMOTE_GET_ALL_IMAGES.toString());
         Element imagesMapElement = queryEl.addElement(IqElement.IMAGES_MAP.toString());
 
-        Element imagesMapClassNameElement = queryEl.addElement(IqElement.IMAGES_MAP_CLASS_NAME.toString());
-        imagesMapClassNameElement.setText(imagesMap.getClass().getName());
+        Element imagesMapClassNameElement = queryEl
+            .addElement(IqElement.IMAGES_MAP_CLASS_NAME.toString());
+        imagesMapClassNameElement.setText(className);
 
         imagesMapElement.setText(new Gson().toJson(imagesMap));
         return iqResponse;
-	}
-	
-	private IQ getImagesMapIQResponseWithWrongClass(Map<String, String> imagesMap) {
-        IQ iqResponse = new IQ();
-        Element queryEl = iqResponse.getElement().addElement(IqElement.QUERY.toString(), RemoteMethod.REMOTE_GET_ALL_IMAGES.toString());
-        Element imagesMapElement = queryEl.addElement(IqElement.IMAGES_MAP.toString());
+    }
 
-        Element imagesMapClassNameElement = queryEl.addElement(IqElement.IMAGES_MAP_CLASS_NAME.toString());
-        imagesMapClassNameElement.setText("wrong-class-name");
-
-        imagesMapElement.setText(new Gson().toJson(imagesMap));
-        return iqResponse;
-	}
+    private IQ getImagesMapIQResponseWithWrongClass(Map<String, String> imagesMap) {
+        return getImagesResponse(imagesMap, "wrongClassName");
+    }
 }
