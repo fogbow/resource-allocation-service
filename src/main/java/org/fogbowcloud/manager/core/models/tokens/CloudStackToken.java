c40512956c55c563f9ddf6223064bd887f26f3c5
Paulo Ewerton <pauloewerton@lsd.ufcg.edu.br>
2018-08-15 15:40:08 -0300

    Adding cloudstack token

---
+++ src/main/java/org/fogbowcloud/manager/core/models/tokens/CloudStackToken.java
@@ -0,0 +1,8 @@
+package org.fogbowcloud.manager.core.models.tokens;
+
+public class CloudStackToken extends Token {
+
+    public CloudStackToken(String tokenValue) {
+        super(tokenValue);
+    }
+}
