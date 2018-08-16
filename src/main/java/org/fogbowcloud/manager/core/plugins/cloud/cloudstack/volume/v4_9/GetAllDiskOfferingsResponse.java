72bac1aa7dc8dbdc53c0601486f44cff574c930b
wassoares <wlfsoares@gmail.com>
2018-08-16 15:31:53 -0300

    Implementing the GetAllDiskOfferingsResponse class

--- src/main/java/org/fogbowcloud/manager/core/plugins/cloud/cloudstack/volume/v4_9/GetAllDiskOfferingsResponse.java
+++ src/main/java/org/fogbowcloud/manager/core/plugins/cloud/cloudstack/volume/v4_9/GetAllDiskOfferingsResponse.java
@@ -1,22 +1,72 @@
 package org.fogbowcloud.manager.core.plugins.cloud.cloudstack.volume.v4_9;
 
+import static org.fogbowcloud.manager.core.plugins.cloud.cloudstack.CloudStackRestApiConstants.Volume.*;
+
+import java.util.List;
+import org.fogbowcloud.manager.core.plugins.cloud.openstack.compute.v2.GetAllFlavorsResponse;
+import org.fogbowcloud.manager.util.GsonHolder;
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Documentation: https://cloudstack.apache.org/api/apidocs-4.9/apis/listDiskOfferings.html
  *
  * Response example:
  * {
- *   "listdiskofferingsresponse": { 
- *      "diskoffering": [
- *      { 
- *          "id": "e9c2a08d-6ca4-4b81-8e21-5ff2a103b7cb", 
- *          "disksize": 10, 
- *          "iscustomized": false, 
- *      }] 
- *   }
+ *      "listdiskofferingsresponse": { 
+ *          "diskoffering": [{ 
+ *              "id": "e9c2a08d-6ca4-4b81-8e21-5ff2a103b7cb", 
+ *              "disksize": 10, 
+ *              "iscustomized": false, 
+ *          }] 
+ *      }
  * }
  */
 public class GetAllDiskOfferingsResponse {
     
+    @SerializedName(LIST_DISK_OFFERING_RESPONSE_KEY_JSON)
+    private DisckOfferingsResponse response;
     
+    public class DisckOfferingsResponse {
+        
+        @SerializedName(DISK_OFFERING_KEY_JSON)
+        private List<DiskOffering> diskOfferings;
+        
+        public List<DiskOffering> getDiskOfferings() {
+            return this.diskOfferings;
+        }
+    }
+    
+    public List<DiskOffering> getDiskOfferings() {
+        return this.response.getDiskOfferings();
+    }
+    
+    public static GetAllDiskOfferingsResponse fromJson(String json) {
+        return GsonHolder.getInstance().fromJson(json, GetAllDiskOfferingsResponse.class);
+    }
+    
+    public class DiskOffering {
+        
+        @SerializedName(ID_KEY_JSON)
+        private String id;
+        
+        @SerializedName(DISK_SIZE_KEY_JSON)
+        private int diskSize;
+        
+        @SerializedName(CUSTOMIZED_KEY_JSON)
+        private boolean customized;
+
+        public String getId() {
+            return id;
+        }
+
+        public int getDiskSize() {
+            return diskSize;
+        }
+
+        public boolean isCustomized() {
+            return customized;
+        }
+        
+    }
     
 }
