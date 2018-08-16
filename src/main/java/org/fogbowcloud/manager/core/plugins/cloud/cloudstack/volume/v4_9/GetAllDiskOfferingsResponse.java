fb71e354adea044e394a3afa3fa1d7ae4bbda968
wassoares <wlfsoares@gmail.com>
2018-08-16 11:08:41 -0300

    Created the GetAllDiskOfferingsResponse class

---
+++ src/main/java/org/fogbowcloud/manager/core/plugins/cloud/cloudstack/volume/v4_9/GetAllDiskOfferingsResponse.java
@@ -0,0 +1,22 @@
+package org.fogbowcloud.manager.core.plugins.cloud.cloudstack.volume.v4_9;
+
+/**
+ * Documentation: https://cloudstack.apache.org/api/apidocs-4.9/apis/listDiskOfferings.html
+ *
+ * Response example:
+ * {
+ *   "listdiskofferingsresponse": { 
+ *      "diskoffering": [
+ *      { 
+ *          "id": "e9c2a08d-6ca4-4b81-8e21-5ff2a103b7cb", 
+ *          "disksize": 10, 
+ *          "iscustomized": false, 
+ *      }] 
+ *   }
+ * }
+ */
+public class GetAllDiskOfferingsResponse {
+    
+    
+    
+}
