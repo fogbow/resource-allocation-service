d51585cf36d7a094a5f67a70a44df4cfbfee4152
Gustavo Yamaguchi <gustavoyama@gmail.com>
2018-08-23 16:29:44 -0300

    Adding application and logs properties

--- src/main/java/org/fogbowcloud/manager/core/HomeDir.java
+++ src/main/java/org/fogbowcloud/manager/core/HomeDir.java
@@ -2,8 +2,10 @@ package org.fogbowcloud.manager.core;
 
 public class HomeDir {
 
+    public static final String PRIVATE_DIRECTORY= "private/";
+
     public static String getPath() {
-        return Thread.currentThread().getContextClassLoader().getResource("").getPath();
+        return Thread.currentThread().getContextClassLoader().getResource("").getPath() + PRIVATE_DIRECTORY;
     }
 
 }
