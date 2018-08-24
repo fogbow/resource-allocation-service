cea30c64ac7c81754afd3eff2ff66d94c0579ee6
Paulo Ewerton <pauloewerton@lsd.ufcg.edu.br>
2018-08-24 17:46:49 -0300

    Removing unused destroy vm response

--- src/main/java/org/fogbowcloud/manager/core/plugins/cloud/cloudstack/compute/v4_9/DestroyVirtualMachineRequest.java
+++ src/main/java/org/fogbowcloud/manager/core/plugins/cloud/cloudstack/compute/v4_9/DestroyVirtualMachineRequest.java
@@ -5,12 +5,14 @@ import org.fogbowcloud.manager.core.plugins.cloud.cloudstack.CloudStackRequest;
 
 public class DestroyVirtualMachineRequest extends CloudStackRequest {
 
-    protected static final String DESTROY_VIRTUAL_MACHINE_COMMAND = "deleteVolume";
+    protected static final String DESTROY_VIRTUAL_MACHINE_COMMAND = "destroyVirtualMachine";
     private static final String VIRTUAL_MACHINE_ID_KEY = "id";
+    private static final String EXPUNGE_KEY = "expunge";
 
     protected DestroyVirtualMachineRequest(Builder builder) throws InvalidParameterException {
         super();
         addParameter(VIRTUAL_MACHINE_ID_KEY, builder.id);
+        addParameter(EXPUNGE_KEY, builder.expunge);
     }
 
     @Override
