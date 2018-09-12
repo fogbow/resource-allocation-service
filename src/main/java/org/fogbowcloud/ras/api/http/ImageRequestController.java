ea33804a2babcccc18a05336930a92e9785a9e77
Gustavo <gustavo.monteiro@ccc.ufcg.edu.br>
2018-09-12 14:57:55 -0300

    Allow cross origin requests.

--- src/main/java/org/fogbowcloud/ras/api/http/ImageRequestController.java
+++ src/main/java/org/fogbowcloud/ras/api/http/ImageRequestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = ImageRequestController.IMAGE_ENDPOINT)
 public class ImageRequestController {
--- src/main/java/org/fogbowcloud/ras/api/http/VersionRequestController.java
+++ src/main/java/org/fogbowcloud/ras/api/http/VersionRequestController.java
@@ -4,10 +4,12 @@ import org.apache.log4j.Logger;
 import org.fogbowcloud.ras.core.ApplicationFacade;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = VersionRequestController.VERSION_ENDPOINT)
 public class VersionRequestController {
