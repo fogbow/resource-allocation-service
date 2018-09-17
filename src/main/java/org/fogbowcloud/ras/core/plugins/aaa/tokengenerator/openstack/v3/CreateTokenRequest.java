17e0abdf39c0919a321a58b95961b694b3ba2681
Arnett Rufino <arn3tt@gmail.com>
2018-09-17 19:47:23 -0300

    [OpenStack] Instantiate CreateTokenRequest properly and test it against a valid request

--- src/main/java/org/fogbowcloud/ras/core/plugins/aaa/tokengenerator/openstack/v3/CreateTokenRequest.java
+++ src/main/java/org/fogbowcloud/ras/core/plugins/aaa/tokengenerator/openstack/v3/CreateTokenRequest.java
@@ -1,34 +1,38 @@
 package org.fogbowcloud.ras.core.plugins.aaa.tokengenerator.openstack.v3;
 
 import com.google.gson.annotations.SerializedName;
+import java.util.Arrays;
 import org.fogbowcloud.ras.util.GsonHolder;
 import org.fogbowcloud.ras.util.JsonSerializable;
 
 import static org.fogbowcloud.ras.core.plugins.interoperability.openstack.OpenstackRestApiConstants.Identity.*;
 
 /**
- * Documentation: https://developer.openstack.org/api-ref/identity/v3/
- * <p>
- * Request Example:
  * {
- * "auth":{
- * "scope":{
- * "project":{
- * "id":"3324431f606d4a74a060cf78c16fcb2111"
- * }
- * },
- * "identity":{
- * "methods":[
- * "password"
- * ],
- * "password":{
- * "user":{
- * "id":"6642431f606d4a74a060cf78c16fcb2121",
- * "password":"password"
- * }
- * }
- * }
- * }
+ *     "auth":{
+ *         "identity":{
+ *             "methods":[
+ *                 "password"
+ *             ],
+ *             "password":{
+ *                 "user":{
+ *                     "domain":{
+ *                         "name":"LSD"
+ *                     },
+ *                     "name":"fogbow",
+ *                     "password":"c24313A4a31a"
+ *                 }
+ *             }
+ *         },
+ *         "scope":{
+ *             "project":{
+ *                 "domain":{
+ *                     "name":"LSD"
+ *                 },
+ *                 "name":"atmosphere-workers"
+ *             }
+ *         }
+ *     }
  * }
  * <p>
  * We use the @SerializedName annotation to specify that the request parameter is not equal to the class field.
@@ -36,6 +40,7 @@ import static org.fogbowcloud.ras.core.plugins.interoperability.openstack.Openst
 public class CreateTokenRequest implements JsonSerializable {
 
     public static final String PASSWORD_AUTHENTICATION_METHOD = "password";
+
     @SerializedName(AUTH_KEY_JSON)
     private Auth auth;
 
@@ -45,6 +50,12 @@ public class CreateTokenRequest implements JsonSerializable {
         Password password = new Password(user);
 
         String[] methods = new String[] { PASSWORD_AUTHENTICATION_METHOD };
+        Identity identity = new Identity(methods, password);
+
+        Project project = new Project(domain, builder.projectName);
+        Scope scope = new Scope(project);
+
+        this.auth = new Auth(identity, scope);
     }
 
     @Override
@@ -52,16 +63,70 @@ public class CreateTokenRequest implements JsonSerializable {
         return GsonHolder.getInstance().toJson(this);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        CreateTokenRequest request = (CreateTokenRequest) o;
+
+        return auth != null ? auth.equals(request.auth) : request.auth == null;
+    }
+
     private static class Auth {
         @SerializedName(SCOPE_KEY_JSON)
         private Scope scope;
         @SerializedName(IDENTITY_KEY_JSON)
         private Identity identity;
+
+        public Auth(Identity identity, Scope scope) {
+            this.identity = identity;
+            this.scope = scope;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+
+            Auth auth = (Auth) o;
+
+            if (scope != null ? !scope.equals(auth.scope) : auth.scope != null) {
+                return false;
+            }
+            return identity != null ? identity.equals(auth.identity) : auth.identity == null;
+        }
     }
 
     private static class Scope {
         @SerializedName(PROJECT_KEY_JSON)
         private Project project;
+
+        public Scope(Project project) {
+            this.project = project;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+
+            Scope scope = (Scope) o;
+
+            return project != null ? project.equals(scope.project) : scope.project == null;
+        }
     }
 
     private static class Identity {
@@ -69,6 +134,30 @@ public class CreateTokenRequest implements JsonSerializable {
         private Password password;
         @SerializedName(METHODS_KEY_JSON)
         private String[] methods;
+
+        public Identity(String[] methods, Password password) {
+            this.methods = methods;
+            this.password = password;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+
+            Identity identity = (Identity) o;
+
+            if (password != null ? !password.equals(identity.password)
+                : identity.password != null) {
+                return false;
+            }
+            return Arrays.equals(methods, identity.methods);
+        }
+
     }
 
     private static class Project {
@@ -76,6 +165,28 @@ public class CreateTokenRequest implements JsonSerializable {
         private Domain domain;
         @SerializedName(NAME_KEY_JSON)
         private String name;
+
+        public Project(Domain domain, String name) {
+            this.domain = domain;
+            this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+
+            Project project = (Project) o;
+
+            if (domain != null ? !domain.equals(project.domain) : project.domain != null) {
+                return false;
+            }
+            return name != null ? name.equals(project.name) : project.name == null;
+        }
     }
 
     private static class Password {
@@ -84,6 +195,20 @@ public class CreateTokenRequest implements JsonSerializable {
 
         public Password(User user) {
             this.user = user;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+
+            Password password = (Password) o;
+
+            return user != null ? user.equals(password.user) : password.user == null;
         }
     }
 
@@ -100,6 +225,26 @@ public class CreateTokenRequest implements JsonSerializable {
             this.password = password;
             this.domain = domain;
         }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+
+            User user = (User) o;
+
+            if (name != null ? !name.equals(user.name) : user.name != null) {
+                return false;
+            }
+            if (password != null ? !password.equals(user.password) : user.password != null) {
+                return false;
+            }
+            return domain != null ? domain.equals(user.domain) : user.domain == null;
+        }
     }
 
     public static class Domain {
@@ -108,6 +253,20 @@ public class CreateTokenRequest implements JsonSerializable {
 
         public Domain(String name) {
             this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+
+            Domain domain = (Domain) o;
+
+            return name != null ? name.equals(domain.name) : domain.name == null;
         }
     }
 
