c58886c9a581dd6fdc4dc10bc3a9704863fcad39
wassoares <wlfsoares@gmail.com>
2018-09-21 15:40:18 -0300

    Created the VirtualMachineGraphics class

---
+++ src/main/java/org/fogbowcloud/ras/core/plugins/interoperability/opennebula/compute/v5_4/VirtualMachineGraphics.java
@@ -0,0 +1,30 @@
+package org.fogbowcloud.ras.core.plugins.interoperability.opennebula.compute.v5_4;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "GRAPHICS")
+public class VirtualMachineGraphics {
+
+	private String listen;
+	private String type;
+	
+	@XmlElement(name = "LISTEN")
+	public String getListen() {
+		return listen;
+	}
+	
+	public void setListen(String listen) {
+		this.listen = listen;
+	}
+	
+	@XmlElement(name = "TYPE")
+	public String getType() {
+		return type;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+}
