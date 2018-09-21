3329840929c33f8ab85dc574d93e94ce0e2efd1b
wassoares <wlfsoares@gmail.com>
2018-09-21 15:39:06 -0300

    Created the VirtualMachineContext class

---
+++ src/main/java/org/fogbowcloud/ras/core/plugins/interoperability/opennebula/compute/v5_4/VirtualMachineContext.java
@@ -0,0 +1,40 @@
+package org.fogbowcloud.ras.core.plugins.interoperability.opennebula.compute.v5_4;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "CONTEXT")
+public class VirtualMachineContext {
+
+	private String encoding;
+	private String userdata;
+	private String network;
+	
+	@XmlElement(name = "USERDATA_ENCODING")
+	public String getEncoding() {
+		return encoding;
+	}
+	
+	public void setEncoding(String encoding) {
+		this.encoding = encoding;
+	}
+	
+	@XmlElement(name = "USERDATA")
+	public String getUserdata() {
+		return userdata;
+	}
+	
+	public void setUserdata(String userdata) {
+		this.userdata = userdata;
+	}
+	
+	@XmlElement(name = "NETWORK")
+	public String getNetwork() {
+		return network;
+	}
+	
+	public void setNetwork(String network) {
+		this.network = network;
+	}
+	
+}
