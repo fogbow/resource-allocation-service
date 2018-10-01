da0db0e8cd74f2b716ba5f995721f7af424a48e5
wassoares <wlfsoares@gmail.com>
2018-10-01 10:26:26 -0300

    Final implementation in the OpenNebulaNetworkPlugin class

--- src/main/java/org/fogbowcloud/ras/core/plugins/interoperability/opennebula/network/v5_4/OpenNebulaNetworkPlugin.java
+++ src/main/java/org/fogbowcloud/ras/core/plugins/interoperability/opennebula/network/v5_4/OpenNebulaNetworkPlugin.java
@@ -1,6 +1,11 @@
 package org.fogbowcloud.ras.core.plugins.interoperability.opennebula.network.v5_4;
 
+import java.util.Properties;
+
 import org.apache.log4j.Logger;
+import org.fogbowcloud.ras.core.HomeDir;
+import org.fogbowcloud.ras.core.constants.DefaultConfigurationConstants;
+import org.fogbowcloud.ras.core.constants.Messages;
 import org.fogbowcloud.ras.core.exceptions.FogbowRasException;
 import org.fogbowcloud.ras.core.exceptions.UnexpectedException;
 import org.fogbowcloud.ras.core.models.instances.InstanceState;
@@ -10,6 +15,7 @@ import org.fogbowcloud.ras.core.models.orders.NetworkOrder;
 import org.fogbowcloud.ras.core.models.tokens.Token;
 import org.fogbowcloud.ras.core.plugins.interoperability.NetworkPlugin;
 import org.fogbowcloud.ras.core.plugins.interoperability.opennebula.OpenNebulaClientFactory;
+import org.fogbowcloud.ras.util.PropertiesUtil;
 import org.opennebula.client.Client;
 import org.opennebula.client.OneResponse;
 import org.opennebula.client.vnet.VirtualNetwork;
@@ -18,13 +24,22 @@ public class OpenNebulaNetworkPlugin implements NetworkPlugin<Token> {
 
 	private static final Logger LOGGER = Logger.getLogger(OpenNebulaNetworkPlugin.class);
 
+	private static final String DEFAULT_NETWORK_BRIDGE = "default_network_bridge";
+	private static final String DEFAULT_NETWORK_DESCRIPTION = "Virtual network created by %s.";
+	private static final String DEFAULT_NETWORK_TYPE = "RANGED";
+	private static final String NETWORK_ADDRESS_RANGE_SYZE = "256";
+	private static final String NETWORK_ADDRESS_RANGE_TYPE = "IP4";
 	private static final String TEMPLATE_NETWORK_ADDRESS_PATH = "TEMPLATE/NETWORK_ADDRESS";
 	private static final String TEMPLATE_NETWORK_GATEWAY_PATH = "TEMPLATE/NETWORK_GATEWAY";
 	private static final String TEMPLATE_VLAN_ID_PATH = "TEMPLATE/VLAN_ID";
 	
 	private OpenNebulaClientFactory factory;
+	private String bridge;
 	
 	public OpenNebulaNetworkPlugin() {
+		Properties properties = PropertiesUtil
+				.readProperties(HomeDir.getPath() + DefaultConfigurationConstants.OPENNEBULA_CONF_FILE_NAME);
+		this.bridge = properties.getProperty(DEFAULT_NETWORK_BRIDGE);
 		this.factory = new OpenNebulaClientFactory();
 	}
 
@@ -32,18 +47,42 @@ public class OpenNebulaNetworkPlugin implements NetworkPlugin<Token> {
 	public String requestInstance(NetworkOrder networkOrder, Token localUserAttributes)
 			throws FogbowRasException, UnexpectedException {
 		
-		LOGGER.debug("Requesting network instance with token=" + localUserAttributes.getTokenValue()); // FIXME
+		LOGGER.info(String.format(Messages.Info.REQUESTING_INSTANCE, localUserAttributes.getTokenValue()));
 		Client client = this.factory.createClient(localUserAttributes.getTokenValue());
-		String template = "";
-		LOGGER.debug("The network instance will be allocated according to template: " + template); // FIXME
+		
+		String name = networkOrder.getName();
+		String description = String.format(DEFAULT_NETWORK_DESCRIPTION, localUserAttributes.getTokenValue());
+		String type = DEFAULT_NETWORK_TYPE;
+		String bridge = this.bridge;
+		String address = networkOrder.getAddress();
+		String gateway = networkOrder.getGateway();
+		String rangeType = NETWORK_ADDRESS_RANGE_TYPE;
+		String rangeIp = networkOrder.getAddress();
+		String rangeSize = NETWORK_ADDRESS_RANGE_SYZE;
+		
+		CreateNetworkRequest request = new CreateNetworkRequest.Builder()
+				.name(name)
+				.description(description)
+				.type(type)
+				.bridge(bridge)
+				.address(address)
+				.gateway(gateway)
+				.rangeType(rangeType)
+				.rangeIp(rangeIp)
+				.rangeSize(rangeSize)
+				.build();
+		
+		String template = request.getVirtualNetwork().generateTemplate();
 		return this.factory.allocateVirtualNetwork(client, template);
 	}
 
 	@Override
 	public NetworkInstance getInstance(String networkInstanceId, Token localUserAttributes)
 			throws FogbowRasException, UnexpectedException {
+
+		LOGGER.info(
+				String.format(Messages.Info.GETTING_INSTANCE, networkInstanceId, localUserAttributes.getTokenValue()));
 		
-		LOGGER.info("Getting network instance ID=" + networkInstanceId + " and token=" + localUserAttributes.getTokenValue()); // FIXME
 		Client client = this.factory.createClient(localUserAttributes.getTokenValue());
 		VirtualNetwork virtualNetwork = this.factory.createVirtualNetwork(client, networkInstanceId);
 		return createInstance(virtualNetwork);
@@ -52,14 +91,16 @@ public class OpenNebulaNetworkPlugin implements NetworkPlugin<Token> {
 	@Override
 	public void deleteInstance(String networkInstanceId, Token localUserAttributes)
 			throws FogbowRasException, UnexpectedException {
+
+		LOGGER.info(
+				String.format(Messages.Info.DELETING_INSTANCE, networkInstanceId, localUserAttributes.getTokenValue()));
 		
-		LOGGER.info("Removing network instance ID=" + networkInstanceId + " and token=" + localUserAttributes.getTokenValue()); // FIXME
 		Client client = this.factory.createClient(localUserAttributes.getTokenValue());
 		VirtualNetwork virtualNetwork = this.factory.createVirtualNetwork(client, networkInstanceId);
 		OneResponse response = virtualNetwork.delete();
 		if (response.isError()) {
-			LOGGER.error("Error occurred while trying to delete network instance with ID=" 
-					+ networkInstanceId + "; " + response.getErrorMessage()); // FIXME
+			LOGGER.error(
+					String.format(Messages.Error.ERROR_WHILE_REMOVING_VN, networkInstanceId, response.getMessage()));
 		}
 	}
 
