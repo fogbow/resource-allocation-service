de9331bf24c89c12b88a6a8a67e401c83e9f9560
wassoares <wlfsoares@gmail.com>
2018-10-08 12:21:38 -0300

    Created the CreatePubliIpRequest class

---
+++ src/main/java/org/fogbowcloud/ras/core/plugins/interoperability/opennebula/publicip/v5_4/CreatePublicIpRequest.java
@@ -0,0 +1,11 @@
+package org.fogbowcloud.ras.core.plugins.interoperability.opennebula.publicip.v5_4;
+
+public class CreatePublicIpRequest {
+
+	private VirtualRouterTemplate virtualRouter;
+
+	public VirtualRouterTemplate getVirtualRouter() {
+		return this.virtualRouter;
+	}
+	
+}
