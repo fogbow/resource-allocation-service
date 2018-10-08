907efd94a43968565fe080af794bf07a58c1aa87
wassoares <wlfsoares@gmail.com>
2018-10-08 12:23:06 -0300

    Created the VirtualRouterTemplate class

---
+++ src/main/java/org/fogbowcloud/ras/core/plugins/interoperability/opennebula/publicip/v5_4/VirtualRouterTemplate.java
@@ -0,0 +1,7 @@
+package org.fogbowcloud.ras.core.plugins.interoperability.opennebula.publicip.v5_4;
+
+import org.fogbowcloud.ras.core.plugins.interoperability.opennebula.OpenNebulaRequestTemplate;
+
+public class VirtualRouterTemplate extends OpenNebulaRequestTemplate {
+
+}
