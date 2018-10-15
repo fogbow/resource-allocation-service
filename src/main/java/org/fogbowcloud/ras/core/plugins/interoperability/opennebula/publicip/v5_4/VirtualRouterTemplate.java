5ede4b93e8459617c75ea242060c49c09b3ff4dc
wassoares <wlfsoares@gmail.com>
2018-10-15 13:38:07 -0300

    Removed the VirtualRouterTemplate class in the opennebula.plublicip.v5_4 package

--- src/main/java/org/fogbowcloud/ras/core/plugins/interoperability/opennebula/publicip/v5_4/VirtualRouterTemplate.java
+++
@@ -1,7 +0,0 @@
-package org.fogbowcloud.ras.core.plugins.interoperability.opennebula.publicip.v5_4;
-
-import org.fogbowcloud.ras.core.plugins.interoperability.opennebula.OpenNebulaRequestTemplate;
-
-public class VirtualRouterTemplate extends OpenNebulaRequestTemplate {
-
-}
