4d4650d7c18b86fc4be96522ef2a35f76adae4d5
wassoares <wlfsoares@gmail.com>
2018-10-15 13:43:42 -0300

    Implemented the CreateNicRequest class

--- src/main/java/org/fogbowcloud/ras/core/plugins/interoperability/opennebula/publicip/v5_4/CreateNicRequest.java
+++ src/main/java/org/fogbowcloud/ras/core/plugins/interoperability/opennebula/publicip/v5_4/CreateNicRequest.java
@@ -1,11 +1,29 @@
 package org.fogbowcloud.ras.core.plugins.interoperability.opennebula.publicip.v5_4;
 
-public class CreatePublicIpRequest {
+public class CreateNicRequest {
 
-	private VirtualRouterTemplate virtualRouter;
+	private NicTemplate nic;
 
-	public VirtualRouterTemplate getVirtualRouter() {
-		return this.virtualRouter;
+	public NicTemplate getNic() {
+		return this.nic;
 	}
-	
+
+	public CreateNicRequest(Builder builder) {
+		String nicId = builder.nicId;
+		this.nic = new NicTemplate(nicId);
+	}
+
+	public static class Builder {
+
+		private String nicId;
+
+		public Builder nicId(String nicId) {
+			this.nicId = nicId;
+			return this;
+		}
+
+		public CreateNicRequest build() {
+			return new CreateNicRequest(this);
+		}
+	}
 }
