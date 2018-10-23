83c6bb078d907e4b96b7dfc72e2de044d51e2e7d
fubica <fubica@dsc.ufcg.edu.br>
2018-10-23 18:12:43 -0300

    Updated API documentation

--- src/main/java/org/fogbowcloud/ras/core/constants/ApiDocumentation.java
+++ src/main/java/org/fogbowcloud/ras/core/constants/ApiDocumentation.java
@@ -1,9 +1,6 @@
 package org.fogbowcloud.ras.core.constants;
 
 public class ApiDocumentation {
-
-    public static final double MAX_LINE_SIZE = 100;
-
     public static class Attachment {
         public static final String API = "Manages attachments.";
         public static final String CREATE_OPERATION = "Creates an attachment.";
@@ -14,8 +11,9 @@ public class ApiDocumentation {
 
         public static final String CREATE_REQUEST_BODY =
                 "The body of the request must specify the ID of the volume (volumeId) and the ID of the\n" +
-                "compute instance (computeId) where the volume will be attached; device specifies the\n" +
-                "name of the device that will be created in the compute instance.";
+                "compute instance (computeId) where the volume will be attached; Optionally, it can specify\n" +
+                "the name of the device that is going to be created in the compute, and the provider where\n" +
+                "the attachment will be created.";
     }
 
     public static class Compute {
@@ -29,11 +27,13 @@ public class ApiDocumentation {
         public static final String ID = "The ID of the specific compute instance.";
 
         public static final String CREATE_REQUEST_BODY =
-            "The body of the request must specify the disk size in GB (disk), the image id (imageId)\n"
-                + "that is going to be used to create the compute, the amount of memory (in MB) (memory),\n"
-                + "the name of the compute, an optional array of private network ids that were\n"
-                + "previously created using RAS, the public key (publicKey) that is going to be used\n"
-                + "to ssh into the compute and the amount of vCpus (vCPU) that the compute will have.";
+                "The body of the request must specify the amount of vCpus (vCPU), the amount of memory (in MB)\n" +
+                "(memory), the disk size in GB (disk) that is required, the image id (imageId) that is going to\n" +
+                "be used to create the compute, and optionally, the provider (provider) where the compute should\n" +
+                "be created, a name (name) that can be assigned to the compute, an array of private network ids\n" +
+                "(networkIds) that were previously created using RAS, the public key (publicKey) that is going to\n" +
+                "be used to allow remote connection to the compute, and information (userData) for the customization\n" +
+                "of the compute through the cloudinit mechanism.";
     }
 
     public static class Image {
@@ -50,19 +50,11 @@ public class ApiDocumentation {
         public static final String GET_BY_ID_OPERATION = "Lists a specific private network.";
         public static final String DELETE_OPERATION = "Deletes a specific private network.";
         public static final String ID = "The ID of the specific private network.";
-
-        /**
-         * {
-         "address": "string",
-         "allocation": "dynamic",
-         "gateway": "string",
-         "name": "string"
-         }
-         */
         public static final String CREATE_REQUEST_BODY =
-            "The body of the request must specify the CIDR of the network to be created (address),\n"
-                + "the allocation of the network (dynamic or static), the gateway address of the\n"
-                + "network (gateway) and the name (name) of the network.";
+                "The body of the request must specify the CIDR of the network to be created (cidr),\n" +
+                "the allocation mode of the network (dynamic or static), the gateway address of the\n" +
+                "network (gateway) and optionally, the provider (provider) where the network should\n" +
+                "be created, and a name (name) to be assigned to the network.";
     }
 
     public static class PublicIp {
@@ -72,19 +64,19 @@ public class ApiDocumentation {
         public static final String GET_BY_ID_OPERATION = "Lists a specific public IP.";
         public static final String DELETE_OPERATION = "Deletes a specific public IP.";
         public static final String ID = "The ID of the specific public IP.";
-
         public static final String CREATE_REQUEST_BODY =
-            "The body of the request must specify the ID of the compute (computeOrderId) this IP\n"
-                + "will be assigned to.";
+                "The body of the request must specify the ID of the compute (computeId)\n" +
+                "to which this IP will be assigned.";
     }
 
     public static class Token {
         public static final String API = "Creates tokens for the users of the federation.";
         public static final String CREATE_OPERATION = "Creates a token.";
         public static final String CREATE_REQUEST_BODY =
-                "The body of the request must specify the values and keys for the type of IdentityPlugin\n"
-                    + "your RAS deploy is using. For instance, if you are using LDAP, you should provide\n"
-                    + "a username and a password.";
+                "The body of the request must specify the values and keys for the type of IdentityPlugin\n" +
+                "your RAS deploy is using. For instance, if you are using LDAP, you should provide a username\n" +
+                "and a password. For Openstack Keystone, you should provide a username, a password, a domain\n" +
+                "and a project name (projectName).";
     }
 
     public static class Version {
@@ -99,16 +91,16 @@ public class ApiDocumentation {
         public static final String GET_BY_ID_OPERATION = "Lists a specific volume.";
         public static final String DELETE_OPERATION = "Deletes a specific volume.";
         public static final String ID = "The ID of the specific volume.";
-
         public static final String CREATE_REQUEST_BODY =
-            "The body of the request must specify the size (volumeSize) of the volume to be"
-                + "created and a name (name).";
+                "The body of the request must specify the size (volumeSize) of the volume to be\n" +
+                "created and, optionally, the provider (provider) where the volume should be\n" +
+                "created and a name (name) to be assigned to the volume.";
     }
 
     public static class CommonParameters {
-        public static final String FEDERATION_TOKEN = "This is the token that identifies a federation user." +
+        public static final String FEDERATION_TOKEN = "This is the token that identifies a federation user.\n" +
                                                       "It is typically created via a call to the /tokens endpoint.";
-        public static final String MEMBER_ID = "The ID of the specific provider";
+        public static final String MEMBER_ID = "The ID of the specific provider.";
     }
 
 }
