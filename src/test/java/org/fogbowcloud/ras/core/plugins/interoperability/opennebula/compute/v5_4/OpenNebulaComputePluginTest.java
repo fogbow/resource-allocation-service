715cfc5625eaf8f8036af6e5e15377050c1fa149
wassoares <wlfsoares@gmail.com>
2018-10-24 08:46:17 -0300

    Creating test implementation from the OpenNebulaComputePluginTest class

--- src/test/java/org/fogbowcloud/ras/core/plugins/interoperability/opennebula/compute/v5_4/OpenNebulaComputePluginTest.java
+++ src/test/java/org/fogbowcloud/ras/core/plugins/interoperability/opennebula/compute/v5_4/OpenNebulaComputePluginTest.java
@@ -4,16 +4,21 @@ import java.util.Properties;
 
 import org.fogbowcloud.ras.core.HomeDir;
 import org.fogbowcloud.ras.core.constants.DefaultConfigurationConstants;
+import org.fogbowcloud.ras.core.exceptions.UnexpectedException;
 import org.fogbowcloud.ras.core.plugins.interoperability.opennebula.OpenNebulaClientFactory;
 import org.fogbowcloud.ras.util.PropertiesUtil;
 import org.junit.Before;
 import org.junit.Test;
+import org.opennebula.client.ClientConfigurationException;
 
 public class OpenNebulaComputePluginTest {
 
+	private static final String DEFAULT_NETWORK_ID_KEY = "default_network_id";
+	
 	private OpenNebulaClientFactory factory;
 	private OpenNebulaComputePlugin plugin;
 	private Properties properties;
+	private String networkId;
 	
 	@Before
 	public void setUp() {
@@ -22,10 +27,32 @@ public class OpenNebulaComputePluginTest {
 
 		this.factory = new OpenNebulaClientFactory();
 		this.plugin = new OpenNebulaComputePlugin();
+		this.networkId = this.properties.getProperty(DEFAULT_NETWORK_ID_KEY);
 	}
 	
 	@Test
-	public void test() {
+	public void test() throws UnexpectedException, ClientConfigurationException {
+
+		//		Mockito.doNothing().when(this.factory.createClient(Mockito.anyString()));
+
+		CreateComputeRequest request = new CreateComputeRequest.Builder()
+				.contextEncoding("base64")
+				.contextUserdata("")
+				.contextNetwork("YES")
+				.cpu("1")
+				.graphicsListen("0.0.0.0")
+				.graphicsType("vnc")
+				.imageId("2")
+				.volumeSize("4")
+				.volumeType("fs")
+				.memory("2048")
+				.networkId(this.networkId)
+				.build();
+
+		String template = request.getVirtualMachine().generateTemplate();
 		
+		System.out.println(template);
+		
+		//		Mockito.verify(this.factory, Mockito.times(1)).createClient(anyString());
 	}
 }
