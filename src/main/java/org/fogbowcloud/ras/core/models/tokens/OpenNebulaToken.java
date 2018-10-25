cd0aee1423cc5853f391f0a7c876e60eec9a8164
Anderson Dantas <a.kleber.d@gmail.com>
2018-10-25 16:58:28 -0300

    Changed OpenNebulaToken parameters

--- src/main/java/org/fogbowcloud/ras/core/models/tokens/OpenNebulaToken.java
+++ src/main/java/org/fogbowcloud/ras/core/models/tokens/OpenNebulaToken.java
@@ -2,22 +2,18 @@ package org.fogbowcloud.ras.core.models.tokens;
 
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 
 @Entity
 public class OpenNebulaToken extends FederationUserToken {
 
-	@ElementCollection
-	private List<Integer> groupIds;
+	@Column
+	private String signature;
 	
-	public OpenNebulaToken(String provider, String tokenValue, String userName, List<Integer> groupIds) {
+	public OpenNebulaToken(String provider, String tokenValue, String userName, String signature) {
 		super(provider, tokenValue, userName, userName);
-		this.groupIds = groupIds;
+		this.signature = signature;
 	}
-
-	public List<Integer> getGroupIds() {
-		return groupIds;
-	}
-	
 }
