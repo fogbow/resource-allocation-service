d7a80aef9e92fca0590f3bdce88395c0941bed29
Anderson Dantas <a.kleber.d@gmail.com>
2018-10-25 18:09:28 -0300

    Implemented OpenNebula AllToOneMapper

---
+++ src/main/java/org/fogbowcloud/ras/core/plugins/aaa/mapper/all2one/OpenNebulaAllToOneMapper.java
@@ -0,0 +1,24 @@
+package org.fogbowcloud.ras.core.plugins.aaa.mapper.all2one;
+
+import org.fogbowcloud.ras.core.exceptions.FogbowRasException;
+import org.fogbowcloud.ras.core.exceptions.UnexpectedException;
+import org.fogbowcloud.ras.core.models.tokens.FederationUserToken;
+import org.fogbowcloud.ras.core.models.tokens.Token;
+import org.fogbowcloud.ras.core.plugins.aaa.mapper.FederationToLocalMapperPlugin;
+import org.fogbowcloud.ras.core.plugins.aaa.tokengenerator.opennebula.OpenNebulaTokenGeneratorPlugin;
+import org.fogbowcloud.ras.core.plugins.aaa.identity.opennebula.OpenNebulaIdentityPlugin;
+
+public class OpenNebulaAllToOneMapper implements FederationToLocalMapperPlugin {
+    private GenericAllToOneFederationToLocalMapper genericMapper;
+
+
+    public OpenNebulaAllToOneMapper() {
+        this.genericMapper = new GenericAllToOneFederationToLocalMapper(new OpenNebulaTokenGeneratorPlugin(),
+                new OpenNebulaIdentityPlugin(), "opennebula-mapper.conf");
+    }
+
+    @Override
+    public Token map(FederationUserToken token) throws UnexpectedException, FogbowRasException {
+        return genericMapper.map(token);
+    }
+}
