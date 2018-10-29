e40dc23099b058e1357b32b61513a2cdbf177dbf
wassoares <wlfsoares@gmail.com>
2018-10-29 15:14:42 -0300

    Fixes made in the OpenNebulaIdentityPlugin class

--- src/main/java/org/fogbowcloud/ras/core/plugins/aaa/identity/opennebula/OpenNebulaIdentityPlugin.java
+++ src/main/java/org/fogbowcloud/ras/core/plugins/aaa/identity/opennebula/OpenNebulaIdentityPlugin.java
@@ -1,26 +1,22 @@
 package org.fogbowcloud.ras.core.plugins.aaa.identity.opennebula;
 
-import org.apache.log4j.Logger;
 import org.fogbowcloud.ras.core.constants.Messages;
 import org.fogbowcloud.ras.core.exceptions.InvalidParameterException;
-import org.fogbowcloud.ras.core.exceptions.UnexpectedException;
 import org.fogbowcloud.ras.core.models.tokens.OpenNebulaToken;
 import org.fogbowcloud.ras.core.plugins.aaa.identity.FederationIdentityPlugin;
 import org.fogbowcloud.ras.core.plugins.interoperability.opennebula.OpenNebulaClientFactory;
-import org.opennebula.client.Client;
-import org.opennebula.client.ClientConfigurationException;
 
 public class OpenNebulaIdentityPlugin implements FederationIdentityPlugin<OpenNebulaToken> {
-    private static final Logger LOGGER = Logger.getLogger(OpenNebulaIdentityPlugin.class);
-
+    
     private static final String OPENNEBULA_FIELD_SEPARATOR = "#&#";
+    private static final String EMPTY_SPACE = "";
 
     public OpenNebulaIdentityPlugin() {
     }
 
     @Override
     public OpenNebulaToken createToken(String tokenValue) throws InvalidParameterException {
-        if(tokenValue == null || tokenValue.trim().equals("")){
+        if(tokenValue == null || tokenValue.trim().equals(EMPTY_SPACE)){
             throw new InvalidParameterException(String.format(Messages.Error.INVALID_TOKEN_VALUE, Messages.Error.EMPTY_OR_NULL_TOKEN));
         }
 
@@ -28,19 +24,19 @@ public class OpenNebulaIdentityPlugin implements FederationIdentityPlugin<OpenNe
 
         String provider = split[0];
         String oneTokenValue = split[1];
-        String userName = split[2];
-        String signature = split[3];
+        String userId = split[2];
+        String userName = split[3];
+        String signature = split[4];
 
         try {
             OpenNebulaClientFactory factory = new OpenNebulaClientFactory();
 
             // Test if oneTokenValue is valid for any authentication.
-            Client client = factory.createClient(oneTokenValue);
+            factory.createClient(oneTokenValue);
         } catch (Exception e){
-
             throw new InvalidParameterException(e.getMessage());
         }
 
-        return new OpenNebulaToken(provider, oneTokenValue, userName, signature);
+        return new OpenNebulaToken(provider, oneTokenValue, userId, userName, signature);
     }
 }
