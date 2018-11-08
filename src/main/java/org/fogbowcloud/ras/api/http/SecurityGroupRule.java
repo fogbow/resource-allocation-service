cc4f5c708e3626f933c84648767a15858ddad8fb
Gustavo Yamaguchi <gustavoyama@gmail.com>
2018-11-08 17:35:57 -0300

    Changing API to fit the new API discussed

--- src/main/java/org/fogbowcloud/ras/api/http/SecurityGroupRule.java
+++ src/main/java/org/fogbowcloud/ras/api/http/SecurityGroupRule.java
@@ -13,22 +13,13 @@ public class SecurityGroupRule {
     Logger LOGGER = Logger.getLogger(SecurityGroupRule.class);
 
     public static final String SECURITY_GROUP_ENDPOINT = "securityGroups";
-    public static final String SECURITY_GROUP_RULE_ENDPOINT = "securityGroupRules";
+    public static final String RULE_ENDPOINT = "rules";
     public static final String FEDERATION_TOKEN_VALUE_HEADER_KEY = "federationTokenValue";
     public static final String ORDER_CONTROLLER_TYPE = "security group rule";
 
-    @RequestMapping(value = "/{orderId}/" + SECURITY_GROUP_RULE_ENDPOINT, method = RequestMethod.GET)
-    public ResponseEntity<SecurityGroupRule> getAllSecurityGroupRules (
-            @PathVariable String orderId,
-            @RequestHeader(required = false, value = FEDERATION_TOKEN_VALUE_HEADER_KEY) String federationTokenValue) {
-
-        LOGGER.info(String.format(Messages.Info.RECEIVING_GET_ALL_REQUEST, ORDER_CONTROLLER_TYPE));
-
-        throw new UnsupportedOperationException();
-    }
-
-    @RequestMapping(value = "/{orderId}/" + SECURITY_GROUP_RULE_ENDPOINT, method = RequestMethod.POST)
+    @RequestMapping(value = "/" + RULE_ENDPOINT, method = RequestMethod.POST)
     public ResponseEntity<SecurityGroupRule> createSecurityGroupRule (
+            @RequestBody String orderId,
             @RequestBody SecurityGroupRule securityGroupRule,
             @RequestHeader(required = false, value = FEDERATION_TOKEN_VALUE_HEADER_KEY) String federationTokenValue) {
 
@@ -37,13 +28,22 @@ public class SecurityGroupRule {
         throw new UnsupportedOperationException();
     }
 
-    @RequestMapping(value = "/{orderId}/" + SECURITY_GROUP_RULE_ENDPOINT, method = RequestMethod.POST)
-    public ResponseEntity<SecurityGroupRule> deleteSecurityGroupRule (
-            @PathVariable String orderId,
-            @PathVariable String securityGroupId,
+    @RequestMapping(value = "/" + RULE_ENDPOINT, method = RequestMethod.GET)
+    public ResponseEntity<SecurityGroupRule> getAllSecurityGroupRules (
+            @RequestParam("orderId") String orderId,
             @RequestHeader(required = false, value = FEDERATION_TOKEN_VALUE_HEADER_KEY) String federationTokenValue) {
 
-        LOGGER.info(String.format(Messages.Info.RECEIVING_DELETE_REQUEST, ORDER_CONTROLLER_TYPE, securityGroupId));
+        LOGGER.info(String.format(Messages.Info.RECEIVING_GET_ALL_REQUEST, ORDER_CONTROLLER_TYPE));
+
+        throw new UnsupportedOperationException();
+    }
+
+    @RequestMapping(value = "/" + RULE_ENDPOINT + "/{ruleId}", method = RequestMethod.POST)
+    public ResponseEntity<SecurityGroupRule> deleteSecurityGroupRule (
+            @PathVariable String ruleId,
+            @RequestHeader(required = false, value = FEDERATION_TOKEN_VALUE_HEADER_KEY) String federationTokenValue) {
+
+        LOGGER.info(String.format(Messages.Info.RECEIVING_DELETE_REQUEST, ORDER_CONTROLLER_TYPE, ruleId));
 
         throw new UnsupportedOperationException();
     }
