6e052d726415c1a35c36c4ac9866703a52bb036b
wassoares <wlfsoares@gmail.com>
2018-11-14 14:38:53 -0300

    Fixes made in the NicTemplate class

--- src/main/java/org/fogbowcloud/ras/core/plugins/interoperability/opennebula/publicip/v5_4/NicTemplate.java
+++ src/main/java/org/fogbowcloud/ras/core/plugins/interoperability/opennebula/publicip/v5_4/NicTemplate.java
@@ -2,54 +2,45 @@ package org.fogbowcloud.ras.core.plugins.interoperability.opennebula.publicip.v5
 
 import static org.fogbowcloud.ras.core.plugins.interoperability.opennebula.OpenNebulaTagNameConstants.NETWORK_ID;
 import static org.fogbowcloud.ras.core.plugins.interoperability.opennebula.OpenNebulaTagNameConstants.NETWORK_INTERFACE_CONNECTED;
-import static org.fogbowcloud.ras.core.plugins.interoperability.opennebula.OpenNebulaTagNameConstants.NIC_ID;
 import static org.fogbowcloud.ras.core.plugins.interoperability.opennebula.OpenNebulaTagNameConstants.SECURITY_GROUPS;
+import static org.fogbowcloud.ras.core.plugins.interoperability.opennebula.OpenNebulaTagNameConstants.TEMPLATE;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import org.fogbowcloud.ras.core.plugins.interoperability.opennebula.OpenNebulaMarshallerTemplate;
 
-@XmlRootElement(name = NETWORK_INTERFACE_CONNECTED)
-public class NetworkInterfaceConnected extends OpenNebulaMarshallerTemplate {
+@XmlRootElement(name = TEMPLATE)
+public class NicTemplate extends OpenNebulaMarshallerTemplate {
 
-	private String nicId;
-	private String networkId;
-	private String securityGroups;
-
-	public NetworkInterfaceConnected() {}
-
-	public NetworkInterfaceConnected(String nicId, String networkId, String securityGroups) {
-		this.nicId = nicId;
-		this.networkId = networkId;
-		this.securityGroups = securityGroups;
-	}
-
-	@XmlElement(name = NIC_ID)
-	public String getNicId() {
-		return nicId;
-	}
-
-	public void setNicId(String nicId) {
-		this.nicId = nicId;
-	}
+	private NetworkInterfaceConnected nic;
 	
-	@XmlElement(name = NETWORK_ID)
-	public String getNetworkId() {
-		return networkId;
+	public NicTemplate() {}
+	
+	public NicTemplate(String networkId, String securityGroups) {
+		this.nic = new NetworkInterfaceConnected();
+		this.nic.networkId = networkId;
+		this.nic.securityGroups = securityGroups;
 	}
 
-	public void setNetworkId(String networkId) {
-		this.networkId = networkId;
+	@XmlElement(name = NETWORK_INTERFACE_CONNECTED)
+	public NetworkInterfaceConnected getNic() {
+		return nic;
 	}
 
-	@XmlElement(name = SECURITY_GROUPS)
-	public String getSecurityGroups() {
-		return securityGroups;
+	public static class NetworkInterfaceConnected {
+		
+		private String networkId;
+		private String securityGroups;
+		
+		@XmlElement(name = NETWORK_ID)
+		public String getNetworkId() {
+			return networkId;
+		}
+		
+		@XmlElement(name = SECURITY_GROUPS)
+		public String getSecurityGroups() {
+			return securityGroups;
+		}
 	}
-
-	public void setSecurityGroups(String securityGroups) {
-		this.securityGroups = securityGroups;
-	}
-
 }
