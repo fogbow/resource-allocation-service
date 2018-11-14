06a1919c0ccaeedc8a1273091f7788759f50765a
wassoares <wlfsoares@gmail.com>
2018-11-14 10:00:42 -0300

    Added constant in the Error internal classes of Messages

--- src/main/java/org/fogbowcloud/ras/core/constants/Messages.java
+++ src/main/java/org/fogbowcloud/ras/core/constants/Messages.java
@@ -153,5 +153,6 @@ public class Messages {
         public static final String UNEXPECTED_JOB_STATUS = "Job status must be one of {0, 1, 2}.";
         public static final String UNSPECIFIED_PROJECT_ID = "Unspecified projectId.";
         public static final String EMPTY_OR_NULL_TOKEN = "A null or empty token was received";
+		public static final String ERROR_WHILE_CREATING_SECURITY_GROUPS = "Error while creating a security groups from template: %s.";
     }
 }
