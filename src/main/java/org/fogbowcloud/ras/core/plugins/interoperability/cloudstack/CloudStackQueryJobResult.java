95c744abbfc1a8aad70dea78b7ac6c184f163c7c
Gustavo Yamaguchi <gustavoyama@gmail.com>
2018-11-21 16:22:18 -0300

    Init create security rule method for cloudstack plugin

---
+++ src/main/java/org/fogbowcloud/ras/core/plugins/interoperability/cloudstack/CloudStackQueryJobResult.java
@@ -0,0 +1,35 @@
+package org.fogbowcloud.ras.core.plugins.interoperability.cloudstack;
+
+import org.apache.http.client.HttpResponseException;
+import org.fogbowcloud.ras.core.exceptions.FogbowRasException;
+import org.fogbowcloud.ras.core.models.tokens.Token;
+import org.fogbowcloud.ras.core.plugins.interoperability.cloudstack.publicip.v4_9.QueryAsyncJobResultRequest;
+import org.fogbowcloud.ras.util.connectivity.HttpRequestClientUtil;
+
+public class CloudStackQueryJobResult {
+
+    public static final int PROCESSING = 0;
+    public static final int SUCCESS = 1;
+    public static final int FAILURE = 2;
+
+    public static String getQueryJobResult(HttpRequestClientUtil client, String jobId, Token cloudStackToken)
+            throws FogbowRasException {
+        QueryAsyncJobResultRequest queryAsyncJobResultRequest = new QueryAsyncJobResultRequest.Builder()
+                .jobId(jobId)
+                .build();
+
+        CloudStackUrlUtil
+                .sign(queryAsyncJobResultRequest.getUriBuilder(), cloudStackToken.getTokenValue());
+
+        String jsonResponse = null;
+        try {
+            jsonResponse = client
+                    .doGetRequest(queryAsyncJobResultRequest.getUriBuilder().toString(),
+                            cloudStackToken);
+        } catch (HttpResponseException e) {
+            CloudStackHttpToFogbowRasExceptionMapper.map(e);
+        }
+
+        return jsonResponse;
+    }
+}
--- src/main/java/org/fogbowcloud/ras/core/plugins/interoperability/cloudstack/securityrule/v4_9/CloudStackSecurityRulePlugin.java
+++ src/main/java/org/fogbowcloud/ras/core/plugins/interoperability/cloudstack/securityrule/v4_9/CloudStackSecurityRulePlugin.java
@@ -1,18 +1,68 @@
 package org.fogbowcloud.ras.core.plugins.interoperability.cloudstack.securityrule.v4_9;
 
+import org.apache.http.client.HttpResponseException;
 import org.fogbowcloud.ras.core.exceptions.FogbowRasException;
 import org.fogbowcloud.ras.core.exceptions.UnexpectedException;
 import org.fogbowcloud.ras.core.models.orders.Order;
+import org.fogbowcloud.ras.core.models.securitygroups.Direction;
+import org.fogbowcloud.ras.core.models.securitygroups.EtherType;
 import org.fogbowcloud.ras.core.models.securitygroups.SecurityGroupRule;
 import org.fogbowcloud.ras.core.models.tokens.Token;
 import org.fogbowcloud.ras.core.plugins.interoperability.SecurityGroupPlugin;
+import org.fogbowcloud.ras.core.plugins.interoperability.cloudstack.CloudStackHttpToFogbowRasExceptionMapper;
+import org.fogbowcloud.ras.core.plugins.interoperability.cloudstack.CloudStackQueryAsyncJobResponse;
+import org.fogbowcloud.ras.core.plugins.interoperability.cloudstack.CloudStackQueryJobResult;
+import org.fogbowcloud.ras.core.plugins.interoperability.cloudstack.CloudStackUrlUtil;
+import org.fogbowcloud.ras.core.plugins.interoperability.cloudstack.publicip.v4_9.CreateFirewallRuleAsyncResponse;
+import org.fogbowcloud.ras.core.plugins.interoperability.cloudstack.publicip.v4_9.CreateFirewallRuleRequest;
+import org.fogbowcloud.ras.util.connectivity.HttpRequestClientUtil;
 
 import java.util.List;
 
 public class CloudStackSecurityRulePlugin implements SecurityGroupPlugin {
+
+    private HttpRequestClientUtil client;
+
+    public CloudStackSecurityRulePlugin() {
+        this.client = new HttpRequestClientUtil();
+    }
+
     @Override
-    public String requestSecurityGroupRule(SecurityGroupRule securityGroupRule, Order majorOrder, Token localUserAttributes) throws FogbowRasException, UnexpectedException {
-        throw new UnsupportedOperationException();
+    public String requestSecurityGroupRule(SecurityGroupRule securityGroupRule, Order majorOrder,
+                                           Token localUserAttributes) throws FogbowRasException, UnexpectedException {
+        if (securityGroupRule.getDirection() == Direction.OUT) {
+            throw new UnsupportedOperationException();
+        }
+        if (securityGroupRule.getEtherType() == EtherType.IPv6) {
+            throw new UnsupportedOperationException();
+        }
+
+        String cidr = securityGroupRule.getCidr();
+        String portFrom = Integer.toString(securityGroupRule.getPortFrom());
+        String portTo = Integer.toString(securityGroupRule.getPortTo());
+        String protocol = securityGroupRule.getProtocol().toString();
+
+        CreateFirewallRuleRequest createFirewallRuleRequest = new CreateFirewallRuleRequest.Builder()
+                .protocol(protocol)
+                .startPort(portFrom)
+                .endPort(portTo)
+                .ipAddressId(cidr)
+                .build();
+
+        CloudStackUrlUtil.sign(createFirewallRuleRequest.getUriBuilder(), localUserAttributes.getTokenValue());
+
+        String jsonResponse = null;
+        try {
+            jsonResponse = this.client.doGetRequest(createFirewallRuleRequest.getUriBuilder().toString(), localUserAttributes);
+        } catch (HttpResponseException e) {
+            CloudStackHttpToFogbowRasExceptionMapper.map(e);
+        }
+
+        CreateFirewallRuleAsyncResponse response = CreateFirewallRuleAsyncResponse.fromJson(jsonResponse);
+
+        waitForJobResult(this.client, response.getJobId(), localUserAttributes);
+
+        return null;
     }
 
     @Override
@@ -23,5 +73,21 @@ public class CloudStackSecurityRulePlugin implements SecurityGroupPlugin {
     @Override
     public void deleteSecurityGroupRule(String securityGroupRuleId, Token localUserAttributes) throws FogbowRasException, UnexpectedException {
         throw new UnsupportedOperationException();
+    }
+
+    private void waitForJobResult(HttpRequestClientUtil client, String jobId, Token token) throws FogbowRasException {
+        String jsonResponse = CloudStackQueryJobResult.getQueryJobResult(client, jobId, token);
+        CloudStackQueryAsyncJobResponse queryAsyncJobResult = CloudStackQueryAsyncJobResponse.fromJson(jsonResponse);
+
+        switch (queryAsyncJobResult.getJobStatus()) {
+            case CloudStackQueryJobResult.PROCESSING:
+                break;
+            case CloudStackQueryJobResult.SUCCESS:
+                break;
+            case CloudStackQueryJobResult.FAILURE:
+                break;
+            default:
+                break;
+        }
     }
 }
