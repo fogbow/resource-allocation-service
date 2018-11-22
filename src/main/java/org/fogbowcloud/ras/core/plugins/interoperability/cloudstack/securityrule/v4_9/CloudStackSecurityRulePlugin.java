de611b6f19e4e1107736b6f8563fd77e61b57924
Francisco Germano de Araujo Neto <chicog@taioba.lsd.ufcg.edu.br>
2018-11-22 10:23:33 -0300

    Adding CloudStackSecurityRulePluginTest

--- src/main/java/org/fogbowcloud/ras/core/plugins/interoperability/cloudstack/securityrule/v4_9/CloudStackSecurityRulePlugin.java
+++ src/main/java/org/fogbowcloud/ras/core/plugins/interoperability/cloudstack/securityrule/v4_9/CloudStackSecurityRulePlugin.java
@@ -1,27 +1,27 @@
 package org.fogbowcloud.ras.core.plugins.interoperability.cloudstack.securityrule.v4_9;
 
+import java.util.List;
+
 import org.fogbowcloud.ras.core.exceptions.FogbowRasException;
 import org.fogbowcloud.ras.core.exceptions.UnexpectedException;
 import org.fogbowcloud.ras.core.models.orders.Order;
 import org.fogbowcloud.ras.core.models.securityrules.SecurityRule;
-import org.fogbowcloud.ras.core.models.tokens.Token;
+import org.fogbowcloud.ras.core.models.tokens.CloudStackToken;
 import org.fogbowcloud.ras.core.plugins.interoperability.SecurityRulePlugin;
 
-import java.util.List;
-
-public class CloudStackSecurityRulePlugin implements SecurityRulePlugin {
+public class CloudStackSecurityRulePlugin implements SecurityRulePlugin<CloudStackToken> {
     @Override
-    public String requestSecurityRule(SecurityRule securityRule, Order majorOrder, Token localUserAttributes) throws FogbowRasException, UnexpectedException {
+    public String requestSecurityRule(SecurityRule securityRule, Order majorOrder, CloudStackToken localUserAttributes) throws FogbowRasException, UnexpectedException {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public List<SecurityRule> getSecurityRules(Order majorOrder, Token localUserAttributes) throws FogbowRasException, UnexpectedException {
+    public List<SecurityRule> getSecurityRules(Order majorOrder, CloudStackToken localUserAttributes) throws FogbowRasException, UnexpectedException {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public void deleteSecurityRule(String securityRuleId, Token localUserAttributes) throws FogbowRasException, UnexpectedException {
+    public void deleteSecurityRule(String securityRuleId, CloudStackToken localUserAttributes) throws FogbowRasException, UnexpectedException {
         throw new UnsupportedOperationException();
     }
 }
---
+++ src/test/java/org/fogbowcloud/ras/core/plugins/interoperability/cloudstack/securityrule/v4_9/CloudStackSecurityRulePluginTest.java
@@ -0,0 +1,5 @@
+package org.fogbowcloud.ras.core.plugins.interoperability.cloudstack.securityrule.v4_9;
+
+public class CloudStackSecurityRulePluginTest {
+
+}
