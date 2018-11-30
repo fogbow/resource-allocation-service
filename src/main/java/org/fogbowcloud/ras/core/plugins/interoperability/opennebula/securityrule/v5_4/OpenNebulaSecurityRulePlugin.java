0d1b1fd038f701579807da259febdd1d837d4762
Gustavo Yamaguchi <gustavoyama@gmail.com>
2018-11-30 17:05:34 -0300

    Adding delete method for security group

--- src/main/java/org/fogbowcloud/ras/core/plugins/interoperability/opennebula/securityrule/v5_4/OpenNebulaSecurityRulePlugin.java
+++ src/main/java/org/fogbowcloud/ras/core/plugins/interoperability/opennebula/securityrule/v5_4/OpenNebulaSecurityRulePlugin.java
@@ -1,13 +1,18 @@
 package org.fogbowcloud.ras.core.plugins.interoperability.opennebula.securityrule.v5_4;
 
 import org.apache.log4j.Logger;
+import org.fogbowcloud.ras.core.constants.Messages;
 import org.fogbowcloud.ras.core.exceptions.FogbowRasException;
+import org.fogbowcloud.ras.core.exceptions.InvalidParameterException;
 import org.fogbowcloud.ras.core.exceptions.UnexpectedException;
 import org.fogbowcloud.ras.core.models.orders.Order;
 import org.fogbowcloud.ras.core.models.securityrules.SecurityRule;
 import org.fogbowcloud.ras.core.models.tokens.OpenNebulaToken;
 import org.fogbowcloud.ras.core.plugins.interoperability.SecurityRulePlugin;
+import org.fogbowcloud.ras.core.plugins.interoperability.opennebula.OpenNebulaClientFactory;
 import org.fogbowcloud.ras.util.connectivity.HttpRequestClientUtil;
+import org.opennebula.client.Client;
+import org.opennebula.client.secgroup.SecurityGroup;
 
 import java.util.List;
 
@@ -15,10 +20,10 @@ public class OpenNebulaSecurityRulePlugin implements SecurityRulePlugin<OpenNebu
 
     public static final Logger LOGGER = Logger.getLogger(OpenNebulaSecurityRulePlugin.class);
 
-    private HttpRequestClientUtil client;
+    private OpenNebulaClientFactory factory;
     
     public OpenNebulaSecurityRulePlugin() {
-        this.client = new HttpRequestClientUtil();
+        this.factory = new OpenNebulaClientFactory();
     }
 
     @Override
@@ -36,6 +41,16 @@ public class OpenNebulaSecurityRulePlugin implements SecurityRulePlugin<OpenNebu
     @Override
     public void deleteSecurityRule(String securityRuleId, OpenNebulaToken localUserAttributes)
             throws FogbowRasException, UnexpectedException {
-        throw new UnsupportedOperationException();
+        LOGGER.info(
+                String.format(Messages.Info.DELETING_INSTANCE, securityRuleId, localUserAttributes.getTokenValue()));
+        Client client = this.factory.createClient(localUserAttributes.getTokenValue());
+        int id;
+        try {
+            id = Integer.parseInt(securityRuleId);
+        } catch (Exception e) {
+            LOGGER.error(String.format(Messages.Error.ERROR_WHILE_CONVERTING_INSTANCE_ID, securityRuleId));
+            throw new InvalidParameterException(Messages.Exception.INVALID_PARAMETER);
+        }
+        SecurityGroup.delete(client, id);
     }
 }
