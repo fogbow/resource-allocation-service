cd5ec464797c16539d84e70513dc46a77d9dd16c
wassoares <wlfsoares@gmail.com>
2018-12-03 15:40:33 -0300

    Create the PublicNetwork class

---
+++ src/main/java/org/fogbowcloud/ras/core/plugins/interoperability/opennebula/publicip/v5_4/PublicNetwork.java
@@ -0,0 +1,88 @@
+package org.fogbowcloud.ras.core.plugins.interoperability.opennebula.publicip.v5_4;
+
+import static org.fogbowcloud.ras.core.plugins.interoperability.opennebula.OpenNebulaTagNameConstants.BRIDGE;
+import static org.fogbowcloud.ras.core.plugins.interoperability.opennebula.OpenNebulaTagNameConstants.IP;
+import static org.fogbowcloud.ras.core.plugins.interoperability.opennebula.OpenNebulaTagNameConstants.LEASES;
+import static org.fogbowcloud.ras.core.plugins.interoperability.opennebula.OpenNebulaTagNameConstants.NAME;
+import static org.fogbowcloud.ras.core.plugins.interoperability.opennebula.OpenNebulaTagNameConstants.TEMPLATE;
+import static org.fogbowcloud.ras.core.plugins.interoperability.opennebula.OpenNebulaTagNameConstants.TYPE;
+import static org.fogbowcloud.ras.core.plugins.interoperability.opennebula.OpenNebulaTagNameConstants.VIRTUAL_NETWORK_BRIDGED_DRIVE;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.fogbowcloud.ras.core.plugins.interoperability.opennebula.OpenNebulaMarshallerTemplate;
+
+@XmlRootElement(name = TEMPLATE)
+public class PublicNetwork extends OpenNebulaMarshallerTemplate{
+
+	private String name;
+	private String type;
+	private String bridge;
+	private String bridgedDrive;
+	private List<LeaseIp> leases;
+	
+	@XmlElement(name = NAME)
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@XmlElement(name = TYPE)
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@XmlElement(name = BRIDGE)
+	public String getBridge() {
+		return bridge;
+	}
+
+	public void setBridge(String bridge) {
+		this.bridge = bridge;
+	}
+
+	@XmlElement(name = VIRTUAL_NETWORK_BRIDGED_DRIVE)
+	public String getBridgedDrive() {
+		return bridgedDrive;
+	}
+
+	public void setBridgedDrive(String bridgedDrive) {
+		this.bridgedDrive = bridgedDrive;
+	}
+
+	@XmlElement(name = LEASES)
+	public List<LeaseIp> getLeases() {
+		return leases;
+	}
+
+	public void setLeases(List<LeaseIp> leases) {
+		this.leases = leases;
+	}
+	
+	public static LeaseIp allocateIpAddress(String ip) {
+		LeaseIp leaseIp = new LeaseIp();
+		leaseIp.ip = ip;
+		return leaseIp;
+	}
+	
+	@XmlRootElement(name = LEASES)
+	public static class LeaseIp {
+		
+		private String ip;
+
+		@XmlElement(name = IP)
+		public String getIp() {
+			return ip;
+		}
+	}
+}
