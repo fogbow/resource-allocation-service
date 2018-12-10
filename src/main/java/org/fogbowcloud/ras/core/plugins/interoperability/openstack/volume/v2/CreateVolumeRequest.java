3652db850f0e008a9ed15b86feb96e1fea863448
Anderson Dantas <a.kleber.d@gmail.com>
2018-12-10 18:06:19 -0300

    Added volume_type attribute to OpenStack's VolumeRequest

--- src/main/java/org/fogbowcloud/ras/core/plugins/interoperability/openstack/volume/v2/CreateVolumeRequest.java
+++ src/main/java/org/fogbowcloud/ras/core/plugins/interoperability/openstack/volume/v2/CreateVolumeRequest.java
@@ -36,16 +36,20 @@ public class CreateVolumeRequest implements JsonSerializable {
         private final String name;
         @SerializedName(SIZE_KEY_JSON)
         private final String size;
+        @SerializedName(VOLUME_TYPES_KEY_JSON)
+        private final String volume_type;
 
         public Volume(Builder builder) {
             this.name = builder.name;
             this.size = builder.size;
+            this.volume_type = builder.volume_type;
         }
     }
 
     public static class Builder {
         private String name;
         private String size;
+        private String volume_type;
 
         public Builder name(String name) {
             this.name = name;
@@ -54,6 +58,11 @@ public class CreateVolumeRequest implements JsonSerializable {
 
         public Builder size(String size) {
             this.size = size;
+            return this;
+        }
+
+        public Builder volume_type(String volume_type){
+            this.volume_type = volume_type;
             return this;
         }
 
