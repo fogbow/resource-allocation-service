2b92d2436957bcf1dc78bf71fee59b99d6840791
fubica <fubica@dsc.ufcg.edu.br>
2019-02-01 09:31:03 -0300

    Merged all configurations in a single file (ras.conf)

---
+++ src/main/java/cloud/fogbow/ras/api/http/CommonKeys.java
@@ -0,0 +1,5 @@
+package cloud.fogbow.ras.api.http;
+
+public class CommonKeys {
+    public static final String FEDERATION_TOKEN_VALUE_HEADER_KEY = "federationTokenValue";
+}
--- src/test/java/cloud/fogbow/ras/requests/api/local/http/SecurityRuleAPITest.java
+++ src/test/java/cloud/fogbow/ras/requests/api/local/http/SecurityRuleAPITest.java
@@ -3,6 +3,7 @@ package cloud.fogbow.ras.requests.api.local.http;
 import cloud.fogbow.common.exceptions.InstanceNotFoundException;
 import cloud.fogbow.common.exceptions.UnauthenticatedUserException;
 import cloud.fogbow.common.exceptions.UnauthorizedRequestException;
+import cloud.fogbow.ras.api.http.CommonKeys;
 import cloud.fogbow.ras.api.http.Network;
 import cloud.fogbow.ras.api.http.PublicIp;
 import cloud.fogbow.ras.core.ApplicationFacade;
@@ -466,7 +467,7 @@ public class SecurityRuleAPITest {
     private HttpHeaders getHttpHeaders() {
         HttpHeaders headers = new HttpHeaders();
         String fakeFederationTokenValue = "fake-access-id";
-        headers.set(Network.FEDERATION_TOKEN_VALUE_HEADER_KEY, fakeFederationTokenValue);
+        headers.set(CommonKeys.FEDERATION_TOKEN_VALUE_HEADER_KEY, fakeFederationTokenValue);
         return headers;
     }
 
