2418482964be6e0083ed868d9d4c893276080fdb
wassoares <wlfsoares@gmail.com>
2019-02-11 14:11:33 -0300

    Created the OpenNebulaGenericRequestPlugin class for later implementation

---
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/opennebula/genericrequest/v5_4/OpenNebulaGenericRequestPlugin.java
@@ -0,0 +1,18 @@
+package cloud.fogbow.ras.core.plugins.interoperability.opennebula.genericrequest.v5_4;
+
+import cloud.fogbow.common.exceptions.FogbowException;
+import cloud.fogbow.common.models.CloudToken;
+import cloud.fogbow.ras.core.plugins.interoperability.genericrequest.GenericRequest;
+import cloud.fogbow.ras.core.plugins.interoperability.genericrequest.GenericRequestResponse;
+import cloud.fogbow.ras.core.plugins.interoperability.genericrequest.HttpBasedGenericRequestPlugin;
+
+public class OpenNebulaGenericRequestPlugin extends HttpBasedGenericRequestPlugin {
+
+	@Override
+	public GenericRequestResponse redirectGenericRequest(GenericRequest genericRequest, CloudToken token)
+			throws FogbowException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
