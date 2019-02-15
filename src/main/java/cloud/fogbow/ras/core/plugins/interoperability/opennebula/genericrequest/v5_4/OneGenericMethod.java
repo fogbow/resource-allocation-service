f886f56571006ecc26e6dbc8aafa8fa6573544e3
wassoares <wlfsoares@gmail.com>
2019-02-15 15:57:08 -0300

    Created the OneGenericMethod class

---
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/opennebula/genericrequest/v5_4/OneGenericMethod.java
@@ -0,0 +1,58 @@
+package cloud.fogbow.ras.core.plugins.interoperability.opennebula.genericrequest.v5_4;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+
+public class OneGenericMethod {
+
+	public static Method generate(Class classType, String method, List<Class> parameters) {
+		switch (parameters.size()) {
+		case 1:
+			try {
+				return classType.getMethod(method, parameters.get(0));
+			} catch (NoSuchMethodException | SecurityException e) {
+				e.printStackTrace();
+			}
+			break;
+		
+		case 2:
+			try {
+				return classType.getMethod(method, parameters.get(0), parameters.get(1));
+			} catch (NoSuchMethodException | SecurityException e) {
+				e.printStackTrace();
+			}
+			break;
+
+		default:
+			try {
+				return classType.getMethod(method);
+			} catch (NoSuchMethodException | SecurityException e) {
+				e.printStackTrace();
+			}
+			break;
+		}
+		return null;
+	}
+
+	public static Object invoke(Object instance, Method method, List<Object> values) {
+		switch (values.size()) {
+		case 1:
+			try {
+				return method.invoke(instance, values.get(0));
+			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e1) {
+				e1.printStackTrace();
+			}
+			break;
+
+		default:
+			try {
+				return method.invoke(instance);
+			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+				e.printStackTrace();
+			}
+			break;
+		}
+		return null;
+	}
+}
