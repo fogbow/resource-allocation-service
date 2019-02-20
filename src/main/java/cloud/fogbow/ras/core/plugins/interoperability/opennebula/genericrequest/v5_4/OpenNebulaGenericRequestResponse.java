0d3adf53fae9a01845ee78a3c3c89cc6ca7a509a
wassoares <wlfsoares@gmail.com>
2019-02-20 09:48:00 -0300

    Fixes made in the OpenNebulaGenericRequestResponse class

--- src/main/java/cloud/fogbow/ras/core/plugins/interoperability/opennebula/genericrequest/v5_4/OpenNebulaGenericRequestResponse.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/opennebula/genericrequest/v5_4/OpenNebulaGenericRequestResponse.java
@@ -1,67 +1,37 @@
 package cloud.fogbow.ras.core.plugins.interoperability.opennebula.genericrequest.v5_4;
 
-
 import cloud.fogbow.common.util.connectivity.GenericRequestResponse;
 
 public class OpenNebulaGenericRequestResponse extends GenericRequestResponse {
-   private String message;
-   private String errorMessage;
-   private int intMessage;
-   private boolean isError;
-   private boolean booleanMessage;
+	private String message;
+	private boolean isError;
 
-   public OpenNebulaGenericRequestResponse(String message) {
-      // NOTE(pauloewerton): either 'message' or 'errorMessage' should be passed as the response content.
-      super(message);
-   }
+	public OpenNebulaGenericRequestResponse(String message) {
+		// NOTE(pauloewerton): either 'message' or 'errorMessage' should be passed as
+		// the response content.
+		super(message);
+	}
 
-   public OpenNebulaGenericRequestResponse(String message, String errorMessage, int intMessage, boolean isError,
-                                           boolean booleanMessage) {
-      super(message);
-      this.message = message;
-      this.errorMessage = errorMessage;
-      this.intMessage = intMessage;
-      this.isError = isError;
-      this.booleanMessage = booleanMessage;
-   }
+	public OpenNebulaGenericRequestResponse(String message, boolean isError) {
+		super(message);
+		this.message = message;
+		this.isError = isError;
+	}
 
-   public String getMessage() {
-      return message;
-   }
+	public String getMessage() {
+		return message;
+	}
 
-   public void setMessage(String message) {
-      this.message = message;
-   }
+	public void setMessage(String message) {
+		this.message = message;
+	}
 
-   public String getErrorMessage() {
-      return errorMessage;
-   }
+	public boolean isError() {
+		return isError;
+	}
 
-   public void setErrorMessage(String errorMessage) {
-      this.errorMessage = errorMessage;
-   }
+	public void setError(boolean isError) {
+		this.isError = isError;
+	}
 
-   public int getIntMessage() {
-      return intMessage;
-   }
-
-   public void setIntMessage(int intMessage) {
-      this.intMessage = intMessage;
-   }
-
-   public boolean isError() {
-      return isError;
-   }
-
-   public void setError(boolean error) {
-      isError = error;
-   }
-
-   public boolean getBooleanMessage() {
-      return booleanMessage;
-   }
-
-   public void setBooleanMessage(boolean booleanMessage) {
-      this.booleanMessage = booleanMessage;
-   }
 }
