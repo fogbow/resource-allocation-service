cd6f64be31d836f4850232ad8ede3d62ea6befb4
wassoares <wlfsoares@gmail.com>
2019-02-27 11:28:38 -0300

    Fixes made in the OneGenericConstructor class

--- src/main/java/cloud/fogbow/ras/core/plugins/interoperability/opennebula/genericrequest/v5_4/OneGenericConstructor.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/opennebula/genericrequest/v5_4/OneGenericConstructor.java
@@ -1,16 +1,18 @@
 package cloud.fogbow.ras.core.plugins.interoperability.opennebula.genericrequest.v5_4;
 
 import java.lang.reflect.Constructor;
-import java.util.List;
 
 public class OneGenericConstructor {
 
-	public static Constructor generate(Class classType, List<Class> parameters) {
+	public static Constructor generate(Class classType, Class[] parameters) {
 		try {
-			if (parameters.isEmpty()) {
+			switch (parameters.length) {
+			case 1:
+				return classType.getDeclaredConstructor(parameters[0]);
+			case 2:
+				return classType.getDeclaredConstructor(parameters[0], parameters[1]);
+			default:
 				return classType.getConstructor();
-			} else {
-				return classType.getDeclaredConstructor(parameters.toArray(new Class[parameters.size()]));
 			}
 		} catch (NoSuchMethodException | SecurityException e) {
 			e.printStackTrace();
