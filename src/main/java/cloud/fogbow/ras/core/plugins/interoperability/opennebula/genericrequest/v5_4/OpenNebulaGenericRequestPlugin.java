40dd308061bb0bc6a4f82e600f07abb23e1f482d
wassoares <wlfsoares@gmail.com>
2019-03-08 15:42:33 -0300

    Fixes made in the OpenNebulaGenericRequestPlugin class

--- src/main/java/cloud/fogbow/ras/core/plugins/interoperability/opennebula/genericrequest/v5_4/OpenNebulaGenericRequestPlugin.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/opennebula/genericrequest/v5_4/OpenNebulaGenericRequestPlugin.java
@@ -27,6 +27,7 @@ public class OpenNebulaGenericRequestPlugin implements GenericRequestPlugin<Open
 	@Override
 	public FogbowGenericResponse redirectGenericRequest(String genericRequest, OpenNebulaUser cloudUser)
 			throws FogbowException {
+		
 		OpenNebulaFogbowGenericRequest oneGenericRequest = new Gson().fromJson(genericRequest, OpenNebulaFogbowGenericRequest.class);
 		Client client = OpenNebulaClientUtil.createClient(oneGenericRequest.getUrl(), cloudUser.getToken());
 
@@ -35,52 +36,95 @@ public class OpenNebulaGenericRequestPlugin implements GenericRequestPlugin<Open
 			throw new InvalidParameterException(Messages.Exception.INVALID_PARAMETER);
 		}
 		
-		// Generate class type of resource
-		OneResource oneResource = OneResource.getValueOf(request.getOneResource());
-		Class resourceClassType = oneResource.getClassType();
-
-		// Instantiate a resource
-		Object instance = null;
-		if (request.getOneResource().endsWith(RESOURCE_POOL_SUFFIX)) {
-			instance = oneResource.createInstance(client);
-		} else if (request.getResourceId() != null) {
-			instance = oneResource.createInstance(Integer.parseInt(request.getResourceId()), client);
-		} else if (oneResource.equals(OneResource.CLIENT)) {
-			instance = (Client) oneResource.createInstance(cloudUser.getToken(), request.getUrl());
-		}
-		
-		// Working with map of parameters
-        List classes = new ArrayList<>();
-		List values = new ArrayList<>();
-		if (request.getResourceId() != null 
-				&& !request.getOneResource().endsWith(RESOURCE_POOL_SUFFIX) 
-				&& !request.getParameters().isEmpty()) {
-			
-			classes.add(Client.class);
-			values.add(client);
-		}
-		for (Map.Entry<String, String> entries : request.getParameters().entrySet()) {
-			OneParameter oneParameter = OneParameter.getValueOf(entries.getKey());
-			classes.add(oneParameter.getClassType());
-			values.add(oneParameter.getValue(entries.getValue()));
-		}
-		
-		// Generate generic method
-		Method method = OneGenericMethod.generate(resourceClassType, request.getOneMethod(), classes);
-		
-		// Invoke generic method
-		Object object = OneGenericMethod.invoke(instance, method, values);
-		
-		// Requesting a response
+		Object instance = instantiateResource(request, client, cloudUser.getToken());
+		Parameters parameters = generateParametersMap(request, client);
+		Method method = generateMethod(request, parameters);
+		Object response = invokeGenericMethod(instance, parameters, method);
+		return reproduceMessage(response);
+	}
+	
+	protected FogbowGenericResponse reproduceMessage(Object response) {
 		String message;
-		if (object instanceof OneResponse) {
-			OneResponse oneResponse = (OneResponse) object;
+		if (response instanceof OneResponse) {
+			OneResponse oneResponse = (OneResponse) response;
 			boolean isError = oneResponse.isError();
 			message = isError ? oneResponse.getErrorMessage() : oneResponse.getMessage();
 			return new OpenNebulaResponse(message, isError);
 		} else {
-			message = GsonHolder.getInstance().toJson(object);
+			message = GsonHolder.getInstance().toJson(response);
 			return new OpenNebulaResponse(message, false);
+		}
+	}
+	
+	protected Object invokeGenericMethod(Object instance, Parameters parameters, Method method) {
+		Object response = OneGenericMethod.invoke(instance, method, parameters.getValues());
+		return response;
+	}
+
+	protected Method generateMethod(OpenNebulaFogbowGenericRequest request, Parameters parameters) {
+		OneResource oneResource = OneResource.getValueOf(request.getOneResource());
+		Class resourceClassType = oneResource.getClassType();
+		Method method = OneGenericMethod.generate(resourceClassType, request.getOneMethod(), parameters.getClasses());
+		return method;
+	}
+
+	protected Parameters generateParametersMap(OpenNebulaFogbowGenericRequest request, Client client) {
+		Parameters parameters = new Parameters();
+		if (!request.getOneResource().endsWith(RESOURCE_POOL_SUFFIX) && !request.getParameters().isEmpty()) {
+			if (request.getResourceId() == null || request.getResourceId().isEmpty()) {
+				parameters.getClasses().add(Client.class);
+				parameters.getValues().add(client);
+			}
+		}
+		for (Map.Entry<String, String> entries : request.getParameters().entrySet()) {
+			OneParameter oneParameter = OneParameter.getValueOf(entries.getKey());
+			parameters.getClasses().add(oneParameter.getClassType());
+			parameters.getValues().add(oneParameter.getValue(entries.getValue()));
+		}
+		return parameters;
+	}
+
+	protected Object instantiateResource(OpenNebulaFogbowGenericRequest request, Client client, String secret)
+			throws InvalidParameterException {
+		
+		OneResource oneResource = OneResource.getValueOf(request.getOneResource());
+		Object instance = null;
+		if (oneResource.equals(OneResource.CLIENT)) {
+			instance = (Client) oneResource.createInstance(secret, request.getUrl());
+		} else if (request.getOneResource().endsWith(RESOURCE_POOL_SUFFIX)) {
+			instance = oneResource.createInstance(client);
+		} else if (request.getResourceId() != null) {
+			if (!request.getResourceId().isEmpty()) {
+				int id = parseToInteger(request.getResourceId());
+				instance = oneResource.createInstance(id, client);
+			}
+		} 
+		return instance;
+	}
+
+	protected Integer parseToInteger(String arg) throws InvalidParameterException {
+		try {
+			return Integer.parseInt(arg);
+		} catch (NumberFormatException e) {
+			throw new InvalidParameterException(Messages.Exception.INVALID_PARAMETER);
+		}
+	}
+	
+	protected static class Parameters {
+		private List<Class> classes;
+		private List<Object> values;
+		
+		public Parameters() {
+			this.classes = new ArrayList<>();
+			this.values = new ArrayList<>();
+		}
+		
+		public List<Class> getClasses() {
+			return classes;
+		}
+
+		public List<Object> getValues() {
+			return values;
 		}
 	}
 }
