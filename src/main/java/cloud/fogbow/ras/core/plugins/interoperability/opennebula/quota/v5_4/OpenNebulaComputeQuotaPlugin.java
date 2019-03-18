ae69a42dff1604d2147e76a2046520ceb9f992d0
wassoares <wlfsoares@gmail.com>
2019-03-18 11:22:12 -0300

    Modifications made in the OpenNebulaComputeQuotaPlugin class

--- src/main/java/cloud/fogbow/ras/core/plugins/interoperability/opennebula/quota/v5_4/OpenNebulaComputeQuotaPlugin.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/opennebula/quota/v5_4/OpenNebulaComputeQuotaPlugin.java
@@ -2,6 +2,7 @@ package cloud.fogbow.ras.core.plugins.interoperability.opennebula.quota.v5_4;
 
 import java.util.Properties;
 
+import org.apache.log4j.Logger;
 import org.opennebula.client.Client;
 import org.opennebula.client.group.Group;
 import org.opennebula.client.user.User;
@@ -9,16 +10,20 @@ import org.opennebula.client.user.UserPool;
 
 import cloud.fogbow.common.exceptions.FatalErrorException;
 import cloud.fogbow.common.exceptions.FogbowException;
+import cloud.fogbow.common.exceptions.UnexpectedException;
 import cloud.fogbow.common.models.CloudUser;
 import cloud.fogbow.common.util.PropertiesUtil;
 import cloud.fogbow.ras.api.http.response.quotas.ComputeQuota;
 import cloud.fogbow.ras.api.http.response.quotas.allocation.ComputeAllocation;
+import cloud.fogbow.ras.constants.Messages;
 import cloud.fogbow.ras.core.plugins.interoperability.ComputeQuotaPlugin;
 import cloud.fogbow.ras.core.plugins.interoperability.opennebula.OpenNebulaClientUtil;
 import cloud.fogbow.ras.core.plugins.interoperability.opennebula.OpenNebulaConfigurationPropertyKeys;
 
 public class OpenNebulaComputeQuotaPlugin implements ComputeQuotaPlugin<CloudUser> {
 
+	private static final Logger LOGGER = Logger.getLogger(OpenNebulaComputeQuotaPlugin.class);
+	
 	private static final String GROUPS_ID_PATH = "GROUPS/ID";
 	private static final String QUOTA_CPU_USED_PATH = "VM_QUOTA/VM/CPU_USED";
 	private static final String QUOTA_MEMORY_USED_PATH = "VM_QUOTA/VM/MEMORY_USED";
@@ -27,9 +32,7 @@ public class OpenNebulaComputeQuotaPlugin implements ComputeQuotaPlugin<CloudUse
 	private static final String QUOTA_MEMORY_PATH = "VM_QUOTA/VM/MEMORY";
 	private static final String QUOTA_VMS_PATH = "VM_QUOTA/VM/VMS";
 	
-	private static final int DEFAULT_RESOURCE_MAX_VALUE = -1;
-	private static final int VALUE_DEFAULT_QUOTA_OPENNEBULA = -1;
-	private static final int VALUE_UNLIMITED_QUOTA_OPENNEBULA = -2;
+	private static final int DEFAULT_RESOURCE_MAX_VALUE = Integer.MAX_VALUE;
 	
 	private String endpoint;
 
@@ -49,10 +52,16 @@ public class OpenNebulaComputeQuotaPlugin implements ComputeQuotaPlugin<CloudUse
 		String cpuInUseByUser = user.xpath(QUOTA_CPU_USED_PATH);
 		String memoryInUseByUser = user.xpath(QUOTA_MEMORY_USED_PATH);
 		String instancesInUseByUser = user.xpath(QUOTA_VMS_USED_PATH);
-		
+
 		String groupId = user.xpath(GROUPS_ID_PATH);
-		int id = Integer.parseInt(groupId);
-		
+		int id;
+		try {
+			id = Integer.parseInt(groupId);
+		} catch (NumberFormatException e) {
+			LOGGER.error(String.format(Messages.Error.ERROR_MESSAGE, e));
+			throw new UnexpectedException();
+		}
+
 		Group group = OpenNebulaClientUtil.getGroup(client, id);
 		String maxCpuByGroup = group.xpath(QUOTA_CPU_PATH);
 		String maxMemoryByGroup = group.xpath(QUOTA_MEMORY_PATH);
@@ -60,60 +69,38 @@ public class OpenNebulaComputeQuotaPlugin implements ComputeQuotaPlugin<CloudUse
 		String cpuInUseByGroup = group.xpath(QUOTA_CPU_USED_PATH);
 		String memoryInUseByGroup = group.xpath(QUOTA_MEMORY_USED_PATH);
 		String instancesInUseByGroup = group.xpath(QUOTA_VMS_USED_PATH);
-		
-		ResourceQuota resourceQuota = getQuota(maxCpuByUser, cpuInUseByUser, maxCpuByGroup, cpuInUseByGroup);
-		Integer maxCpu = resourceQuota.getMaxResource();
-		Integer cpuInUse = resourceQuota.getResourceInUse();
-		
-		resourceQuota = getQuota(maxMemoryByUser, memoryInUseByUser, maxMemoryByGroup, memoryInUseByGroup);
-		Integer maxMemory = resourceQuota.getMaxResource();
-		Integer memoryInUse = resourceQuota.getResourceInUse();
-		
-		resourceQuota = getQuota(maxInstancesByUser, instancesInUseByUser, maxInstancesByGroup, instancesInUseByGroup);
-		Integer maxNumberInstances = resourceQuota.getMaxResource();
-		Integer instancesInUse = resourceQuota.getResourceInUse();
-		
-		ComputeAllocation totalAllocation = new ComputeAllocation(maxCpu, maxMemory, maxNumberInstances);
-		ComputeAllocation usedAllocation = new ComputeAllocation(cpuInUse, memoryInUse, instancesInUse);
-		
+
+		ResourceQuota cpuQuota = getQuota(maxCpuByUser, cpuInUseByUser, maxCpuByGroup, cpuInUseByGroup);
+		ResourceQuota memoryQuota = getQuota(maxMemoryByUser, memoryInUseByUser, maxMemoryByGroup, memoryInUseByGroup);
+		ResourceQuota vmsQuota = getQuota(maxInstancesByUser, instancesInUseByUser, maxInstancesByGroup,
+				instancesInUseByGroup);
+
+		ComputeAllocation totalAllocation = new ComputeAllocation(cpuQuota.getMaxResource(),
+				memoryQuota.getMaxResource(), vmsQuota.getMaxResource());
+
+		ComputeAllocation usedAllocation = new ComputeAllocation(cpuQuota.getResourceInUse(),
+				memoryQuota.getResourceInUse(), vmsQuota.getResourceInUse());
+
 		ComputeQuota computeQuota = new ComputeQuota(totalAllocation, usedAllocation);
-		
 		return computeQuota;
 	}
 
 	private ResourceQuota getQuota(String maxUserResource, String resourceInUseByUser, String maxGroupResource, String resourceInUseByGroup) {
 		if (isValidNumber(maxUserResource) && isValidNumber(maxGroupResource)) {
-			if (isUnlimitedOrDefaultQuota(maxUserResource)){
-				maxUserResource = String.valueOf(DEFAULT_RESOURCE_MAX_VALUE);
-			}
-			if (isUnlimitedOrDefaultQuota(maxGroupResource)){
-				maxGroupResource = String.valueOf(DEFAULT_RESOURCE_MAX_VALUE);
-			}
 			if (isUserSmallerQuota(maxUserResource, maxGroupResource)) {
 				return getResouceQuota(maxUserResource, resourceInUseByUser);
 			} else {
 				return getResouceQuota(maxGroupResource, resourceInUseByGroup);
 			}
-		} else if (isValidNumber(maxUserResource)) {
-			if (isUnlimitedOrDefaultQuota(maxUserResource)){
-				maxUserResource = String.valueOf(DEFAULT_RESOURCE_MAX_VALUE);
-			}
-			return getResouceQuota(maxUserResource, resourceInUseByUser);
-		} else if (isValidNumber(maxGroupResource)) {
-			if (isUnlimitedOrDefaultQuota(maxGroupResource)){
-				maxGroupResource = String.valueOf(DEFAULT_RESOURCE_MAX_VALUE);
-			}
-			return getResouceQuota(maxGroupResource, resourceInUseByGroup);
-		} else {
-			String maxResource = String.valueOf(DEFAULT_RESOURCE_MAX_VALUE);
-			String resourceInUse = String.valueOf(getBiggerValue(resourceInUseByUser, resourceInUseByGroup));
-			return getResouceQuota(maxResource, resourceInUse);
-		}
+		} 
+		String maxResource = String.valueOf(DEFAULT_RESOURCE_MAX_VALUE);
+		String resourceInUse = String.valueOf(getBiggerValue(resourceInUseByUser, resourceInUseByGroup));
+		return getResouceQuota(maxResource, resourceInUse);
 	}
 
-	private ResourceQuota getResouceQuota(String maxUserResource, String resourceInUseByUser) {
-		int maxResourceValue = parseToInteger(maxUserResource);
-		int resourceInUseValue = parseToInteger(resourceInUseByUser);
+	private ResourceQuota getResouceQuota(String maxResource, String resourceInUse) {
+		int maxResourceValue = parseToInteger(maxResource);
+		int resourceInUseValue = parseToInteger(resourceInUse);
 		return new ResourceQuota(maxResourceValue, resourceInUseValue);
 	}
 
@@ -132,23 +119,18 @@ public class OpenNebulaComputeQuotaPlugin implements ComputeQuotaPlugin<CloudUse
 	}
 	
 	private boolean isUserSmallerQuota(String userResource, String groupResource) {
-		int userResourceValue = Integer.parseInt(userResource);
-		int groupResourceValue = Integer.parseInt(groupResource);
+		int userResourceValue = parseToInteger(userResource);
+		int groupResourceValue = parseToInteger(groupResource);
 		if (userResourceValue < groupResourceValue) {
 			return true;
 		}
 		return false;
 	}
 
-	private boolean isUnlimitedOrDefaultQuota(String resource) {
-		int resourceValue = Integer.parseInt(resource);
-		return resourceValue == VALUE_DEFAULT_QUOTA_OPENNEBULA || resourceValue == VALUE_UNLIMITED_QUOTA_OPENNEBULA;
-	}
-
 	private boolean isValidNumber(String number) {
 		try {
 			Integer.parseInt(number);
-		} catch (Exception e) {
+		} catch (NumberFormatException e) {
 			return false;
 		}
 		return true;
