fc19b6d4bcad419ec6c826098dbee857e84ef91b
Paulo Ewerton <pauloewerton@lsd.ufcg.edu.br>
2019-04-02 11:40:52 -0300

    Adding cloud and system user types to cloudstack and one mappers

--- src/main/java/cloud/fogbow/ras/core/plugins/mapper/one2one/CloudStackOneToOneMapper.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/mapper/one2one/CloudStackOneToOneMapper.java
@@ -1,8 +1,10 @@
 package cloud.fogbow.ras.core.plugins.mapper.one2one;
 
+import cloud.fogbow.as.core.models.CloudStackSystemUser;
+import cloud.fogbow.common.models.CloudStackUser;
 import cloud.fogbow.ras.core.plugins.mapper.all2one.CloudStackAllToOneMapper;
 
-public class CloudStackOneToOneMapper extends GenericOneToOneSystemToCloudMapper {
+public class CloudStackOneToOneMapper extends GenericOneToOneSystemToCloudMapper<CloudStackUser, CloudStackSystemUser> {
     public CloudStackOneToOneMapper(String mapperConfFilePath) {
         super(new CloudStackAllToOneMapper(mapperConfFilePath));
     }
--- src/main/java/cloud/fogbow/ras/core/plugins/mapper/one2one/OpenNebulaOneToOneMapper.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/mapper/one2one/OpenNebulaOneToOneMapper.java
@@ -1,8 +1,10 @@
 package cloud.fogbow.ras.core.plugins.mapper.one2one;
 
+import cloud.fogbow.as.core.models.OpenNebulaSystemUser;
+import cloud.fogbow.common.models.OpenNebulaUser;
 import cloud.fogbow.ras.core.plugins.mapper.all2one.OpenNebulaAllToOneMapper;
 
-public class OpenNebulaOneToOneMapper extends GenericOneToOneSystemToCloudMapper {
+public class OpenNebulaOneToOneMapper extends GenericOneToOneSystemToCloudMapper<OpenNebulaUser, OpenNebulaSystemUser> {
     public OpenNebulaOneToOneMapper(String mapperConfFilePath) {
             super(new OpenNebulaAllToOneMapper(mapperConfFilePath));
     }
