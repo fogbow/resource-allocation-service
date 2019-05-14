6e03f9fb1dc9bd5ab795fd13012a52e5e479a114
wassoares <wlfsoares@gmail.com>
2019-05-14 16:00:59 -0300

    Fixes made in the AwsV2ClientUtil, AwsV2VolumePlugin and AwsV2AttachmentPlugin classes

--- src/main/java/cloud/fogbow/ras/core/plugins/interoperability/aws/AwsV2ClientUtil.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/aws/AwsV2ClientUtil.java
@@ -8,19 +8,22 @@ import cloud.fogbow.common.exceptions.UnexpectedException;
 import cloud.fogbow.ras.constants.Messages;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.exception.SdkClientException;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.ec2.Ec2Client;
 
 public class AwsV2ClientUtil {
 
 	private static final Logger LOGGER = Logger.getLogger(AwsV2ClientUtil.class);
+	private static final int ACCESS_KEY_ID_TOKEN_INDEX = 0;
+	private static final int SECRET_KEY_ID_TOKEN_INDEX = 1;
 
 	public static Ec2Client createEc2Client(String tokenValue, String selectedRegion)
 			throws InvalidParameterException, UnexpectedException {
 
 		String[] token = tokenValue.split(AwsConstants.TOKEN_VALUE_SEPARATOR);
-		String accessKeyId = token[0];
-		String secretKeyId = token[1];
+		String accessKeyId = token[ACCESS_KEY_ID_TOKEN_INDEX];
+		String secretKeyId = token[SECRET_KEY_ID_TOKEN_INDEX];
 
 		Region region = Region.of(selectedRegion);
 		if (!Region.regions().contains(region)) {
@@ -36,7 +39,7 @@ public class AwsV2ClientUtil {
 					.build();
 
 			return client;
-		} catch (Exception e) {
+		} catch (SdkClientException e) {
 			LOGGER.error(Messages.Error.ERROR_WHILE_CREATING_CLIENT, e);
 			throw new UnexpectedException();
 		}
