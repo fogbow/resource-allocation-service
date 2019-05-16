2643523c00d86d3435d8a86b52be2bb21dcb9c5a
wassoares <wlfsoares@gmail.com>
2019-05-16 15:41:57 -0300

    Fixes test in the AwsV2ClientTest class

--- src/test/java/cloud/fogbow/ras/core/plugins/interoperability/aws/AwsV2ClientUtilTest.java
+++ src/test/java/cloud/fogbow/ras/core/plugins/interoperability/aws/AwsV2ClientUtilTest.java
@@ -1,58 +1,72 @@
 package cloud.fogbow.ras.core.plugins.interoperability.aws;
 
-import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.mockito.internal.verification.VerificationModeFactory;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import cloud.fogbow.common.exceptions.InvalidParameterException;
 import cloud.fogbow.common.exceptions.UnexpectedException;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.ec2.Ec2Client;
+import software.amazon.awssdk.services.ec2.Ec2ClientBuilder;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ AwsBasicCredentials.class, StaticCredentialsProvider.class, Ec2Client.class })
+@PrepareForTest({ Ec2Client.class })
 public class AwsV2ClientUtilTest {
 
-	// test case: ...
-	@Ignore
+	private static final String ANYTHING_VALUE = "anything";
+	private static final String EAST_SOUTH_AMERICA_REGION = "sa-east-1";
+	private static final String EMPTY_STRING = "";
+	private static final String FAKE_TOKEN_VALUE = "fake-access-key:fake-secret-key";
+
+	// test case: Verifies that the invocation of the build method to create a
+	// client was called correctly.
 	@Test
 	public void testcreateEc2ClientSuccessful() throws InvalidParameterException, UnexpectedException {
 		// set up
-		String tokenValue = "fake-access-key:fake-secret-key";
-		String regionName = "sa-east-1";
+		Ec2Client client = Mockito.mock(Ec2Client.class);
+		Ec2ClientBuilder clientBuilder = Mockito.mock(Ec2ClientBuilder.class);
 
-		Ec2Client ec2client = Mockito.mock(Ec2Client.class);
-		PowerMockito.mock(Ec2Client.class);
-		Mockito.when(Ec2Client.builder().credentialsProvider(Mockito.any(StaticCredentialsProvider.class))
-				.region(Mockito.eq(Region.AWS_GLOBAL)).build()).thenReturn(ec2client);
+		PowerMockito.mockStatic(Ec2Client.class);
+		PowerMockito.when(Ec2Client.builder()).thenReturn(clientBuilder);
+
+		Mockito.when(clientBuilder.credentialsProvider(Mockito.any())).thenReturn(clientBuilder);
+		Mockito.when(clientBuilder.region(Mockito.any())).thenReturn(clientBuilder);
+		Mockito.when(clientBuilder.build()).thenReturn(client);
+
+		String tokenValue = FAKE_TOKEN_VALUE;
+		String regionName = EAST_SOUTH_AMERICA_REGION;
 
 		// exercise
-		Ec2Client client = AwsV2ClientUtil.createEc2Client(tokenValue, regionName);
+		AwsV2ClientUtil.createEc2Client(tokenValue, regionName);
 
 		// verify
-		Assert.assertNotNull(client);
+		PowerMockito.verifyStatic(Ec2Client.class, VerificationModeFactory.times(1));
+		Ec2Client.builder();
+
+		Mockito.verify(clientBuilder, Mockito.times(1)).credentialsProvider(Mockito.any());
+		Mockito.verify(clientBuilder, Mockito.times(1)).region(Mockito.any());
+		Mockito.verify(clientBuilder, Mockito.times(1)).build();
 	}
 	
-	// test case: ...
+	// test case: When calling the deleteInstance method, with an invalid token, an
+	// UnexpectedException will be thrown.
 	@Test(expected = UnexpectedException.class) // verify
 	public void testcreateEc2ClientUnsuccessful() throws InvalidParameterException, UnexpectedException {
 		// set up
-		String tokenValue = "fake-access-key:fake-secret-key";
-		String regionName = "sa-east-1";
+		String tokenValue = FAKE_TOKEN_VALUE;
+		String regionName = EAST_SOUTH_AMERICA_REGION;
 
 		// exercise
 		AwsV2ClientUtil.createEc2Client(tokenValue, regionName);
 	}
 	
-	// test case: ...
-	@Test (expected = InvalidParameterException.class) // verify
+	// test case: When calling the validateRegion method with a null region name, an
+	// InvalidParameterException will be thrown.
+	@Test(expected = InvalidParameterException.class) // verify
 	public void testValidateRegionWithANullRegionName() throws InvalidParameterException, UnexpectedException {
 		// set up
 		String regionName = null;
@@ -61,21 +75,23 @@ public class AwsV2ClientUtilTest {
 		AwsV2ClientUtil.validateRegion(regionName);
 	}
 	
-	// test case: ...
+	// test case: When calling the validateRegion method with any value in region
+	// name, an InvalidParameterException will be thrown.
 	@Test(expected = InvalidParameterException.class) // verify
 	public void testValidateRegionWithAInconsistentRegionName() throws InvalidParameterException, UnexpectedException {
 		// set up
-		String regionName = "anything";
+		String regionName = ANYTHING_VALUE;
 
 		// exercise
 		AwsV2ClientUtil.validateRegion(regionName);
 	}
 	
-	// test case: ...
+	// test case: When calling the validateRegion method with an empty string in
+	// region name, an InvalidParameterException will be thrown.
 	@Test(expected = InvalidParameterException.class) // verify
 	public void testValidateRegionWithAEmptyRegionName() throws InvalidParameterException, UnexpectedException {
 		// set up
-		String regionName = "";
+		String regionName = EMPTY_STRING;
 
 		// exercise
 		AwsV2ClientUtil.validateRegion(regionName);
