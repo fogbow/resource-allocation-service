96ee177c459f3f9e6f9ee54cec0a077f9fbceb2f
wassoares <wlfsoares@gmail.com>
2019-05-17 16:03:42 -0300

    Added new constants in the AwsV2ConfigurationPropertyKeys class

--- src/main/java/cloud/fogbow/ras/core/plugins/interoperability/aws/AwsV2ConfigurationPropertyKeys.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/aws/AwsV2ConfigurationPropertyKeys.java
@@ -2,7 +2,9 @@ package cloud.fogbow.ras.core.plugins.interoperability.aws;
 
 public class AwsV2ConfigurationPropertyKeys {
 
-	public static final String AWS_REGION_SELECTION_KEY = "aws_region_selection_key";
 	public static final String AWS_AVAILABILITY_ZONE_KEY = "aws_availability_zone_key";
+	public static final String AWS_DEFAULT_VPC_SECURITY_GROUP_KEY = "aws_default_vpc_security_group_key";
+	public static final String AWS_REGION_SELECTION_KEY = "aws_region_selection_key";
+	public static final String AWS_SUBNET_ID_KEY = "aws_subnet_id_key";
 
 }
