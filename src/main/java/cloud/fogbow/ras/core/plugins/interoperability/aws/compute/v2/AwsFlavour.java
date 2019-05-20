4ee2bc094346b97237660970b2a771a2ba78b76b
wassoares <wlfsoares@gmail.com>
2019-05-20 15:08:45 -0300

    Created the AwsFlavour class

---
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/aws/compute/v2/AwsFlavour.java
@@ -0,0 +1,36 @@
+package cloud.fogbow.ras.core.plugins.interoperability.aws.compute.v2;
+
+public enum AwsFlavour {
+
+	T1_MICRO("t1.micro") {
+
+		private final int VCPU_CORE_VALUE = 1;
+		private final double MEMORY_VALUE = 0.613; 
+		
+		@Override
+		public Integer getVCpu() {
+			return VCPU_CORE_VALUE;
+		}
+
+		@Override
+		public Double getMemory() {
+			return MEMORY_VALUE;
+		}
+
+	};
+
+	private String name;
+
+	AwsFlavour(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public abstract Integer getVCpu();
+
+	public abstract Double getMemory();
+
+}
