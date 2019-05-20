eb09ead83a267b3af8ed53a6ff26e06eb04e0a37
wassoares <wlfsoares@gmail.com>
2019-05-20 15:07:18 -0300

    Implementing the AwsV2ComputePlugin class

--- src/main/java/cloud/fogbow/ras/core/plugins/interoperability/aws/compute/v2/AwsV2ComputePlugin.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/aws/compute/v2/AwsV2ComputePlugin.java
@@ -8,6 +8,7 @@ import java.util.TreeSet;
 import org.apache.log4j.Logger;
 
 import cloud.fogbow.common.exceptions.FogbowException;
+import cloud.fogbow.common.exceptions.InstanceNotFoundException;
 import cloud.fogbow.common.models.AwsV2User;
 import cloud.fogbow.common.util.PropertiesUtil;
 import cloud.fogbow.ras.api.http.response.ComputeInstance;
@@ -20,23 +21,28 @@ import cloud.fogbow.ras.core.models.orders.ComputeOrder;
 import cloud.fogbow.ras.core.plugins.interoperability.ComputePlugin;
 import cloud.fogbow.ras.core.plugins.interoperability.aws.AwsV2ClientUtil;
 import cloud.fogbow.ras.core.plugins.interoperability.aws.AwsV2ConfigurationPropertyKeys;
+import cloud.fogbow.ras.core.plugins.interoperability.aws.AwsV2InstanceTypeMapper;
 import cloud.fogbow.ras.core.plugins.interoperability.aws.AwsV2StateMapper;
 import cloud.fogbow.ras.core.plugins.interoperability.util.DefaultLaunchCommandGenerator;
 import cloud.fogbow.ras.core.plugins.interoperability.util.LaunchCommandGenerator;
 import software.amazon.awssdk.services.ec2.Ec2Client;
 import software.amazon.awssdk.services.ec2.model.DescribeInstancesRequest;
 import software.amazon.awssdk.services.ec2.model.DescribeInstancesResponse;
+import software.amazon.awssdk.services.ec2.model.DescribeVolumesRequest;
+import software.amazon.awssdk.services.ec2.model.DescribeVolumesResponse;
 import software.amazon.awssdk.services.ec2.model.Instance;
 import software.amazon.awssdk.services.ec2.model.InstanceNetworkInterfaceSpecification;
 import software.amazon.awssdk.services.ec2.model.InstanceType;
 import software.amazon.awssdk.services.ec2.model.Reservation;
 import software.amazon.awssdk.services.ec2.model.RunInstancesRequest;
 import software.amazon.awssdk.services.ec2.model.RunInstancesResponse;
+import software.amazon.awssdk.services.ec2.model.Volume;
 
 public class AwsV2ComputePlugin implements ComputePlugin<AwsV2User> {
 
     private static final Logger LOGGER = Logger.getLogger(AwsV2ComputePlugin.class);
 	private static final int FIRST_POSITION = 0;
+	private static final int ONE_GIGABYTE = 1024;
 	
 	private TreeSet<HardwareRequirements> flavors;
 	private LaunchCommandGenerator launchCommandGenerator;
@@ -60,6 +66,10 @@ public class AwsV2ComputePlugin implements ComputePlugin<AwsV2User> {
 		// Implements flavors to get an image compatible with the requirements of the order
 		String imageId = computeOrder.getImageId();
 		
+		int cpu = computeOrder.getvCPU();
+		double memory = computeOrder.getMemory() / ONE_GIGABYTE; // The AWS cloud calculate this value in gigabytes
+		InstanceType instanceType = AwsV2InstanceTypeMapper.map(cpu, memory);
+		
 		InstanceNetworkInterfaceSpecification networkSpecification = InstanceNetworkInterfaceSpecification.builder()
 				.deviceIndex(FIRST_POSITION) // The first position of the network interface in the attachment order.
 				.subnetId(this.subnetId)
@@ -70,7 +80,7 @@ public class AwsV2ComputePlugin implements ComputePlugin<AwsV2User> {
 
 		RunInstancesRequest request = RunInstancesRequest.builder()
 				.imageId(imageId)
-				.instanceType(InstanceType.T1_MICRO)
+				.instanceType(instanceType)
 				.maxCount(1)
 				.minCount(1)
 				.networkInterfaces(networkSpecification)
@@ -79,6 +89,8 @@ public class AwsV2ComputePlugin implements ComputePlugin<AwsV2User> {
 
 		Ec2Client client = AwsV2ClientUtil.createEc2Client(cloudUser.getToken(), this.region);
 		RunInstancesResponse response = client.runInstances(request);
+		
+		// Create tag name to define the name of instance
 		
 		String instanceId = null;
 		if (!response.instances().isEmpty()) {
@@ -96,8 +108,7 @@ public class AwsV2ComputePlugin implements ComputePlugin<AwsV2User> {
     			.build();
     	
     	Ec2Client client = AwsV2ClientUtil.createEc2Client(cloudUser.getToken(), this.region);
-    	DescribeInstancesResponse response = client.describeInstances(request);
-    	ComputeInstance computeInstance = mountComputeInstance(response);
+    	ComputeInstance computeInstance = mountComputeInstance(request, client);
     	return computeInstance;
     }
 
@@ -116,34 +127,29 @@ public class AwsV2ComputePlugin implements ComputePlugin<AwsV2User> {
     	return AwsV2StateMapper.map(ResourceType.COMPUTE, instanceState).equals(InstanceState.FAILED);
     }
     
-    protected ComputeInstance mountComputeInstance(DescribeInstancesResponse response) {
-    	String id = null;
-    	String cloudState = null;
-    	String name = null;
-    	int cpu = 0;
-    	int memory = 0;
-    	int disk = 0;
-    	List<String> ipAddresses = null;
-    	List<UserData> userData = null;
-    	String imageId = null;
-    	String publicKey = null;
-    	
-		if (!response.reservations().isEmpty()) {
-			Reservation reservation = response.reservations().get(FIRST_POSITION);
-			Instance instance = reservation.instances().get(FIRST_POSITION);
-			id = instance.instanceId();
-			cloudState = instance.state().nameAsString();
-			cpu = instance.cpuOptions().coreCount();
-			memory = getRamDiskValue(instance);
-			disk = getVolumeSize(instance);
-			ipAddresses = getIpAddresses(instance);
-			return new ComputeInstance(id, cloudState, name, cpu, memory, disk, ipAddresses, imageId, publicKey, userData);
-		}
-    	// TODO Auto-generated method stub
-		return null;
+	protected ComputeInstance mountComputeInstance(DescribeInstancesRequest request, Ec2Client client)
+			throws InstanceNotFoundException {
+
+		DescribeInstancesResponse instancesResponse = client.describeInstances(request);
+		Instance instance = getInstanceReservation(instancesResponse);
+
+		List<String> volumeIds = getVolumeIds(instance);
+		List<Volume> volumes = getInstanceVolumes(volumeIds, client);
+
+		String id = instance.instanceId();
+		String cloudState = instance.state().nameAsString();
+		String name = null; // FIXME
+		int cpu = instance.cpuOptions().coreCount();
+		int memory = getMemoryValueFrom(instance.instanceType());
+		int disk = getAllDisksSize(volumes);
+		List<String> ipAddresses = getIpAddresses(instance);
+		List<UserData> userData = null; // FIXME
+		String imageId = instance.imageId();
+		String publicKey = null; // FIXME
+		return new ComputeInstance(id, cloudState, name, cpu, memory, disk, ipAddresses, imageId, publicKey, userData);
 	}
 
-	private List<String> getIpAddresses(Instance instance) {
+	protected List<String> getIpAddresses(Instance instance) {
 		List<String> ipAddresses = new ArrayList<String>();
 		for (int i = 0; i < instance.networkInterfaces().size(); i++) {
 			ipAddresses.add(instance.networkInterfaces().get(i).privateIpAddress());
@@ -151,17 +157,53 @@ public class AwsV2ComputePlugin implements ComputePlugin<AwsV2User> {
 		return ipAddresses;
 	}
 
-	protected int getVolumeSize(Instance instance) {
-		// get all volumeId under blockDeviceMappings
-		String volumeId = instance.blockDeviceMappings().get(FIRST_POSITION).ebs().volumeId();
-		// describe all volumes passing all volumeIds
-		// for each volume get size value
-		return 0;
+	protected int getAllDisksSize(List<Volume> volumes) {
+		int size = 0;
+		for (Volume volume : volumes) {
+			size += volume.size();
+		}
+		return size;
+	}
+	
+	protected int getMemoryValueFrom(InstanceType instanceType) {
+		Double memory = AwsFlavour.valueOf(instanceType.name()).getMemory() * ONE_GIGABYTE; 
+		return memory.intValue();
+	}
+	
+	protected List<Volume> getInstanceVolumes(List<String> volumeIds, Ec2Client client) {
+		DescribeVolumesResponse response;
+		for (String volumeId : volumeIds) {
+			response = getDescribeVolume(volumeId, client);
+			return response.volumes();
+
+		}
+		return null;
 	}
 
-	protected int getRamDiskValue(Instance instance) {
-		String ramDiskId = instance.ramdiskId();
-		return 0;
+	protected DescribeVolumesResponse getDescribeVolume(String volumeId, Ec2Client client) {
+		DescribeVolumesRequest request = DescribeVolumesRequest.builder()
+				.volumeIds(volumeId)
+				.build();
+
+		return client.describeVolumes(request);
 	}
 
+	protected List<String> getVolumeIds(Instance instance) {
+		List<String> volumeIds = new ArrayList<String>();
+		for (int i = 0; i < instance.blockDeviceMappings().size(); i++) {
+			volumeIds.add(instance.blockDeviceMappings().get(i).ebs().volumeId());
+		}
+		return volumeIds;
+	}
+
+	protected Instance getInstanceReservation(DescribeInstancesResponse response) throws InstanceNotFoundException {
+		if (!response.reservations().isEmpty()) {
+			Reservation reservation = response.reservations().get(FIRST_POSITION);
+			if (!reservation.instances().isEmpty()) {
+				return reservation.instances().get(FIRST_POSITION);
+			}
+		}
+		throw new InstanceNotFoundException();
+	}
+	
 }
