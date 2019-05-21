2edcf7f1eaa5b437d35fc4585b9dca67aec754ea
wassoares <wlfsoares@gmail.com>
2019-05-21 15:25:02 -0300

    Fixes made in the AwsV2InstanceTypeMapper class

--- src/main/java/cloud/fogbow/ras/core/plugins/interoperability/aws/AwsV2InstanceTypeMapper.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/aws/AwsV2InstanceTypeMapper.java
@@ -8,10 +8,11 @@ public class AwsV2InstanceTypeMapper {
 
 	public static InstanceType map(int cpu, double memory) throws NoAvailableResourcesException {
 		for (AwsFlavour flavour : AwsFlavour.values()) {
-			if (flavour.getVCpu() <= cpu && flavour.getMemory() <= memory) {
+			if (flavour.getVCpu() == cpu && memory <= flavour.getMemory()) {
 				return InstanceType.fromValue(flavour.getName());
 			}
 		}
+		// TODO Fix exception message
 		throw new NoAvailableResourcesException("Not found compatible values of vcpu or memory.");
 	}
 	
