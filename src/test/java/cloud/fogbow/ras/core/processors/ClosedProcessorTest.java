0fb31ba02a5cc87aa7d21b1a8774b7ee986b3581
wassoares <wlfsoares@gmail.com>
2019-07-24 13:13:20 -0300

    Added more tests to increased coverage from ClosedProcessor class and test class organization

--- src/test/java/cloud/fogbow/ras/core/processors/ClosedProcessorTest.java
+++ src/test/java/cloud/fogbow/ras/core/processors/ClosedProcessorTest.java
@@ -1,14 +1,8 @@
 package cloud.fogbow.ras.core.processors;
 
-import cloud.fogbow.common.exceptions.UnexpectedException;
-import cloud.fogbow.common.models.linkedlists.ChainedList;
-import cloud.fogbow.ras.constants.ConfigurationPropertyDefaults;
-import cloud.fogbow.ras.core.*;
-import cloud.fogbow.ras.core.cloudconnector.CloudConnectorFactory;
-import cloud.fogbow.ras.core.cloudconnector.LocalCloudConnector;
-import cloud.fogbow.ras.core.cloudconnector.RemoteCloudConnector;
-import cloud.fogbow.ras.core.models.orders.Order;
-import cloud.fogbow.ras.core.models.orders.OrderState;
+import java.util.Map;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,39 +10,45 @@ import org.mockito.Mockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import java.util.Map;
-import java.util.Properties;
-
-import static org.junit.Assert.assertNull;
+import cloud.fogbow.common.exceptions.FogbowException;
+import cloud.fogbow.common.exceptions.UnexpectedException;
+import cloud.fogbow.common.models.linkedlists.ChainedList;
+import cloud.fogbow.ras.constants.ConfigurationPropertyDefaults;
+import cloud.fogbow.ras.core.BaseUnitTests;
+import cloud.fogbow.ras.core.OrderController;
+import cloud.fogbow.ras.core.OrderStateTransitioner;
+import cloud.fogbow.ras.core.SharedOrderHolders;
+import cloud.fogbow.ras.core.cloudconnector.CloudConnectorFactory;
+import cloud.fogbow.ras.core.models.orders.Order;
+import cloud.fogbow.ras.core.models.orders.OrderState;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(CloudConnectorFactory.class)
 public class ClosedProcessorTest extends BaseUnitTests {
 
-    private ClosedProcessor closedProcessor;
-
-    @SuppressWarnings("unused")
-    private RemoteCloudConnector remoteCloudConnector;
-    private LocalCloudConnector localCloudConnector;
-
-    @SuppressWarnings("unused")
-    private Properties properties;
-    private Thread thread;
+    private static final long DEFAULT_SLEEP_TIME = 500;
+    
+    private static final String FAKE_INSTANCE_ID = "fake-instance-id";
+    
+    private Map<String, Order> activeOrdersMap;
+    private ChainedList<Order> closedOrderList;
+    private ClosedProcessor processor;
     private OrderController orderController;
+    private Thread thread;
 
     @Before
     public void setUp() throws UnexpectedException {
-        mockReadOrdersFromDataBase();
-        this.properties = new Properties();
+        super.mockReadOrdersFromDataBase();
 
-        PropertiesHolder propertiesHolder = PropertiesHolder.getInstance();
-        this.properties = propertiesHolder.getProperties();
-        this.orderController = new OrderController();
-
-        this.localCloudConnector = Mockito.mock(LocalCloudConnector.class);
-        this.remoteCloudConnector = Mockito.mock(RemoteCloudConnector.class);
-        this.closedProcessor = Mockito.spy(new ClosedProcessor(orderController,
+        this.orderController = Mockito.spy(new OrderController());
+        this.processor = Mockito.spy(new ClosedProcessor(this.orderController,
                 ConfigurationPropertyDefaults.CLOSED_ORDERS_SLEEP_TIME));
+        
+        SharedOrderHolders sharedOrderHolders = SharedOrderHolders.getInstance();
+        this.activeOrdersMap = sharedOrderHolders.getActiveOrdersMap();
+        this.closedOrderList = sharedOrderHolders.getClosedOrdersList();
+        
+        this.thread = null;
     }
 
     @Override
@@ -63,31 +63,61 @@ public class ClosedProcessorTest extends BaseUnitTests {
     //test case: the closed processor remove closed orders from the closed orders list
     @Test
     public void testProcessClosedLocalOrder() throws Exception {
-
         //set up
-        String instanceId = "fake-id";
+        Order order = createLocalOrder(getLocalMemberId());
+        order.setInstanceId(FAKE_INSTANCE_ID);
 
-        Order localOrder = createLocalOrder(getLocalMemberId());
-        localOrder.setInstanceId(instanceId);
-
-        this.orderController.activateOrder(localOrder);
-        OrderStateTransitioner.transition(localOrder, OrderState.CLOSED);
-
+        this.orderController.activateOrder(order);
+        OrderStateTransitioner.transition(order, OrderState.CLOSED);
+        
         //exercise
-        this.thread = new Thread(this.closedProcessor);
+        this.thread = new Thread(this.processor);
         this.thread.start();
-        Thread.sleep(500);
+        Thread.sleep(DEFAULT_SLEEP_TIME);
 
         //verify
-        SharedOrderHolders sharedOrderHolders = SharedOrderHolders.getInstance();
-        ChainedList<Order> closedOrders = sharedOrderHolders.getClosedOrdersList();
-        Map<String, Order> activeOrdersMap = sharedOrderHolders.getActiveOrdersMap();
-        assertNull(activeOrdersMap.get(localOrder.getId()));
-
-        closedOrders.resetPointer();
-        assertNull(closedOrders.getNext());
-
-        //TODO: it would be good to verity that the OrderStateTransitioner.deactivateOrder was called. However,
-        //it is static thus, hard to mock
+        Mockito.verify(this.orderController, Mockito.times(1)).deactivateOrder(Mockito.eq(order));
+        Assert.assertNull(activeOrdersMap.get(order.getId()));
+        Assert.assertNull(closedOrderList.getNext());
     }
+    
+    // test case: Check the throw of UnexpectedException when running the thread in
+    // the closed processor, while running a local order.
+    @Test
+    public void testRunProcessLocalOrderThrowsUnexpectedException() throws InterruptedException, FogbowException {
+        // set up
+        Order order = createLocalOrder(getLocalMemberId());
+        this.closedOrderList.addItem(order);
+
+        Mockito.doThrow(new UnexpectedException()).when(this.processor).processClosedOrder(Mockito.eq(order));
+
+        // exercise
+        this.thread = new Thread(this.processor);
+        this.thread.start();
+        Thread.sleep(DEFAULT_SLEEP_TIME);
+
+        // verify
+        Mockito.verify(this.processor, Mockito.times(1)).processClosedOrder(Mockito.eq(order));
+    }
+    
+    // test case: During a thread running in closed processor, if any
+    // errors occur, the processClosedOrder method will catch an exception.
+    @Test
+    public void testRunProcessLocalOrderToCatchException() throws InterruptedException, FogbowException {
+
+        // set up
+        Order order = createLocalOrder(getLocalMemberId());
+        this.closedOrderList.addItem(order);
+
+        Mockito.doThrow(new RuntimeException()).when(this.processor).processClosedOrder(Mockito.eq(order));
+
+        // exercise
+        this.thread = new Thread(this.processor);
+        this.thread.start();
+        Thread.sleep(DEFAULT_SLEEP_TIME);
+
+        // verify
+        Mockito.verify(this.processor, Mockito.times(1)).processClosedOrder(Mockito.eq(order));
+    }
+    
 }
