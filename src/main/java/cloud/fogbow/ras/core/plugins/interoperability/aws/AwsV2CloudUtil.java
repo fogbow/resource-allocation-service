f558c1119aaa1201eb4bb934663c856f1acd25a4
wassoares <wlfsoares@gmail.com>
2019-07-29 09:28:33 -0300

    Added new methods in the AwsV2CloudUtil class

--- src/main/java/cloud/fogbow/ras/core/plugins/interoperability/aws/AwsV2CloudUtil.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/aws/AwsV2CloudUtil.java
@@ -6,16 +6,35 @@ import cloud.fogbow.common.exceptions.UnexpectedException;
 import cloud.fogbow.ras.constants.Messages;
 import software.amazon.awssdk.services.ec2.Ec2Client;
 import software.amazon.awssdk.services.ec2.model.CreateTagsRequest;
+import software.amazon.awssdk.services.ec2.model.DescribeImagesRequest;
+import software.amazon.awssdk.services.ec2.model.DescribeImagesResponse;
 import software.amazon.awssdk.services.ec2.model.DescribeVolumesRequest;
 import software.amazon.awssdk.services.ec2.model.DescribeVolumesResponse;
+import software.amazon.awssdk.services.ec2.model.Image;
 import software.amazon.awssdk.services.ec2.model.Tag;
 import software.amazon.awssdk.services.ec2.model.Volume;
 
 public class AwsV2CloudUtil {
     
     public static final String AWS_TAG_NAME = "Name";
-
-    public static Volume getVolumeFrom(DescribeVolumesResponse response) throws InstanceNotFoundException {
+    
+    public static Image getImagesFrom(DescribeImagesResponse response) throws FogbowException {
+        if (response != null && !response.images().isEmpty()) {
+            return response.images().listIterator().next();
+        }
+        throw new InstanceNotFoundException(Messages.Exception.INSTANCE_NOT_FOUND);
+    }
+    
+    public static DescribeImagesResponse doDescribeImagesRequest(Ec2Client client, DescribeImagesRequest request)
+            throws FogbowException {
+        try {
+            return client.describeImages(request);
+        } catch (Exception e) {
+            throw new UnexpectedException(String.format(Messages.Exception.GENERIC_EXCEPTION, e), e);
+        }
+    }
+    
+    public static Volume getVolumeFrom(DescribeVolumesResponse response) throws FogbowException {
         if (response != null && !response.volumes().isEmpty()) {
             return response.volumes().listIterator().next();
         }
@@ -32,7 +51,7 @@ public class AwsV2CloudUtil {
     }
     
     public static void doCreateTagsRequest(Ec2Client client, String resourceId, String key, String value)
-            throws UnexpectedException {
+            throws FogbowException {
 
         Tag tag = Tag.builder()
                 .key(key)
