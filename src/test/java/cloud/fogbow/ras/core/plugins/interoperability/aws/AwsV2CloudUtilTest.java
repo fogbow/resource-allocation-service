ede2b81bec44c78d992e4cdab5149f2c760ed592
raonismaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2019-08-16 17:30:46 -0300

    starting the tests

---
+++ src/test/java/cloud/fogbow/ras/core/plugins/interoperability/aws/AwsV2CloudUtilTest.java
@@ -0,0 +1,11 @@
+package cloud.fogbow.ras.core.plugins.interoperability.aws;
+
+import cloud.fogbow.ras.core.BaseUnitTests;
+
+import static org.junit.Assert.*;
+
+public class AwsV2CloudUtilTest extends BaseUnitTests {
+
+    
+
+}
