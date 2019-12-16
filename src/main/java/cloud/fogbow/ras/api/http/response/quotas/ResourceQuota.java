99c640cd17165a9c4c745b64c3683cf9a8f72f9c
wassoares <wlfsoares@gmail.com>
2019-12-16 14:29:27 -0300

    Created the ResourceQuota class into api.http.response.quotas package

---
+++ src/main/java/cloud/fogbow/ras/api/http/response/quotas/ResourceQuota.java
@@ -0,0 +1,59 @@
+package cloud.fogbow.ras.api.http.response.quotas;
+
+import com.google.common.annotations.VisibleForTesting;
+
+import cloud.fogbow.ras.api.http.response.quotas.allocation.Allocation;
+import cloud.fogbow.ras.api.http.response.quotas.allocation.ResourceAllocation;
+
+public class ResourceQuota extends Quota {
+
+    private ResourceAllocation totalQuota;
+    private ResourceAllocation usedQuota;
+    private ResourceAllocation availableQuota;
+    
+    public ResourceQuota() {}
+
+    @Override
+    public Allocation getTotalQuota() {
+        return this.totalQuota;
+    }
+
+    @Override
+    public Allocation getUsedQuota() {
+        return this.usedQuota;
+    }
+
+    @Override
+    public Allocation getAvailableQuota() {
+        return this.availableQuota;
+    }
+
+    @Override
+    public String toString() {
+        return "ResourceQuota { totalQuota = " + totalQuota 
+                + ", usedQuota = " + usedQuota 
+                + ", availableQuota = " + availableQuota + " }";
+    }
+    
+    @VisibleForTesting
+    ResourceAllocation calculateQuota(ResourceAllocation totalQuota, ResourceAllocation usedQuota) {
+        int instance = totalQuota.getInstances() - usedQuota.getInstances();
+        int vCPU = totalQuota.getvCPU() - usedQuota.getvCPU();
+        int ram = totalQuota.getRam() - usedQuota.getRam();
+        int disk = totalQuota.getDisk() - usedQuota.getDisk();
+        int networks = totalQuota.getNetworks() - usedQuota.getNetworks();
+        int publicIps = totalQuota.getPublicIps() - usedQuota.getPublicIps();
+        
+        ResourceAllocation resourceAllocation = ResourceAllocation.builder()
+                .instances(instance)
+                .vCPU(vCPU)
+                .ram(ram)
+                .disk(disk)
+                .networks(networks)
+                .publicIps(publicIps)
+                .build();
+        
+        return resourceAllocation;
+    }
+    
+}
