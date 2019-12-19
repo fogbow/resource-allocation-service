d75ab1661da1483c5cc09d20c839b20596683388
wassoares <wlfsoares@gmail.com>
2019-12-19 14:23:58 -0300

    Created the GetVolumeQuotasResponse class

---
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/openstack/quota/v2/GetVolumeQuotasResponse.java
@@ -0,0 +1,10 @@
+package cloud.fogbow.ras.core.plugins.interoperability.openstack.quota.v2;
+
+public class GetVolumeQuotasResponse {
+
+    public static GetVolumeQuotasResponse fromJson(String response) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
