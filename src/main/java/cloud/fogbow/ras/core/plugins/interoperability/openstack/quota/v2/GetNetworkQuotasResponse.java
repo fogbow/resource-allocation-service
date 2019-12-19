bbaea4de9a4b4c9eb5ecff443185f1c6021e3a19
wassoares <wlfsoares@gmail.com>
2019-12-19 14:23:35 -0300

    Created the GetNetworkQuotasResponse class

---
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/openstack/quota/v2/GetNetworkQuotasResponse.java
@@ -0,0 +1,10 @@
+package cloud.fogbow.ras.core.plugins.interoperability.openstack.quota.v2;
+
+public class GetNetworkQuotasResponse {
+
+    public static GetNetworkQuotasResponse fromJson(String response) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
