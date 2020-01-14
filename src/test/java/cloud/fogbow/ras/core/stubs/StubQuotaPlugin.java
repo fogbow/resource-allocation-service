e01db04468de63815f13fda72e77ecd5bea16c9c
Paulo Ewerton <pauloewerton@lsd.ufcg.edu.br>
2020-01-14 15:48:12 -0300

    Adding StubQuotaPlugin

---
+++ src/test/java/cloud/fogbow/ras/core/stubs/StubQuotaPlugin.java
@@ -0,0 +1,20 @@
+package cloud.fogbow.ras.core.stubs;
+
+import cloud.fogbow.common.models.CloudUser;
+import cloud.fogbow.ras.api.http.response.quotas.ResourceQuota;
+import cloud.fogbow.ras.core.plugins.interoperability.QuotaPlugin;
+
+/**
+ * This class is a stub for the QuotaPlugin interface used for tests only.
+ * Should not have a proper implementation.
+ */
+public class StubQuotaPlugin implements QuotaPlugin<CloudUser> {
+
+    public StubQuotaPlugin(String confFilePath) {
+    }
+
+    @Override
+    public ResourceQuota getUserQuota(CloudUser cloudUser) {
+        return null;
+    }
+}
