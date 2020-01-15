fe5cf8ae1542d05c2e5f66d980e4a190534ab6c1
fgan1 <fgan1@hotmail.com>
2020-01-15 12:05:51 -0300

    Add AzureGeneralPolicy

---
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/azure/util/AzureGeneralPolicy.java
@@ -0,0 +1,39 @@
+package cloud.fogbow.ras.core.plugins.interoperability.azure.util;
+
+import cloud.fogbow.common.exceptions.InvalidParameterException;
+import cloud.fogbow.ras.constants.Messages;
+import cloud.fogbow.ras.core.models.orders.ComputeOrder;
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class AzureGeneralPolicy {
+
+    static final int MINIMUM_DISK = 30;
+    static final String PASSWORD_PREFIX = "P4ss@";
+
+    /**
+     * Azure Password Policy:
+     * 1) Contains an uppercase character
+     * 2) Contains a lowercase character
+     * 3) Contains a numeric digit
+     * 4) Contains a special character
+     * 5) Control characters are not allowed
+     */
+    public static String generatePassword() {
+        return PASSWORD_PREFIX + RandomStringUtils.randomAlphabetic(PASSWORD_PREFIX.length());
+    }
+
+    /**
+     * Azure Disk Policy
+     * 1) Greater than 30GB
+     */
+    public static int getDisk(ComputeOrder computeOrder) throws InvalidParameterException {
+        int disk = computeOrder.getDisk();
+        if (disk < MINIMUM_DISK) {
+            throw new InvalidParameterException(
+                    String.format(Messages.Error.ERROR_DISK_PARAMETER_AZURE_POLICY, MINIMUM_DISK));
+        }
+
+        return disk;
+    }
+
+}
