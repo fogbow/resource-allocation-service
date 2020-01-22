c48082e72410db2e88a3f5a3367165c1dc35cc37
Jadson Luan <jadson.silva@ccc.ufcg.edu.br>
2020-01-22 10:51:55 -0300

    Added new volume allocation endpoint

--- src/main/java/cloud/fogbow/ras/api/http/request/Volume.java
+++ src/main/java/cloud/fogbow/ras/api/http/request/Volume.java
@@ -5,6 +5,7 @@ import cloud.fogbow.ras.api.http.CommonKeys;
 import cloud.fogbow.ras.api.http.response.ResourceId;
 import cloud.fogbow.ras.api.http.response.InstanceStatus;
 import cloud.fogbow.ras.api.http.response.VolumeInstance;
+import cloud.fogbow.ras.api.http.response.quotas.allocation.VolumeAllocation;
 import cloud.fogbow.ras.constants.ApiDocumentation;
 import cloud.fogbow.ras.constants.Messages;
 import cloud.fogbow.ras.constants.SystemConstants;
@@ -28,6 +29,8 @@ public class Volume {
     public static final String VOLUME_SUFFIX_ENDPOINT = "volumes";
     public static final String VOLUME_ENDPOINT = SystemConstants.SERVICE_BASE_ENDPOINT + VOLUME_SUFFIX_ENDPOINT;
     public static final String ORDER_CONTROLLER_TYPE = "volume";
+    public static final String ALLOCATION_SUFFIX_ENDPOINT = "allocation";
+    public static final String VOLUME_ALLOCATION_RESOURCE = "volume allocation";
 
     private final Logger LOGGER = Logger.getLogger(Volume.class);
 
@@ -101,6 +104,29 @@ public class Volume {
             LOGGER.info(String.format(Messages.Info.RECEIVING_DELETE_REQUEST, ORDER_CONTROLLER_TYPE, volumeId));
             ApplicationFacade.getInstance().deleteVolume(volumeId, systemUserToken);
             return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            LOGGER.debug(String.format(Messages.Exception.GENERIC_EXCEPTION, e.getMessage()), e);
+            throw e;
+        }
+    }
+
+    //
+    @ApiOperation(value = ApiDocumentation.Volume.GET_ALLOCATION)
+    @RequestMapping(value = "/" + ALLOCATION_SUFFIX_ENDPOINT + "/{providerId:.+}" + "/{cloudName}", method = RequestMethod.GET)
+    public ResponseEntity<VolumeAllocation> getUserAllocation(
+            @ApiParam(value = ApiDocumentation.CommonParameters.PROVIDER_ID)
+            @PathVariable String providerId,
+            @ApiParam(value = ApiDocumentation.CommonParameters.CLOUD_NAME)
+            @PathVariable String cloudName,
+            @ApiParam(value = cloud.fogbow.common.constants.ApiDocumentation.Token.SYSTEM_USER_TOKEN)
+            @RequestHeader(required = false, value = CommonKeys.SYSTEM_USER_TOKEN_HEADER_KEY) String systemUserToken)
+            throws FogbowException {
+
+        try {
+            LOGGER.info(String.format(Messages.Info.RECEIVING_RESOURCE_S_REQUEST, VOLUME_ALLOCATION_RESOURCE, providerId));
+            VolumeAllocation volumeAllocation =
+                    ApplicationFacade.getInstance().getVolumeAllocation(providerId, cloudName, systemUserToken);
+            return new ResponseEntity<>(volumeAllocation, HttpStatus.OK);
         } catch (Exception e) {
             LOGGER.debug(String.format(Messages.Exception.GENERIC_EXCEPTION, e.getMessage()), e);
             throw e;
