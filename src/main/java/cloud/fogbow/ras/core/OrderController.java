e167b31e636be7227cb739218789824c2768ac89
Jadson Luan <jadson.silva@ccc.ufcg.edu.br>
2020-02-03 11:43:13 -0300

    Refactored getUserAllocation method

--- src/main/java/cloud/fogbow/ras/core/OrderController.java
+++ src/main/java/cloud/fogbow/ras/core/OrderController.java
@@ -13,6 +13,7 @@ import cloud.fogbow.ras.core.models.Operation;
 import cloud.fogbow.ras.core.models.ResourceType;
 import cloud.fogbow.ras.core.models.UserData;
 import cloud.fogbow.ras.core.models.orders.*;
+import com.google.common.annotations.VisibleForTesting;
 import org.apache.log4j.Logger;
 
 import java.util.*;
@@ -178,32 +179,31 @@ public class OrderController {
 
         switch (resourceType) {
             case COMPUTE:
-                List<ComputeOrder> computeOrders = new ArrayList<>();
-                for (Order order : filteredOrders) {
-                    computeOrders.add((ComputeOrder) order);
-                }
+                List<ComputeOrder> computeOrders = castOrders(filteredOrders);
                 return getUserComputeAllocation(computeOrders);
             case VOLUME:
-                List<VolumeOrder> volumeOrders = new ArrayList<>();
-                for (Order order : filteredOrders) {
-                    volumeOrders.add((VolumeOrder) order);
-                }
+                List<VolumeOrder> volumeOrders = castOrders(filteredOrders);
                 return getUserVolumeAllocation(volumeOrders);
             case NETWORK:
-                List<NetworkOrder> networkOrders = new ArrayList<>();
-                for (Order order : filteredOrders) {
-                    networkOrders.add((NetworkOrder) order);
-                }
+                List<NetworkOrder> networkOrders = castOrders(filteredOrders);
                 return getUserNetworkAllocation(networkOrders);
             case PUBLIC_IP:
-                List<PublicIpOrder> publicIpOrders = new ArrayList<>();
-                for (Order order : filteredOrders) {
-                    publicIpOrders.add((PublicIpOrder) order);
-                }
+                List<PublicIpOrder> publicIpOrders = castOrders(filteredOrders);
                 return getUserPublicIpAllocation(publicIpOrders);
             default:
                 throw new UnexpectedException(Messages.Exception.RESOURCE_TYPE_NOT_IMPLEMENTED);
         }
+    }
+
+    @VisibleForTesting
+    <T extends Order> List<T> castOrders(List<Order> orders) {
+        List<T> result = new ArrayList<>();
+
+        for (Order order : orders) {
+            result.add((T) order);
+        }
+
+        return result;
     }
 
     private PublicIpAllocation getUserPublicIpAllocation(List<PublicIpOrder> publicIpOrders) {
