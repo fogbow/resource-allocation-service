97a6ffe6c80c4e46f447eb67e04161cc7a5bc3c5
Jadson Luan <jadson.silva@ccc.ufcg.edu.br>
2020-02-20 15:25:26 -0300

    Add setIdentityProviderPlugin for testing purposes

--- src/main/java/cloud/fogbow/ras/core/plugins/mapper/all2one/AzureAllToOneMapper.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/mapper/all2one/AzureAllToOneMapper.java
@@ -7,6 +7,7 @@ import cloud.fogbow.common.exceptions.FatalErrorException;
 import cloud.fogbow.common.exceptions.FogbowException;
 import cloud.fogbow.common.models.AzureUser;
 import cloud.fogbow.common.plugins.cloudidp.azure.AzureIdentityProviderPlugin;
+import com.google.common.annotations.VisibleForTesting;
 
 public class AzureAllToOneMapper extends GenericAllToOneSystemToCloudMapper<AzureUser, AzureSystemUser> {
 
@@ -22,4 +23,8 @@ public class AzureAllToOneMapper extends GenericAllToOneSystemToCloudMapper<Azur
         return this.plugin.getCloudUser(credentials);
     }
 
+    @VisibleForTesting
+    void setIdentityProviderPlugin(AzureIdentityProviderPlugin plugin) {
+        this.plugin = plugin;
+    }
 }
