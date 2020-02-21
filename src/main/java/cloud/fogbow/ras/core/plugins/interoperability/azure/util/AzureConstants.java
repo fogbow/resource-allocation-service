09bbb6bd002407e8786ab00ea3573149343d846e
wassoares <wlfsoares@gmail.com>
2020-02-21 15:13:02 -0300

    Fixed constants into AzureConstants class

--- src/main/java/cloud/fogbow/ras/core/plugins/interoperability/azure/util/AzureConstants.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/azure/util/AzureConstants.java
@@ -3,7 +3,7 @@ package cloud.fogbow.ras.core.plugins.interoperability.azure.util;
 public class AzureConstants {
 
     public static final String DEFAULT_NETWORK_INTERFACE_NAME_KEY = "default_network_interface_name";
-    public static final String REGION_NAME_KEY = "region_name";
-    public static final String RESOURCE_GROUP_NAME_KEY = "resource_group_name";
+    public static final String DEFAULT_REGION_NAME_KEY = "default_region_name";
+    public static final String DEFAULT_RESOURCE_GROUP_NAME_KEY = "default_resource_group_name";
 
 }
