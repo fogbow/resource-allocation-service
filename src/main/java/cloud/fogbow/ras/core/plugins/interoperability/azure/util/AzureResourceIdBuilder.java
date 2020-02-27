b08f0964f71e96bd576f63e233926e276066ebd6
wassoares <wlfsoares@gmail.com>
2020-02-27 13:17:46 -0300

    Refactoring and fixes made in the AzureResourceIdBuilder class

--- src/main/java/cloud/fogbow/ras/core/plugins/interoperability/azure/util/AzureResourceIdBuilder.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/azure/util/AzureResourceIdBuilder.java
@@ -1,58 +1,49 @@
 package cloud.fogbow.ras.core.plugins.interoperability.azure.util;
 
+import cloud.fogbow.common.constants.AzureConstants;
 import cloud.fogbow.common.exceptions.InvalidParameterException;
-import cloud.fogbow.common.models.AzureUser;
 import cloud.fogbow.ras.constants.Messages;
 import cloud.fogbow.ras.core.models.orders.Order;
-import com.google.common.annotations.VisibleForTesting;
 
-public class AzureIdBuilder {
+public class AzureResourceIdBuilder {
 
-    // /subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Compute/virtualMachines/{virtualMachineName}
-    @VisibleForTesting
-    public static String VIRTUAL_MACHINE_STRUCTURE = "/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Compute/virtualMachines/%s";
-
-    // /subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Network/networkInterfaces/{networkInterfaceName}
-    @VisibleForTesting
-    public static String NETWORK_INTERFACE_STRUCTURE = "/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Network/networkInterfaces/%s";
-
-    // /subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Compute/disks/{diskName}
-    @VisibleForTesting
-    public static final String BIGGER_STRUCTURE = NETWORK_INTERFACE_STRUCTURE;
-
-    public static AzureIdBuilderConfigured configure(AzureUser azureCloudUser) {
-        return new AzureIdBuilderConfigured(azureCloudUser);
+    public static AzureResourceIdConfigured configure() {
+        return new AzureResourceIdConfigured();
+    }
+    
+    public static AzureResourceIdConfigured configure(String structure) {
+        return new AzureResourceIdConfigured(structure);
     }
 
-    public static class AzureIdBuilderConfigured {
-        private AzureUser azureUser;
+    public static class AzureResourceIdConfigured {
+        private String structure;
+        private String subscriptionId;
         private String resourceGroupName;
         private String resourceName;
-        private String structure;
-
-        public AzureIdBuilderConfigured(AzureUser azureCloudUser) {
-            this.azureUser = azureCloudUser;
+        
+        public AzureResourceIdConfigured() {}
+        
+        public AzureResourceIdConfigured(String structure) {
+            this.structure = structure;
         }
         
-        public AzureIdBuilderConfigured resourceGroupName(String resourceGroupName) {
+        public AzureResourceIdConfigured withSubscriptionId(String subscriptionId) {
+            this.subscriptionId = subscriptionId;
+            return this;
+        }
+
+        public AzureResourceIdConfigured withResourceGroupName(String resourceGroupName) {
             this.resourceGroupName = resourceGroupName;
             return this;
         }
 
-        public AzureIdBuilderConfigured resourceName(String resourceName) {
+        public AzureResourceIdConfigured withResourceName(String resourceName) {
             this.resourceName = resourceName;
             return this;
         }
 
-        public AzureIdBuilderConfigured structure(String structure) {
-            this.structure = structure;
-            return this;
-        }
-
-        public String build() {
-            String subscriptionId = this.azureUser.getSubscriptionId();
-            String resourceGroupName = this.resourceGroupName;
-            return String.format(this.structure, subscriptionId, resourceGroupName, this.resourceName);
+        public String buildResourceId() {
+            return String.format(this.structure, this.subscriptionId, this.resourceGroupName, this.resourceName);
         }
 
         /**
@@ -60,10 +51,9 @@ public class AzureIdBuilder {
          * It happens because the resource name makes up the instance Id. Also, it might happen due
          * to the fact that the user choose some values such as resourceName and resourceGroupName.
          */
-        public void checkIdSizePolicy(String resourceName) throws InvalidParameterException {
-            this.resourceName(resourceName);
-            this.structure(BIGGER_STRUCTURE);
-            String idBuilt = this.build();
+        public void checkIdSizePolicy() throws InvalidParameterException {
+            this.structure = AzureConstants.BIGGER_STRUCTURE;
+            String idBuilt = this.buildResourceId();
             int sizeExceeded = idBuilt.length() - Order.FIELDS_MAX_SIZE;
             if (sizeExceeded > 0) {
                 throw new InvalidParameterException(
