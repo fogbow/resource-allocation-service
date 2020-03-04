a8dd94561552587f8cb5625739c84eb23a85de6f
Jadson Luan <jadson.silva@ccc.ufcg.edu.br>
2020-03-04 15:53:41 -0300

    Refactor AzureQuotaPlugin class

--- src/main/java/cloud/fogbow/ras/core/plugins/interoperability/azure/quota/AzureQuotaPlugin.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/azure/quota/AzureQuotaPlugin.java
@@ -40,28 +40,25 @@ public class AzureQuotaPlugin implements QuotaPlugin<AzureUser> {
     private static final int MAXIMUM_STORAGE_ACCOUNT_CAPACITY = 50 * ONE_PETABYTE_IN_GIGABYTES;
 
     private final String defaultRegionName;
-    private final String defaultResourceGroup;
 
     public AzureQuotaPlugin(@NotNull String confFilePath) {
         Properties properties = PropertiesUtil.readProperties(confFilePath);
         this.defaultRegionName = properties.getProperty(AzureConstants.DEFAULT_REGION_NAME_KEY);
-        this.defaultResourceGroup = properties.getProperty(AzureConstants.DEFAULT_RESOURCE_GROUP_NAME_KEY);
     }
 
     @Override
     public ResourceQuota getUserQuota(AzureUser cloudUser) throws FogbowException {
         Azure azure = AzureClientCacheManager.getAzure(cloudUser);
 
-        Map<String, ComputeUsage> computeUsages = this.getComputeUsages(azure);
-        Map<String, NetworkUsage> networkUsages = this.getNetworkUsage(azure);
-        PagedList<Disk> disks = getDisks(azure);
+        Map<String, ComputeUsage> computeUsages = this.getComputeUsageMap(azure);
+        Map<String, NetworkUsage> networkUsages = this.getNetworkUsageMap(azure);
+        PagedList<Disk> disks = this.getDisks(azure);
 
         ResourceAllocation totalQuota = this.getTotalQuota(computeUsages, networkUsages);
         ResourceAllocation usedQuota = this.getUsedQuota(computeUsages, networkUsages, disks, azure);
         return new ResourceQuota(totalQuota, usedQuota);
     }
 
-    // USED QUOTA AUXILIARY METHODS
     @VisibleForTesting
     ResourceAllocation getUsedQuota(Map<String, ComputeUsage> computeUsages, Map<String, NetworkUsage> networkUsages, PagedList<Disk> disks, Azure azure) {
         ComputeAllocation computeAllocation = this.getUsedComputeAllocation(computeUsages, azure);
@@ -73,49 +70,43 @@ public class AzureQuotaPlugin implements QuotaPlugin<AzureUser> {
 
     @VisibleForTesting
     VolumeAllocation getUsedVolumeAllocation(PagedList<Disk> disks) {
-        Optional<Integer> diskUsage = disks.stream()
-                .map(disk -> disk.sizeInGB())
-                .reduce((accumulated, current) -> accumulated + current);
-
-        int storage = diskUsage.isPresent() ? diskUsage.get().intValue() : NO_USAGE;
+        int initialValue = NO_USAGE;
         int volumes = disks.size();
+        int storage = disks.stream()
+                .map(disk -> disk.sizeInGB())
+                .reduce(initialValue, Integer::sum);
         return new VolumeAllocation(volumes, storage);
     }
 
     @VisibleForTesting
     NetworkAllocation getUsedNetworkAllocation(Map<String, NetworkUsage> networkUsages) {
-        int instances = 0;
         NetworkUsage networkUsage = networkUsages.get(QUOTA_NETWORK_INSTANCES);
-        if (networkUsage != null) instances = (int) networkUsage.currentValue();
+        int instances = networkUsage == null ? NO_USAGE : (int) networkUsage.currentValue();
         return new NetworkAllocation(instances);
     }
 
     @VisibleForTesting
     ComputeAllocation getUsedComputeAllocation(Map<String, ComputeUsage> computeUsages, Azure azure) {
-        int instances = NO_USAGE;
-        int cores = NO_USAGE;
-        int ram = this.getMemoryUsage(azure);
-
         ComputeUsage vmUsage = computeUsages.get(QUOTA_VM_INSTANCES_KEY);
-        if (vmUsage != null) instances = vmUsage.currentValue();
-
         ComputeUsage coreUsage = computeUsages.get(QUOTA_VM_CORES_KEY);
-        if (coreUsage != null) cores = coreUsage.currentValue();
+
+        int instances = vmUsage == null ? NO_USAGE : vmUsage.currentValue();
+        int cores = coreUsage == null ? NO_USAGE : coreUsage.currentValue();
+        int ram = this.getMemoryUsage(azure);
 
         return new ComputeAllocation(cores, ram, instances);
     }
 
     @VisibleForTesting
     PublicIpAllocation getUsedPublicIpAllocation(Map<String, NetworkUsage> networkUsages) {
-        int instances = 0;
         NetworkUsage publicIpUsage = networkUsages.get(QUOTA_PUBLIC_IP_ADDRESSES);
-        if (publicIpUsage != null) instances = (int) publicIpUsage.currentValue();
+        int instances = publicIpUsage == null ? NO_USAGE : (int) publicIpUsage.currentValue();
         return new PublicIpAllocation(instances);
     }
 
-    // TOTAL QUOTA AUXILIARY METHODS
     @VisibleForTesting
-    ResourceAllocation getTotalQuota(Map<String, ComputeUsage> computeUsages, Map<String, NetworkUsage> networkUsages) {
+    ResourceAllocation getTotalQuota(Map<String, ComputeUsage> computeUsages,
+                                     Map<String, NetworkUsage> networkUsages) {
         ComputeAllocation computeAllocation = this.getTotalComputeAllocation(computeUsages);
         NetworkAllocation networkAllocation = this.getTotalNetworkAllocation(networkUsages);
         PublicIpAllocation publicIpAllocation = this.getTotalPublicIpAllocation(networkUsages);
@@ -132,36 +123,30 @@ public class AzureQuotaPlugin implements QuotaPlugin<AzureUser> {
 
     @VisibleForTesting
     PublicIpAllocation getTotalPublicIpAllocation(Map<String, NetworkUsage> networkUsages) {
-        int instances = 0;
         NetworkUsage publicIpUsage = networkUsages.get(QUOTA_PUBLIC_IP_ADDRESSES);
-        if (publicIpUsage != null) instances = (int) publicIpUsage.limit();
+        int instances = publicIpUsage == null ? NO_USAGE : (int) publicIpUsage.limit();
         return new PublicIpAllocation(instances);
     }
 
     @VisibleForTesting
     NetworkAllocation getTotalNetworkAllocation(Map<String, NetworkUsage> networkUsages) {
-        int instances = 0;
         NetworkUsage networkUsage = networkUsages.get(QUOTA_NETWORK_INSTANCES);
-        if (networkUsage != null) instances = (int) networkUsage.limit();
+        int instances = networkUsage == null ? NO_USAGE : (int) networkUsage.limit();
         return new NetworkAllocation(instances);
     }
 
     @VisibleForTesting
     ComputeAllocation getTotalComputeAllocation(Map<String, ComputeUsage> computeUsages) {
-        int instances = 0;
-        int cores = 0;
-        int ram = -1;
-
         ComputeUsage vmUsage = computeUsages.get(QUOTA_VM_INSTANCES_KEY);
-        if (vmUsage != null) instances = (int) vmUsage.limit();
-
         ComputeUsage coreUsage = computeUsages.get(QUOTA_VM_CORES_KEY);
-        if (coreUsage != null) cores = (int) coreUsage.limit();
+
+        int instances = vmUsage == null ? NO_USAGE : (int) vmUsage.limit();
+        int cores = coreUsage == null ? NO_USAGE : (int) coreUsage.limit();
+        int ram = FogbowConstants.UNLIMITED_RESOURCE;
 
         return new ComputeAllocation(cores, ram, instances);
     }
 
-    // COMMON AUXILIARY METHODS
     @VisibleForTesting
     ResourceAllocation buildQuota(ComputeAllocation computeAllocation,
                                   NetworkAllocation networkAllocation,
@@ -187,51 +172,62 @@ public class AzureQuotaPlugin implements QuotaPlugin<AzureUser> {
     }
 
     @VisibleForTesting
-    Map<String, ComputeUsage> getComputeUsages(Azure azure) {
+    Map<String, ComputeUsage> getComputeUsageMap(Azure azure) {
         Map<String, ComputeUsage> computeUsageMap = new HashMap<>();
+        List<String> validComputeUsages = Arrays.asList(QUOTA_VM_INSTANCES_KEY, QUOTA_VM_CORES_KEY);
 
-        PagedList<ComputeUsage> computeUsages = azure.computeUsages().listByRegion(this.defaultRegionName);
-        computeUsages.stream()
-                .filter(computeUsage -> {
-                    String name = computeUsage.name().value();
-                    return name.equals(QUOTA_VM_INSTANCES_KEY) || name.equals(QUOTA_VM_CORES_KEY);
-                })
+        this.getComputeUsage(azure)
+                .stream()
+                .filter(computeUsage -> validComputeUsages.contains(computeUsage.name().value()))
                 .forEach(computeUsage -> computeUsageMap.put(computeUsage.name().value(), computeUsage));
 
         return computeUsageMap;
     }
 
     @VisibleForTesting
-    Map<String, NetworkUsage> getNetworkUsage(Azure azure) {
-        Map<String, NetworkUsage> networkUsageMap = new HashMap<>();
+    PagedList<ComputeUsage> getComputeUsage(Azure azure) {
+        return azure.computeUsages().listByRegion(this.defaultRegionName);
+    }
 
-        PagedList<NetworkUsage> networkUsages = azure.networkUsages().listByRegion(this.defaultRegionName);
-        networkUsages.stream()
-                .filter(networkUsage -> {
-                    String name = networkUsage.name().value();
-                    return name.equals(QUOTA_NETWORK_INSTANCES) || name.equals(QUOTA_PUBLIC_IP_ADDRESSES);
-                })
+    @VisibleForTesting
+    Map<String, NetworkUsage> getNetworkUsageMap(Azure azure) {
+        Map<String, NetworkUsage> networkUsageMap = new HashMap<>();
+        List<String> validNetworkUsages = Arrays.asList(QUOTA_NETWORK_INSTANCES, QUOTA_PUBLIC_IP_ADDRESSES);
+
+        this.getNetworkUsage(azure)
+                .stream()
+                .filter(networkUsage -> validNetworkUsages.contains(networkUsage.name().value()))
                 .forEach(networkUsage -> networkUsageMap.put(networkUsage.name().value(), networkUsage));
 
         return networkUsageMap;
     }
 
     @VisibleForTesting
-    int getMemoryUsage(Azure azure) {
-        List<String> sizeNames = this.getVirtualMachineSizeNames(azure);
-        Map<String, VirtualMachineSize> sizesInUsage = this.getVirtualMachineSizesInUse(sizeNames, azure);
-        Optional<Integer> totalMemoryUsage = this.doGetMemoryUsage(sizeNames, sizesInUsage);
-        return totalMemoryUsage.isPresent() ? totalMemoryUsage.get().intValue() : NO_USAGE;
+    PagedList<NetworkUsage> getNetworkUsage(Azure azure) {
+        return azure.networkUsages().listByRegion(this.defaultRegionName);
     }
 
     @VisibleForTesting
-    Optional<Integer> doGetMemoryUsage(List<String> sizeNames, Map<String, VirtualMachineSize> sizesInUsage) {
-        return sizeNames.stream()
-                .map(sizeName -> {
-                    VirtualMachineSize virtualMachineSize = sizesInUsage.get(sizeName);
-                    return virtualMachineSize != null ? virtualMachineSize.memoryInMB() : NO_USAGE;
-                })
-                .reduce((accumulated, current) -> accumulated + current);
+    int getMemoryUsage(Azure azure) {
+        List<String> sizeNamesInUse = this.getVirtualMachineSizeNamesInUse(azure);
+        Map<String, VirtualMachineSize> virtualMachineSizes = this.getVirtualMachineSizes(sizeNamesInUse, azure);
+        return this.doGetMemoryUsage(sizeNamesInUse, virtualMachineSizes);
+    }
+
+    /**
+     * Return the total memory in use by all created virtual machines
+     *
+     * @param sizeNamesInUse list of virtual machine size names
+     * @param virtalMachineSizes list of virtual machine sizes in use
+     * @return total memory in use
+     */
+    @VisibleForTesting
+    int doGetMemoryUsage(List<String> sizeNamesInUse, Map<String, VirtualMachineSize> virtalMachineSizes) {
+        Integer initialValue = NO_USAGE;
+        return sizeNamesInUse.stream()
+                .map(sizeName -> virtalMachineSizes.get(sizeName))
+                .map(virtualMachineSize -> virtualMachineSize.memoryInMB())
+                .reduce(initialValue, Integer::sum);
     }
 
     /**
@@ -241,7 +237,7 @@ public class AzureQuotaPlugin implements QuotaPlugin<AzureUser> {
      * @return map where
      */
     @VisibleForTesting
-    Map<String, VirtualMachineSize> getVirtualMachineSizesInUse(List<String> sizeNames, Azure azure) {
+    Map<String, VirtualMachineSize> getVirtualMachineSizes(List<String> sizeNames, Azure azure) {
         Map<String, VirtualMachineSize> sizes = new HashMap<>();
         this.getVirtualMachineSizes(azure)
                 .stream()
@@ -256,7 +252,7 @@ public class AzureQuotaPlugin implements QuotaPlugin<AzureUser> {
      * @return a list of virtual machine size names
      */
     @VisibleForTesting
-    List<String> getVirtualMachineSizeNames(Azure azure) {
+    List<String> getVirtualMachineSizeNamesInUse(Azure azure) {
         return this.getVirtualMachines(azure)
                 .stream()
                 .map(virtualMachine -> virtualMachine.size().toString())
