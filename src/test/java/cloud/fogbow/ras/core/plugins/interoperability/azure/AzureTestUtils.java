b988a360e17d38d5413fe38b8b1d500afb41ac6b
wassoares <wlfsoares@gmail.com>
2020-03-04 15:14:00 -0300

    Added new constants into AzureTestUtils class

--- src/test/java/cloud/fogbow/ras/core/plugins/interoperability/azure/AzureTestUtils.java
+++ src/test/java/cloud/fogbow/ras/core/plugins/interoperability/azure/AzureTestUtils.java
@@ -1,18 +1,28 @@
 package cloud.fogbow.ras.core.plugins.interoperability.azure;
 
-import cloud.fogbow.common.models.AzureUser;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import cloud.fogbow.common.models.AzureUser;
+
+@RunWith(PowerMockRunner.class)
 public class AzureTestUtils {
 
     public static final String AZURE_CLOUD_NAME = "azure";
     public static final String DEFAULT_RESOURCE_GROUP_NAME = "default-resource-group-name";
     public static final String DEFAULT_REGION_NAME = "eastus";
-    private static final String SUBSCRIPTION_ID_DEFAULT = "subscriptionId";
+    public static final String DEFAULT_SUBSCRIPTION_ID = "default-subscription-id";
+    public static final String ORDER_NAME = "order-name";
+    public static final String RESOURCE_ID = "resource-id";
+    public static final String RESOUCE_ID_PREFIX = "prefix-";
+    
+    public static final int BIGGEST_LENGTH = 100;
+    public static final int SIZE_EXCEEDED = 20;
 
     public static AzureUser createAzureUser() {
         AzureUser azureUser = Mockito.mock(AzureUser.class);
-        Mockito.when(azureUser.getSubscriptionId()).thenReturn(SUBSCRIPTION_ID_DEFAULT);
+        Mockito.when(azureUser.getSubscriptionId()).thenReturn(DEFAULT_SUBSCRIPTION_ID);
         return azureUser;
     }
 
