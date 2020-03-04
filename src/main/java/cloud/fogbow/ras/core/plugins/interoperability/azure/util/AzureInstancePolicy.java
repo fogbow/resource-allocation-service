a9e5fc99721504ad47f4d23539c077816844b791
wassoares <wlfsoares@gmail.com>
2020-03-04 15:08:35 -0300

    Fixes made into AzureInstancePolicy class

--- src/main/java/cloud/fogbow/ras/core/plugins/interoperability/azure/util/AzureInstancePolicy.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/azure/util/AzureInstancePolicy.java
@@ -3,53 +3,47 @@ package cloud.fogbow.ras.core.plugins.interoperability.azure.util;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import com.google.common.annotations.VisibleForTesting;
+import com.microsoft.azure.management.resources.fluentcore.utils.SdkContext;
+import com.sun.istack.Nullable;
+
 import cloud.fogbow.common.constants.AzureConstants;
 import cloud.fogbow.common.exceptions.InvalidParameterException;
-import cloud.fogbow.common.models.AzureUser;
-import cloud.fogbow.ras.constants.SystemConstants;
 import cloud.fogbow.ras.core.models.orders.ComputeOrder;
 
 public class AzureInstancePolicy {
 
+    private static final int MAXIMUM_ID_LENGTH = 20;
+
     public static String defineAzureResourceName(
-            @NotNull ComputeOrder computeOrder,
-            @NotNull AzureUser azureCloudUser, 
-            @NotBlank String resourceGroupName) throws InvalidParameterException {
-
-        String subscriptionId = azureCloudUser.getSubscriptionId();
-        return defineAzureResourceName(computeOrder.getId(), resourceGroupName, subscriptionId);
+            @NotNull ComputeOrder computeOrder) throws InvalidParameterException {
+        
+        String orderName = computeOrder.getName();
+        String resourceId = generateResourceId(AzureConstants.VIRTUAL_MACHINE_ID_PREFIX);
+        return defineAndCheckResourceName(resourceId, orderName);
     }
-
+    
+    public static String generateResourceId(@NotBlank String prefix) {
+        return SdkContext.randomResourceName(prefix, MAXIMUM_ID_LENGTH);
+    }
+    
     /**
      * Define the Azure Resource Name and check if It's in accordance with the policy.
      */
-    private static String defineAzureResourceName(
-            @NotBlank String orderId,
-            @NotBlank String resourceGroupName, 
-            @NotBlank String subscriptionId) throws InvalidParameterException {
+    @VisibleForTesting
+    static String defineAndCheckResourceName(
+            @NotBlank String resourceId,
+            @Nullable String orderName) throws InvalidParameterException {
 
-        String resourceName = SystemConstants.FOGBOW_INSTANCE_NAME_PREFIX + orderId;
+        String resourceName = resourceId 
+                + AzureConstants.RESOURCE_NAME_SEPARATOR 
+                + orderName;        
 
         AzureResourceIdBuilder.configure()
-                .withSubscriptionId(subscriptionId)
-                .withResourceGroupName(resourceGroupName)
                 .withResourceName(resourceName)
-                .checkIdSizePolicy();
+                .checkSizePolicy();
 
         return resourceName;
-    }
-
-    public static String generateFogbowInstanceId(
-            @NotNull ComputeOrder computeOrder, 
-            @NotNull AzureUser azureUser,
-            @NotBlank String resourceGroupName) throws InvalidParameterException {
-
-        String resourceName = defineAzureResourceName(computeOrder, azureUser, resourceGroupName);
-        return AzureResourceIdBuilder.configure(AzureConstants.VIRTUAL_MACHINE_STRUCTURE)
-                .withSubscriptionId(azureUser.getSubscriptionId())
-                .withResourceGroupName(resourceGroupName)
-                .withResourceName(resourceName)
-                .build();
     }
 
 }
