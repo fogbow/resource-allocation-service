0d326c7d7c28401cac2ebdec2bb6f2d8a32e901c
wassoares <wlfsoares@gmail.com>
2020-03-10 13:53:05 -0300

    Added implementation of volume resource type into map method from AzureStateMapper class

--- src/main/java/cloud/fogbow/ras/core/plugins/interoperability/azure/util/AzureStateMapper.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/azure/util/AzureStateMapper.java
@@ -4,6 +4,7 @@ import cloud.fogbow.ras.api.http.response.InstanceState;
 import cloud.fogbow.ras.constants.Messages;
 import cloud.fogbow.ras.core.models.ResourceType;
 import cloud.fogbow.ras.core.plugins.interoperability.azure.compute.AzureComputePlugin;
+import cloud.fogbow.ras.core.plugins.interoperability.azure.volume.AzureVolumePlugin;
 
 import org.apache.log4j.Logger;
 
@@ -12,6 +13,8 @@ public class AzureStateMapper {
     private static final Logger LOGGER = Logger.getLogger(AzureStateMapper.class);
 
     private static final String COMPUTE_PLUGIN = AzureComputePlugin.class.getSimpleName();
+    private static final String VOLUME_PLUGIN = AzureVolumePlugin.class.getSimpleName();
+    
     public static final String CREATING_STATE = "Creating";
     public static final String SUCCEEDED_STATE = "Succeeded";
     public static final String FAILED_STATE = "Failed";
@@ -29,6 +32,19 @@ public class AzureStateMapper {
                         return InstanceState.FAILED;
                     default:
                         LOGGER.error(String.format(Messages.Error.UNDEFINED_INSTANCE_STATE_MAPPING, state, COMPUTE_PLUGIN));
+                        return InstanceState.INCONSISTENT;
+                }
+            case VOLUME:
+                // cloud state values: [creating, succeeded]
+                switch (state) {
+                    case CREATING_STATE:
+                        return InstanceState.CREATING;
+                    case SUCCEEDED_STATE:
+                        return InstanceState.READY;
+                    case FAILED_STATE:
+                        return InstanceState.FAILED;
+                    default:
+                        LOGGER.error(String.format(Messages.Error.UNDEFINED_INSTANCE_STATE_MAPPING, state, VOLUME_PLUGIN));
                         return InstanceState.INCONSISTENT;
                 }
             default:
