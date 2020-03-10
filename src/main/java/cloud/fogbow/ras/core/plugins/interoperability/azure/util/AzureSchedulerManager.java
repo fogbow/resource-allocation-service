6e146190396b36450f5e4f3ccb260a4fbdec8471
wassoares <wlfsoares@gmail.com>
2020-03-10 13:46:20 -0300

    Added new method into AzureSchedulerManager class

--- src/main/java/cloud/fogbow/ras/core/plugins/interoperability/azure/util/AzureSchedulerManager.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/azure/util/AzureSchedulerManager.java
@@ -6,9 +6,14 @@ import java.util.concurrent.Executors;
 public class AzureSchedulerManager {
 
     private static final int VIRTUAL_MACHINE_POOL_SIZE = 2;
+    private static final int VOLUME_POOL_SIZE = 2;
 
     public static ExecutorService getVirtualMachineExecutor() {
         return Executors.newFixedThreadPool(VIRTUAL_MACHINE_POOL_SIZE);
+    }
+
+    public static ExecutorService getVolumeExecutor() {
+        return Executors.newFixedThreadPool(VOLUME_POOL_SIZE);
     }
 
 }
