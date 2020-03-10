a2dd67b108d8113756bc3a21739c2a3f547cbafb
wassoares <wlfsoares@gmail.com>
2020-03-10 13:43:33 -0300

    Added new constants into Messages class

--- src/main/java/cloud/fogbow/ras/constants/Messages.java
+++ src/main/java/cloud/fogbow/ras/constants/Messages.java
@@ -131,6 +131,7 @@ public class Messages {
         public static final String CREATING_AZURE_CLIENT = "Creating a new Azure client";
         public static final String SEEK_VIRTUAL_MACHINE_SIZE_BY_NAME =
                 "Seek for the Virtual Machine Size by name %s at region %s";
+        public static final String END_CREATE_DISK_ASYNC_BEHAVIOUR = "End asynchronous create disk";
         public static final String END_CREATE_VM_ASYNC_BEHAVIOUR = "End asynchronous create virtual machine";
         public static final String SEEK_VIRTUAL_MACHINE_SIZE_NAME =
                 "Seek for the Virtual Machine Size that fits with memory(%s) and vCpu(%s) at region %s";
@@ -202,6 +203,7 @@ public class Messages {
                 "The resource name exceeded %s characters of the limit";
         public static final String ERROR_DISK_PARAMETER_AZURE_POLICY = "The disk size must be greater than %sGB";;
         public static final String ERROR_MULTIPLE_NETWORKS_NOT_ALLOWED = "Multiple networks not allowed";
+        public static final String ERROR_CREATE_DISK_ASYNC_BEHAVIOUR = "Error while creating disk asynchronously";
         public static final String ERROR_CREATE_VM_ASYNC_BEHAVIOUR = "Error while creating virtual machine asynchronously";
         public static final String ERROR_DELETE_DISK_ASYNC_BEHAVIOUR = "Error while deleting disk asynchonously";
         public static final String ERROR_DELETE_VM_ASYNC_BEHAVIOUR = "Error while deleting virtual machine asynchronously";
