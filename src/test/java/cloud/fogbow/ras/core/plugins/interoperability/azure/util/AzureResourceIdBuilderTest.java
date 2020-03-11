b4b93b92a7f52b4e71f61b1ec90e9df9a6baafdd
wassoares <wlfsoares@gmail.com>
2020-03-11 14:56:47 -0300

    Fixed tests into AzureResourceIdBuilderTest class

--- src/test/java/cloud/fogbow/ras/core/plugins/interoperability/azure/util/AzureResourceIdBuilderTest.java
+++ src/test/java/cloud/fogbow/ras/core/plugins/interoperability/azure/util/AzureResourceIdBuilderTest.java
@@ -6,12 +6,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import com.microsoft.azure.management.resources.fluentcore.utils.SdkContext;
-
 import cloud.fogbow.common.constants.AzureConstants;
-import cloud.fogbow.common.exceptions.InvalidParameterException;
 import cloud.fogbow.common.models.AzureUser;
-import cloud.fogbow.ras.constants.Messages;
 import cloud.fogbow.ras.core.plugins.interoperability.azure.AzureTestUtils;
 
 public class AzureResourceIdBuilderTest extends AzureTestUtils {
@@ -39,7 +35,7 @@ public class AzureResourceIdBuilderTest extends AzureTestUtils {
                 networkInterfaceName);
 
         // exercise
-        String networkInterfaceId = AzureResourceIdBuilder.configure(AzureConstants.NETWORK_INTERFACE_STRUCTURE)
+        String networkInterfaceId = AzureResourceIdBuilder.networkInterfaceId()
                 .withSubscriptionId(this.azureUser.getSubscriptionId())
                 .withResourceGroupName(AzureTestUtils.DEFAULT_RESOURCE_GROUP_NAME)
                 .withResourceName(networkInterfaceName)
@@ -62,7 +58,7 @@ public class AzureResourceIdBuilderTest extends AzureTestUtils {
                 virtualMachineName);
 
         // exercise
-        String networkInterfaceId = AzureResourceIdBuilder.configure(AzureConstants.VIRTUAL_MACHINE_STRUCTURE)
+        String networkInterfaceId = AzureResourceIdBuilder.virtualMachineId()
                 .withSubscriptionId(this.azureUser.getSubscriptionId())
                 .withResourceGroupName(AzureTestUtils.DEFAULT_RESOURCE_GROUP_NAME)
                 .withResourceName(virtualMachineName)
@@ -70,30 +66,6 @@ public class AzureResourceIdBuilderTest extends AzureTestUtils {
 
         // verify
         Assert.assertEquals(virtualMachineIdExpected, networkInterfaceId);
-    }
-
-    // test case: When calling the checkIdSizePolicy method with resourceName out of the limit,
-    // it must verify if It throws an InvalidParameterException.
-    @Test
-    public void testCheckSizePolicyFail() {
-        // set up
-        String resourceName = getResourceNameOutOfLimit();
-        String expected = String.format(Messages.Error.ERROR_ID_LIMIT_SIZE_EXCEEDED, SIZE_EXCEEDED);
-        
-        try {
-            // exercise
-            AzureResourceIdBuilder.configure().withResourceName(resourceName).checkSizePolicy();
-            Assert.fail();
-        } catch (InvalidParameterException e) {
-            // verify
-            Assert.assertEquals(expected, e.getMessage());
-        }
-    }
-
-    private String getResourceNameOutOfLimit() {
-        String resourceId = AzureInstancePolicy.generateResourceId(RESOUCE_ID_PREFIX);
-        String orderName = SdkContext.randomResourceName(ORDER_NAME, BIGGEST_LENGTH);
-        return resourceId + AzureConstants.RESOURCE_NAME_SEPARATOR + orderName;
     }
 
 }
