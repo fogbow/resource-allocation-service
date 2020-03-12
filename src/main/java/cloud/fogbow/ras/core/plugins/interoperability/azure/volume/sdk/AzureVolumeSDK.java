ace4113c28e5524f6cc68ad59e97359bd5671f0c
wassoares <wlfsoares@gmail.com>
2020-03-12 15:36:08 -0300

    Changes made into AzureVolumeSDK class

--- src/main/java/cloud/fogbow/ras/core/plugins/interoperability/azure/volume/sdk/AzureVolumeSDK.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/azure/volume/sdk/AzureVolumeSDK.java
@@ -1,12 +1,21 @@
 package cloud.fogbow.ras.core.plugins.interoperability.azure.volume.sdk;
 
 import com.microsoft.azure.management.Azure;
+import com.microsoft.azure.management.compute.Disk;
+import com.microsoft.azure.management.resources.fluentcore.model.Creatable;
+import com.microsoft.azure.management.resources.fluentcore.model.Indexable;
+
 import rx.Completable;
+import rx.Observable;
 
 public class AzureVolumeSDK {
 
-    public static Completable buildDeleteDiskCompletable(Azure azure, String diskId) {
-        return azure.disks().deleteByIdAsync(diskId);
+    public static Observable<Indexable> buildCreateDiskObservable(Creatable<Disk> diskCreatable) {
+        return diskCreatable.createAsync();
+    }
+    
+    public static Completable buildDeleteDiskCompletable(Azure azure, String resourceId) {
+        return azure.disks().deleteByIdAsync(resourceId);
     }
 
 }
