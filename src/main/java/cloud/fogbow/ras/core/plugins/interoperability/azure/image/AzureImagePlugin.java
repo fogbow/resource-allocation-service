cdcd6fa6b00f65f824bc28050dcf1bf8843f3735
Jadson Luan <jadson.silva@ccc.ufcg.edu.br>
2020-03-12 17:34:14 -0300

    Finish getAllImages implementation into AzureImagePlugin

--- src/main/java/cloud/fogbow/ras/core/plugins/interoperability/azure/image/AzureImagePlugin.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/azure/image/AzureImagePlugin.java
@@ -1,66 +1,93 @@
-package cloud.fogbow.ras.core.plugins.interoperability.azure;
+package cloud.fogbow.ras.core.plugins.interoperability.azure.image;
 
 import cloud.fogbow.common.constants.AzureConstants;
+import cloud.fogbow.common.exceptions.FatalErrorException;
 import cloud.fogbow.common.exceptions.FogbowException;
 import cloud.fogbow.common.models.AzureUser;
 import cloud.fogbow.common.util.AzureClientCacheManager;
 import cloud.fogbow.common.util.PropertiesUtil;
 import cloud.fogbow.ras.api.http.response.ImageInstance;
 import cloud.fogbow.ras.api.http.response.ImageSummary;
+import cloud.fogbow.ras.constants.Messages;
 import cloud.fogbow.ras.core.plugins.interoperability.ImagePlugin;
-import cloud.fogbow.ras.core.plugins.interoperability.azure.compute.sdk.model.AzureGetImageRef;
+import cloud.fogbow.ras.core.plugins.interoperability.azure.util.AzureGeneralPolicy;
 import cloud.fogbow.ras.core.plugins.interoperability.azure.util.AzureImageOperationUtil;
-import com.microsoft.azure.PagedList;
+import com.google.common.annotations.VisibleForTesting;
 import com.microsoft.azure.management.Azure;
+import com.microsoft.azure.management.compute.VirtualMachineOffer;
 import com.microsoft.azure.management.compute.VirtualMachinePublisher;
+import com.microsoft.azure.management.compute.VirtualMachineSku;
 import org.apache.commons.lang.NotImplementedException;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
-import java.util.stream.Stream;
+import java.util.*;
 
 public class AzureImagePlugin implements ImagePlugin<AzureUser> {
 
     private final String defaultRegionName;
     private final List<String> publishers;
+    private final AzureImageOperation operation;
+    private static Map<String, ImageSummary> images = new HashMap<>();
 
     public AzureImagePlugin(String confFilePath) {
         Properties properties = PropertiesUtil.readProperties(confFilePath);
         this.defaultRegionName = properties.getProperty(AzureConstants.DEFAULT_REGION_NAME_KEY);
-        this.publishers = new ArrayList<>();
-        publishers.add("Canonical");
+        AzureGeneralPolicy.checkRegionName(defaultRegionName);
+        this.operation = new AzureImageOperation(this.defaultRegionName);
+        this.publishers = this.loadPublishers(properties);
+    }
+
+    @VisibleForTesting
+    List<String> loadPublishers(Properties properties) {
+        List<String> publishers = new ArrayList<>();
+        String publisherList = properties.getProperty(AzureConstants.IMAGES_PUBLISHERS_KEY);
+
+        if (publisherList == null || publisherList.isEmpty()) {
+            throw new FatalErrorException(Messages.Exception.NO_IMAGES_PUBLISHER);
+        }
+
+        for (String publisher : publisherList.split(",")) {
+            publisher = publisher.trim();
+            publishers.add(publisher);
+        }
+
+        return publishers;
     }
 
     @Override
     public List<ImageSummary> getAllImages(AzureUser cloudUser) throws FogbowException {
         Azure azure = AzureClientCacheManager.getAzure(cloudUser);
-        List<ImageSummary> images = new ArrayList<>();
 
-        Stream<VirtualMachinePublisher> publishersStream = this.getPublishers(azure).stream()
-                .filter(publisher -> publishers.contains(publisher.name()));
+        if (this.images.isEmpty()) {
+            this.images = this.loadImages(azure);
+        }
 
-        publishersStream.forEach(publisher -> {
-            publisher.offers().list().stream().forEach(offer -> {
-                offer.skus().list().stream().forEach(sku -> {
-                    AzureGetImageRef image = new AzureGetImageRef(publisher.toString(), offer.toString(), sku.toString());
-                    String id = AzureImageOperationUtil.convertToImageSummaryId(image);
-                    String name = AzureImageOperationUtil.convertToImageSummaryName(image);
-                    images.add(new ImageSummary(id, name));
-                });
-            });
-        });
-
-        return images;
-    }
-
-    private PagedList<VirtualMachinePublisher> getPublishers(Azure azure) {
-        return azure.virtualMachineImages().publishers().listByRegion(this.defaultRegionName);
+        return new ArrayList<>(this.images.values());
     }
 
     @Override
     public ImageInstance getImage(String imageId, AzureUser cloudUser) throws FogbowException {
         // TODO(jadsonluan): Implement this method
         throw new NotImplementedException("getImage is not implemented yet");
+    }
+
+    @VisibleForTesting
+    Map<String, ImageSummary> loadImages(Azure azure) {
+        Map<String, ImageSummary> imageMap = new HashMap<>();
+
+        for (VirtualMachinePublisher publisher : this.operation.getPublishers(azure)) {
+            if (this.publishers.contains(publisher.name())) {
+                for (VirtualMachineOffer offer : this.operation.getOffersFrom(publisher)) {
+                    for (VirtualMachineSku sku : this.operation.getSkusFrom(offer)) {
+                        String publisherName = publisher.name();
+                        String offerName = offer.name();
+                        String skuName = sku.name();
+                        ImageSummary imageSummary = AzureImageOperationUtil.buildImageSummaryBy(publisherName, offerName, skuName);
+                        imageMap.put(imageSummary.getId(), imageSummary);
+                    }
+                }
+            }
+        }
+
+        return imageMap;
     }
 }
