6ca09a7ef85e9fd5546d1948c48033286bee1d96
wassoares <wlfsoares@gmail.com>
2020-03-16 12:57:34 -0300

    Added the getAttachmentExecutor method into AzureSchedulerManager class

--- src/main/java/cloud/fogbow/ras/core/plugins/interoperability/azure/util/AzureSchedulerManager.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/azure/util/AzureSchedulerManager.java
@@ -5,9 +5,14 @@ import java.util.concurrent.Executors;
 
 public class AzureSchedulerManager {
 
+    private static final int ATTACHMENT_POOL_SIZE = 2;
     private static final int VIRTUAL_MACHINE_POOL_SIZE = 2;
     private static final int VOLUME_POOL_SIZE = 2;
 
+    public static ExecutorService getAttachmentExecutor() {
+        return Executors.newFixedThreadPool(ATTACHMENT_POOL_SIZE);
+    }
+    
     public static ExecutorService getVirtualMachineExecutor() {
         return Executors.newFixedThreadPool(VIRTUAL_MACHINE_POOL_SIZE);
     }
