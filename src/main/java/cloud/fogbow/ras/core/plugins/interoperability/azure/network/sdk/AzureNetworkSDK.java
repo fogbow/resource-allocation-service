7606aa2b9141361f17714e42d356bf34e52aa03a
wassoares <wlfsoares@gmail.com>
2020-03-26 16:48:18 -0300

    Added new methods into AzureNetworkSDK class

--- src/main/java/cloud/fogbow/ras/core/plugins/interoperability/azure/network/sdk/AzureNetworkSDK.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/azure/network/sdk/AzureNetworkSDK.java
@@ -1,9 +1,14 @@
 package cloud.fogbow.ras.core.plugins.interoperability.azure.network.sdk;
 
 import cloud.fogbow.common.exceptions.UnexpectedException;
+import cloud.fogbow.ras.constants.Messages;
+
+import com.google.common.annotations.VisibleForTesting;
 import com.microsoft.azure.management.Azure;
+import com.microsoft.azure.management.network.Network;
 import com.microsoft.azure.management.network.NetworkInterface;
 import com.microsoft.azure.management.network.NetworkInterfaces;
+import com.microsoft.azure.management.network.Networks;
 
 import java.util.Optional;
 
@@ -23,6 +28,22 @@ public class AzureNetworkSDK {
 
     public static NetworkInterfaces getNetworkInterfacesSDK(Azure azure) {
         return azure.networkInterfaces();
+    }
+
+    public static Optional<Network> getNetwork(Azure azure, String resourceId)
+            throws UnexpectedException {
+        try {
+            Networks networks = getNetworkSDK(azure);
+            return Optional.ofNullable(networks.getById(resourceId));
+        } catch (Exception e) {
+            String message = String.format(Messages.Exception.GENERIC_EXCEPTION, e);
+            throw new UnexpectedException(message, e);
+        }
+    }
+
+    @VisibleForTesting
+    static Networks getNetworkSDK(Azure azure) {
+        return azure.networks();
     }
 
 }
