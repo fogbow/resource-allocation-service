86a7ff129450b31c324c39c9868eae73be8d574e
fgan1 <fgan1@hotmail.com>
2020-03-30 15:37:48 -0300

    Add test classes

---
+++ src/test/java/cloud/fogbow/ras/core/plugins/interoperability/azure/securityrule/AzureNetworkSecurityGroupOperationSDKTest.java
@@ -0,0 +1,4 @@
+package cloud.fogbow.ras.core.plugins.interoperability.azure.securityrule;
+
+public class AzureNetworkSecurityGroupOperationSDKTest {
+}
---
+++ src/test/java/cloud/fogbow/ras/core/plugins/interoperability/azure/securityrule/util/AzureSecurityRuleUtilTest.java
@@ -0,0 +1,4 @@
+package cloud.fogbow.ras.core.plugins.interoperability.azure.securityrule.util;
+
+public class AzureSecurityRuleUtilTest {
+}
