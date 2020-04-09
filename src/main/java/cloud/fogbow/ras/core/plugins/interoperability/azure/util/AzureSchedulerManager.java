baa4afac32747e43324749c9aa2a17d15cec5c88
wassoares <wlfsoares@gmail.com>
2020-04-09 09:55:01 -0300

    Added getPublicIpExecutor method into AzureSchedulerManager class

--- src/main/java/cloud/fogbow/ras/core/plugins/interoperability/azure/util/AzureSchedulerManager.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/azure/util/AzureSchedulerManager.java
@@ -6,9 +6,14 @@ import java.util.concurrent.Executors;
 public class AzureSchedulerManager {
 
     private static final int VIRTUAL_MACHINE_POOL_SIZE = 2;
+    private static final int PUBLIC_IP_POOL_SIZE = 2;
 
     public static ExecutorService getVirtualMachineExecutor() {
         return Executors.newFixedThreadPool(VIRTUAL_MACHINE_POOL_SIZE);
+    }
+
+    public static ExecutorService getPublicIpExecutor() {
+        return Executors.newFixedThreadPool(PUBLIC_IP_POOL_SIZE);
     }
 
 }
