d711720d4d93ee98cfd884fbe05577443aa06dca
wassoares <wlfsoares@gmail.com>
2020-04-14 16:46:25 -0300

    Changes made into AzurePublicIPAddressSDK class

--- src/main/java/cloud/fogbow/ras/core/plugins/interoperability/azure/publicip/sdk/AzurePublicIPAddressSDK.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/azure/publicip/sdk/AzurePublicIPAddressSDK.java
@@ -5,10 +5,15 @@ import java.util.Optional;
 import com.google.common.annotations.VisibleForTesting;
 import com.microsoft.azure.management.Azure;
 import com.microsoft.azure.management.compute.VirtualMachine;
+import com.microsoft.azure.management.network.Network;
 import com.microsoft.azure.management.network.NetworkInterface;
 import com.microsoft.azure.management.network.NetworkSecurityGroup;
+import com.microsoft.azure.management.network.Networks;
 import com.microsoft.azure.management.network.PublicIPAddress;
 import com.microsoft.azure.management.network.PublicIPAddresses;
+import com.microsoft.azure.management.network.SecurityRuleProtocol;
+import com.microsoft.azure.management.network.Subnet;
+import com.microsoft.azure.management.network.implementation.NetworkManager;
 import com.microsoft.azure.management.resources.fluentcore.model.Creatable;
 
 import cloud.fogbow.common.exceptions.UnexpectedException;
@@ -18,39 +23,92 @@ import rx.Observable;
 
 public class AzurePublicIPAddressSDK {
 
-    public static void deleteSecurityRuleFrom(VirtualMachine virtualMachine) {
-        NetworkInterface networkInterface = virtualMachine.getPrimaryNetworkInterface();
-        NetworkSecurityGroup networkSecurityGroup = networkInterface.getNetworkSecurityGroup();
-        networkSecurityGroup.update().withoutRule("ruleName").apply();
+    @VisibleForTesting
+    static final int SSH_ACCESS_PORT = 22;
+    @VisibleForTesting
+    static final String SECURITY_RULE_NAME_SUFIX = "_security-rule";
+
+    public static Observable<NetworkInterface> associatePublicIPAddressAsync(
+            NetworkInterface networkInterface, Creatable<PublicIPAddress> creatable) {
+
+        return networkInterface.update()
+                .withNewPrimaryPublicIPAddress(creatable)
+                .applyAsync();
     }
     
-    public static void associatePublicIPAddressCreatable(VirtualMachine virtualMachine,
-            Creatable<PublicIPAddress> publicIPCreatable) throws UnexpectedException {
-        try {
-            NetworkInterface networkInterface = virtualMachine.getPrimaryNetworkInterface();
-            networkInterface.update().withNewPrimaryPublicIPAddress(publicIPCreatable).apply();
-        } catch (Exception e) {
-            String message = String.format(Messages.Exception.GENERIC_EXCEPTION, e);
-            throw new UnexpectedException(message, e);
-        }
+    public static Creatable<NetworkSecurityGroup> buildSecurityRuleCreatable(Azure azure,
+            PublicIPAddress publicIPAddress, NetworkSecurityGroup networkSecurityGroup) {
+
+        String networkSecurityGroupName = networkSecurityGroup.name();
+        String regionName = publicIPAddress.regionName();
+        String resourceGroupName = publicIPAddress.resourceGroupName();
+        String securityRuleName = publicIPAddress.name() + SECURITY_RULE_NAME_SUFIX;
+        String ipAddress = publicIPAddress.ipAddress();
+
+        return azure.networkSecurityGroups()
+                .define(networkSecurityGroupName)
+                .withRegion(regionName)
+                .withExistingResourceGroup(resourceGroupName)
+                .defineRule(securityRuleName)
+                    .allowInbound()
+                    .fromAnyAddress()
+                    .fromAnyPort()
+                    .toAddress(ipAddress)
+                    .toPort(SSH_ACCESS_PORT)
+                    .withProtocol(SecurityRuleProtocol.ASTERISK)
+                    .attach();
     }
     
-    public static void disassociatePublicIPAddressFrom(VirtualMachine virtualMachine)
+    public static Observable<NetworkInterface> associateNetworkSecurityGroupAsync(
+            NetworkInterface networkInterface, Creatable<NetworkSecurityGroup> creatable) {
+
+        return networkInterface.update()
+                .withNewNetworkSecurityGroup(creatable)
+                .applyAsync();
+    }
+
+    public static Observable<NetworkSecurityGroup> deleteSecurityRuleAsync(
+            NetworkSecurityGroup networkSecurityGroup, String resourceName) {
+
+        String securityRuleName = resourceName + SECURITY_RULE_NAME_SUFIX;
+        return networkSecurityGroup.update()
+                .withoutRule(securityRuleName)
+                .applyAsync();
+    }
+
+    public static Observable<NetworkInterface> disassociateNetworkSecurityGroupAsync(
+            NetworkInterface networkInterface) {
+
+        return networkInterface.update()
+                .withoutNetworkSecurityGroup()
+                .applyAsync();
+    }
+    
+    public static Observable<NetworkInterface> disassociatePublicIPAddressAsync(
+            NetworkInterface networkInterface) {
+
+        return networkInterface.update()
+                .withoutPrimaryPublicIPAddress()
+                .applyAsync();
+    }
+
+    public static Completable deletePublicIpAddressAsync(Azure azure, String resourceId) {
+        PublicIPAddresses publicIPAddresses = getPublicIPAddressesSDK(azure);
+        return publicIPAddresses.deleteByIdAsync(resourceId);
+    }
+
+    public static NetworkSecurityGroup getNetworkSecurityGroupFrom(NetworkInterface networkInterface) {
+        NetworkManager networkManager = networkInterface.manager();
+        Networks networks = networkManager.networks();
+        Network network = networks.list().listIterator().next();
+        Subnet subnet = network.subnets().values().iterator().next();
+        return subnet.getNetworkSecurityGroup();
+    }
+
+    public static NetworkInterface getPrimaryNetworkInterfaceFrom(VirtualMachine virtualMachine)
             throws UnexpectedException {
         try {
-            NetworkInterface networkInterface = virtualMachine.getPrimaryNetworkInterface();
-            networkInterface.update().withoutPrimaryPublicIPAddress().apply();
-        } catch (Exception e) {
-            String message = String.format(Messages.Exception.GENERIC_EXCEPTION, e);
-            throw new UnexpectedException(message, e);
-        }
-    }
-    
-    public static Completable buildDeletePublicIpAddressCompletable(Azure azure, String resourceId)
-            throws UnexpectedException {
-        try {
-            PublicIPAddresses publicIPAddresses = getPublicIPAddressesSDK(azure);
-            return publicIPAddresses.deleteByIdAsync(resourceId);
+            return virtualMachine.getPrimaryNetworkInterface();
         } catch (Exception e) {
             String message = String.format(Messages.Exception.GENERIC_EXCEPTION, e);
             throw new UnexpectedException(message, e);
