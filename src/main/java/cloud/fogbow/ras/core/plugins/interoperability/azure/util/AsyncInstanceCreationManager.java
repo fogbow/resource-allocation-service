d2170fc9b511d7109b15dda8b1b91b4e74155ed0
fgan1 <fgan1@hotmail.com>
2020-05-07 11:01:50 -0300

    Implementing AzureAsync context at PublicIpPlugin

--- src/main/java/cloud/fogbow/ras/core/plugins/interoperability/azure/util/AsyncInstanceCreationManager.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/azure/util/AsyncInstanceCreationManager.java
@@ -21,6 +21,7 @@ public class AsyncInstanceCreationManager {
     @return It is a callback that it must be used when the cloud finish asynchronous creation operation in the cloud;
     Suggestion: Use "finishCreationCallback" as attribute name.
        */
+    // TODO(chico) - Add log debug
     public Runnable startCreation(String instanceId) {
         defineAsCreating(instanceId);
         return () -> defineAsCreated(instanceId);
