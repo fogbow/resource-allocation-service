1967d4d60818c97ccb689054d4113cf7550d3484
Jadson Luan <jadsonluan1@gmail.com>
2020-09-01 15:19:16 -0300

    Implement EmulatedSecurityGroup class

---
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/emulatedcloud/emulatedmodels/EmulatedSecurityGroup.java
@@ -0,0 +1,60 @@
+package cloud.fogbow.ras.core.plugins.interoperability.emulatedcloud.emulatedmodels;
+
+import cloud.fogbow.common.util.GsonHolder;
+import cloud.fogbow.common.util.JsonSerializable;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+import static cloud.fogbow.ras.core.plugins.interoperability.emulatedcloud.EmulatedCloudConstants.Json.SECURITY_RULES_KEY_JSON;
+import static cloud.fogbow.ras.core.plugins.interoperability.emulatedcloud.EmulatedCloudConstants.Json.ID_KEY_JSON;
+
+public class EmulatedSecurityRuleGroup implements JsonSerializable {
+
+    @SerializedName(ID_KEY_JSON)
+    private String id;
+
+    @SerializedName(SECURITY_RULES_KEY_JSON)
+    private List<EmulatedSecurityRule> securityRules;
+
+    private EmulatedSecurityRuleGroup(String id, List<EmulatedSecurityRule> securityRules) {
+        this.id = id;
+        this.securityRules = securityRules;
+    }
+
+    @Override
+    public String toJson() {
+        return GsonHolder.getInstance().toJson(this);
+    }
+
+    public static EmulatedSecurityRuleGroup fromJson(String json) {
+        return GsonHolder.getInstance().fromJson(json, EmulatedSecurityRuleGroup.class);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public List<EmulatedSecurityRule> getSecurityRules() {
+        return securityRules;
+    }
+
+    public static class Builder {
+        private String id;
+        private List<EmulatedSecurityRule> securityRules;
+
+        public Builder id(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder securityRules(List<EmulatedSecurityRule> securityRules) {
+            this.securityRules = securityRules;
+            return this;
+        }
+
+        public EmulatedSecurityRuleGroup build() {
+            return new EmulatedSecurityRuleGroup(id, securityRules);
+        }
+    }
+}
