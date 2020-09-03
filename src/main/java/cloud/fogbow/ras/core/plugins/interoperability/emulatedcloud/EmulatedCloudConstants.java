0222964b4dede187fa2117b3b51a1a4b30b8c644
Jadson Luan <jadsonluan1@gmail.com>
2020-09-02 22:04:49 -0300

    Add new conf constants to EmulatedCloudConstants class

--- src/main/java/cloud/fogbow/ras/core/plugins/interoperability/emulatedcloud/EmulatedCloudConstants.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/emulatedcloud/EmulatedCloudConstants.java
@@ -7,6 +7,16 @@ public class EmulatedCloudConstants {
 
     public static class Conf {
         public static final String IMAGE_NAMES_KEY = "image_names";
+
+        public static final String QUOTA_INSTANCES_KEY = "quota_instances";
+        public static final String QUOTA_RAM_KEY = "quota_ram";
+        public static final String QUOTA_VCPU_KEY = "quota_vCPU";
+
+        public static final String QUOTA_VOLUMES_KEY = "quota_volumes";
+        public static final String QUOTA_STORAGE_KEY = "quota_storage";
+
+        public static final String QUOTA_NETWORKS_KEY = "quota_networks";
+        public static final String QUOTA_PUBLIC_IP_KEY = "quota_public_ips";
     }
 
     public static class Plugins {
@@ -28,6 +38,8 @@ public class EmulatedCloudConstants {
         public static final String UNABLE_TO_ATTACH_COMPUTE_NOT_FOUND = "Unable to attach. Compute not found.";
         public static final String UNABLE_TO_ATTACH_VOLUME_NOT_FOUND = "Unable to attach. Volume not found.";
         public static final String NO_IMAGE_NAMES_SPECIFIED = "No image names specified in the cloud.conf file";
+        public static final String THE_REQUIRED_PROPERTY_S_WAS_NOT_SPECIFIED = "The required property %s was not specified.";
+        public static final String THE_PROPERTY_S_MUST_BE_AN_INTEGER = "The property %s must be an integer.";
     }
 }
 
