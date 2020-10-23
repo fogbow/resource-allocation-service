5679c6c3fd08b95f204b0ff674a86c8cad604bba
DacioSB <dacio.bezerra@ccc.ufcg.edu.br>
2020-10-22 22:03:31 -0300

    Implemented Network And Subnetwork Requests

--- src/main/java/cloud/fogbow/ras/core/plugins/interoperability/googlecloud/sdk/v1/network/models/InsertNetworkResponse.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/googlecloud/sdk/v1/network/models/InsertNetworkResponse.java
@@ -3,27 +3,28 @@ package cloud.fogbow.ras.core.plugins.interoperability.googlecloud.sdk.v1.networ
 import cloud.fogbow.common.util.GsonHolder;
 import com.google.gson.annotations.SerializedName;
 import static cloud.fogbow.ras.core.plugins.interoperability.googlecloud.util.GoogleCloudConstants.Network.NETWORK_KEY_JSON;
+import static cloud.fogbow.ras.core.plugins.interoperability.googlecloud.util.GoogleCloudConstants.Network.TARGET_LINK_KEY_JSON;
 
 /**
  * Documentation: https://cloud.google.com/compute/docs/reference/rest/v1/networks
  * <p>
  * Response Example:
  * {
- *  "network": https://www.googleapis.com/compute/v1/projects/chatflutter-5c6ad/global/networks/net1
+ *  "targetLink": https://www.googleapis.com/compute/v1/projects/chatflutter-5c6ad/global/networks/net1
  * }
  * <p>
  * We use the @SerializedName annotation to specify that the request parameter is not equal to the class field.
  */
 public class InsertNetworkResponse {
-    @SerializedName(NETWORK_KEY_JSON)
-    private String network;
+    @SerializedName(TARGET_LINK_KEY_JSON)
+    private String targetLink;
 
-    public InsertNetworkResponse(String network){
-        this.network = network;
+    public InsertNetworkResponse(String targetLink){
+        this.targetLink = targetLink;
     }
 
-    public String getNetwork(){
-        return this.network;
+    public String getTargetLink(){
+        return this.targetLink;
     }
 
     public String toJson() {
