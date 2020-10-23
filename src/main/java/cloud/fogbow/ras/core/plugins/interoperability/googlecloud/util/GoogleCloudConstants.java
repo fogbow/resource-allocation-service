9ac4073b87449fd8734e18963ebd7f35c2b2d6d6
Pedro Silva <pedro.henrique.silva@ccc.ufcg.edu.br>
2020-10-23 18:08:51 -0300

    Deleted GoogleCloudConstants since it is not necessary anymore.

--- src/main/java/cloud/fogbow/ras/core/plugins/interoperability/googlecloud/util/GoogleCloudConstants.java
+++
@@ -1,7 +0,0 @@
-package cloud.fogbow.ras.core.plugins.interoperability.googlecloud.util;
-
-import cloud.fogbow.ras.api.parameters.SecurityRule.EtherType;
-
-public class GoogleCloudConstants {
-
-}
