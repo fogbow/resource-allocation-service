7421e359dd5959ecfa772922dcf97b7efe6780da
raisson souto <raisson.souto@ccc.ufcg.edu.br>
2020-10-30 00:31:19 -0300

    adding some generic code for the PausingProcessor and ResumingProcessor.

--- src/main/java/cloud/fogbow/ras/core/processors/ResumingProcessor.java
+++ src/main/java/cloud/fogbow/ras/core/processors/ResumingProcessor.java
@@ -1,10 +1,8 @@
 package cloud.fogbow.ras.core.processors;
 
 import cloud.fogbow.common.exceptions.FogbowException;
-import cloud.fogbow.common.exceptions.InternalServerErrorException;
-import cloud.fogbow.common.exceptions.UnavailableProviderException;
+import cloud.fogbow.common.exceptions.InstanceNotFoundException;
 import cloud.fogbow.common.models.linkedlists.ChainedList;
-import cloud.fogbow.ras.api.http.response.OrderInstance;
 import cloud.fogbow.ras.constants.Messages;
 import cloud.fogbow.ras.core.OrderStateTransitioner;
 import cloud.fogbow.ras.core.SharedOrderHolders;
@@ -12,63 +10,80 @@ import cloud.fogbow.ras.core.cloudconnector.CloudConnectorFactory;
 import cloud.fogbow.ras.core.cloudconnector.LocalCloudConnector;
 import cloud.fogbow.ras.core.models.orders.Order;
 import cloud.fogbow.ras.core.models.orders.OrderState;
+import com.google.common.annotations.VisibleForTesting;
 import org.apache.log4j.Logger;
 
 public class ResumingProcessor implements Runnable {
-    private static final Logger LOGGER = Logger.getLogger(cloud.fogbow.ras.core.processors.ResumingProcessor.class);
+    private static final Logger LOGGER = Logger.getLogger(AssignedForDeletionProcessor.class);
 
     private String localProviderId;
-    private ChainedList<Order> resumingOrderList;
+    private ChainedList<Order> resumingOrdersList;
     /**
      * Attribute that represents the thread sleep time when there are no orders to be processed.
      */
     private Long sleepTime;
 
-
-    public ResumingProcessor(String providerId, String sleepTimeStr) {
+    public ResumingProcessor(String localProviderId, String sleepTimeStr) {
+        this.localProviderId = localProviderId;
         SharedOrderHolders sharedOrderHolders = SharedOrderHolders.getInstance();
-        this.resumingOrderList = sharedOrderHolders.getResumingOrdersList();
+        this.resumingOrdersList = sharedOrderHolders.getResumingOrdersList();
         this.sleepTime = Long.valueOf(sleepTimeStr);
-        this.localProviderId = providerId;
     }
 
     /**
-     * Iterates over the spawning orders list and tries to process one order at a time. When the order
+     * Iterates over the assignedForDeletion orders list and tries to process one order at a time. When the order
      * is null, it indicates that the iteration ended. A new iteration is started after some time.
      */
     @Override
     public void run() {
         boolean isActive = true;
-        Order order = null;
         while (isActive) {
             try {
-                order = this.resumingOrderList.getNext();
-                if (order != null) {
-                    processResumingOrder(order);
-                } else {
-                    this.resumingOrderList.resetPointer();
-                    Thread.sleep(this.sleepTime);
-                }
+                assignForDeletion();
             } catch (InterruptedException e) {
                 isActive = false;
-                LOGGER.error(Messages.Log.THREAD_HAS_BEEN_INTERRUPTED, e);
-            } catch (InternalServerErrorException e) {
-                LOGGER.error(e.getMessage(), e);
-            } catch (Throwable e) {
-                LOGGER.error(Messages.Log.UNEXPECTED_ERROR, e);
             }
         }
     }
 
-    protected void processResumingOrder(Order order) throws FogbowException {
-        // The order object synchronization is needed to prevent a race
-        // condition on order access. For example: a user can delete an spawning
-        // order while this method is trying to check the status of an instance
-        // that has been requested in the cloud.
+    @VisibleForTesting
+    void assignForDeletion() throws InterruptedException {
+        try {
+            Order order = this.resumingOrdersList.getNext();
+
+            if (order != null) {
+                processResumingOrder(order);
+            } else {
+                this.resumingOrdersList.resetPointer();
+                Thread.sleep(this.sleepTime);
+            }
+        } catch (InterruptedException e) {
+            LOGGER.error(Messages.Log.THREAD_HAS_BEEN_INTERRUPTED, e);
+            throw e;
+        } catch (FogbowException e) {
+            LOGGER.error(e.getMessage(), e);
+        } catch (Throwable e) {
+            LOGGER.error(Messages.Log.UNEXPECTED_ERROR, e);
+        }
+    }
+
+    /**
+     * Starts the deletion procedure in the cloud. Some plugins do this synchronously, others do asynchronously.
+     * Thus, we always assume an asynchronous semantic. This threads issues the delete in the cloud, and transitions
+     * the order to the CHECKING_DELETION state. The CheckingDeletion processor monitors when the operation has
+     * finished. Essentially it keeps repeating getInstance() calls until an InstanceNotFound exception is raised.
+     *
+     * @param order {@link Order}
+     */
+    @VisibleForTesting
+    void processResumingOrder(Order order) throws FogbowException {
+        // The order object synchronization is needed to prevent a race condition on order access.
         synchronized (order) {
-            // Check if the order is still in the SPAWNING state (it could have been changed by another thread)
+            // Check if the order is still in the ASSIGNED_FOR_DELETION state (for this particular state, this should
+            // always happen, since once the order gets to this state, only this thread can operate on it. However,
+            // the cost of safe programming is low).
             OrderState orderState = order.getOrderState();
-            if (!orderState.equals(OrderState.SPAWNING)) { //mudar aq
+            if (!orderState.equals(OrderState.ASSIGNED_FOR_DELETION)) {
                 return;
             }
             // Only local orders need to be monitored. Remote orders are monitored by the remote provider.
@@ -80,26 +95,19 @@ public class ResumingProcessor implements Runnable {
                 LOGGER.error(Messages.Log.UNEXPECTED_ERROR);
                 return;
             }
-            // Here we know that the CloudConnector is local, but the use of CloudConnectFactory facilitates testing.
-            LocalCloudConnector localCloudConnector = (LocalCloudConnector)
-                    CloudConnectorFactory.getInstance().getCloudConnector(this.localProviderId, order.getCloudName());
-            // We don't audit requests we make
-            localCloudConnector.switchOffAuditing();
-
             try {
-                OrderInstance instance = localCloudConnector.getInstance(order);
-                if (instance.hasFailed()) {
-                    OrderStateTransitioner.transition(order, OrderState.FAILED_AFTER_SUCCESSFUL_REQUEST);
-                } else if (instance.isReady()) {
-                    OrderStateTransitioner.transition(order, OrderState.FULFILLED);
-                }
-            } catch (UnavailableProviderException e1) {
-                OrderStateTransitioner.transition(order, OrderState.UNABLE_TO_CHECK_STATUS);
-                throw e1;
-            } catch (Exception e2) {
-                order.setOnceFaultMessage(e2.getMessage());
-                LOGGER.info(String.format(Messages.Exception.GENERIC_EXCEPTION_S, e2));
-                OrderStateTransitioner.transition(order, OrderState.FAILED_AFTER_SUCCESSFUL_REQUEST);
+                // Here we know that the CloudConnector is local, but the use of CloudConnectFactory facilitates testing.
+                LocalCloudConnector localCloudConnector = (LocalCloudConnector)
+                        CloudConnectorFactory.getInstance().getCloudConnector(this.localProviderId, order.getCloudName());
+                localCloudConnector.deleteInstance(order);
+                OrderStateTransitioner.transition(order, OrderState.CHECKING_DELETION);
+            } catch (InstanceNotFoundException e) {
+                // If the provider crashes after calling deleteInstance() and before setting the order's state to
+                // CHECKING_DELETION, then the deleteInstance() method will be called again, after recovery.
+                // This is not an issue, because calling deleteInstance() multiple times has no undesired collateral
+                // effect. The order needs simply to be advanced to the CHECKING_DELETION state, to later be closed
+                // by the CheckingDeletion processor.
+                OrderStateTransitioner.transition(order, OrderState.CHECKING_DELETION);
             }
         }
     }
