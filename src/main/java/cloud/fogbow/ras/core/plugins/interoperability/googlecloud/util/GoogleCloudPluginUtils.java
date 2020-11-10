ad9f6db35b4a0a882ec3e3468b181624fba729f9
DacioSB <dacio.bezerra@ccc.ufcg.edu.br>
2020-11-10 19:07:04 -0300

    Ending Google Cloud Network Basic Implementation

--- src/main/java/cloud/fogbow/ras/core/plugins/interoperability/googlecloud/util/GoogleCloudPluginUtils.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/googlecloud/util/GoogleCloudPluginUtils.java
@@ -9,8 +9,6 @@ import org.apache.log4j.Logger;
 public class GoogleCloudPluginUtils {
     private static final Logger LOGGER = Logger.getLogger(GoogleCloudPluginUtils.class);
     public static final String VOLUME_COMPUTE_URL_KEY = "googlecloud_compute_url";
-    public static final String PROJECT_NAME_EXAMPLE = "diesel_talon";
-    public static final String NETWORK_URL_KEY = "google_cloud_net_url";
 
     public static String getProjectIdFrom(GoogleCloudUser cloudUser) throws InvalidParameterException {
         String projectId = cloudUser.getProjectId();
