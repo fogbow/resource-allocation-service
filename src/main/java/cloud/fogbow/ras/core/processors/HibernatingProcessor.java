ef51d6ee80473d888a0c68d54950aa401f2f10ba
Jadson Luan <jadsonluan1@gmail.com>
2020-11-18 22:55:20 -0300

    Fix new processors

--- src/main/java/cloud/fogbow/ras/core/processors/HibernatingProcessor.java
+++ src/main/java/cloud/fogbow/ras/core/processors/HibernatingProcessor.java
@@ -3,11 +3,15 @@ package cloud.fogbow.ras.core.processors;
 import cloud.fogbow.common.exceptions.FogbowException;
 import cloud.fogbow.common.exceptions.InstanceNotFoundException;
 import cloud.fogbow.common.models.linkedlists.ChainedList;
+import cloud.fogbow.ras.api.http.response.ComputeInstance;
+import cloud.fogbow.ras.api.http.response.InstanceState;
+import cloud.fogbow.ras.api.http.response.OrderInstance;
 import cloud.fogbow.ras.constants.Messages;
 import cloud.fogbow.ras.core.OrderStateTransitioner;
 import cloud.fogbow.ras.core.SharedOrderHolders;
 import cloud.fogbow.ras.core.cloudconnector.CloudConnectorFactory;
 import cloud.fogbow.ras.core.cloudconnector.LocalCloudConnector;
+import cloud.fogbow.ras.core.models.ResourceType;
 import cloud.fogbow.ras.core.models.orders.Order;
 import cloud.fogbow.ras.core.models.orders.OrderState;
 import com.google.common.annotations.VisibleForTesting;
@@ -85,13 +89,19 @@ public class HibernatingProcessor extends StoppableProcessor implements Runnable
     void processHibernatingOrder(Order order) throws FogbowException {
         // The order object synchronization is needed to prevent a race condition on order access.
         synchronized (order) {
-            // Check if the order is still in the ASSIGNED_FOR_DELETION state (for this particular state, this should
+            // Check if the order is still in the HIBERNATING state (for this particular state, this should
             // always happen, since once the order gets to this state, only this thread can operate on it. However,
             // the cost of safe programming is low).
             OrderState orderState = order.getOrderState();
-            if (!orderState.equals(OrderState.ASSIGNED_FOR_DELETION)) {
+            if (!orderState.equals(OrderState.HIBERNATING)) {
                 return;
             }
+
+            // Only compute order can be hibernated.
+            if (!order.getType().equals(ResourceType.COMPUTE)) {
+                return;
+            }
+
             // Only local orders need to be monitored. Remote orders are monitored by the remote provider.
             // State changes that happen at the remote provider are synchronized by the RemoteOrdersStateSynchronization
             // processor.
@@ -105,15 +115,14 @@ public class HibernatingProcessor extends StoppableProcessor implements Runnable
                 // Here we know that the CloudConnector is local, but the use of CloudConnectFactory facilitates testing.
                 LocalCloudConnector localCloudConnector = (LocalCloudConnector)
                         CloudConnectorFactory.getInstance().getCloudConnector(this.localProviderId, order.getCloudName());
-                localCloudConnector.deleteInstance(order);
-                OrderStateTransitioner.transition(order, OrderState.CHECKING_DELETION);
+
+                ComputeInstance instance = (ComputeInstance) localCloudConnector.getInstance(order);
+
+                if (instance.isHibernated()) {
+                    OrderStateTransitioner.transition(order, OrderState.HIBERNATED);
+                }
             } catch (InstanceNotFoundException e) {
-                // If the provider crashes after calling deleteInstance() and before setting the order's state to
-                // CHECKING_DELETION, then the deleteInstance() method will be called again, after recovery.
-                // This is not an issue, because calling deleteInstance() multiple times has no undesired collateral
-                // effect. The order needs simply to be advanced to the CHECKING_DELETION state, to later be closed
-                // by the CheckingDeletion processor.
-                OrderStateTransitioner.transition(order, OrderState.CHECKING_DELETION);
+                OrderStateTransitioner.transition(order, OrderState.FAILED_AFTER_SUCCESSFUL_REQUEST);
             }
         }
     }
