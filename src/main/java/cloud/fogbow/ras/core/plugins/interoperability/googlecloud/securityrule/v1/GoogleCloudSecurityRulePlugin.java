24eeed6fd819ccc85f1356e6c241e4750bcee84c
Jadson Luan <jadsonluan1@gmail.com>
2021-01-10 14:30:31 -0300

    Refactor GoogleCloudSecurityRulePlugin

--- src/main/java/cloud/fogbow/ras/core/plugins/interoperability/googlecloud/securityrule/v1/GoogleCloudSecurityRulePlugin.java
+++ src/main/java/cloud/fogbow/ras/core/plugins/interoperability/googlecloud/securityrule/v1/GoogleCloudSecurityRulePlugin.java
@@ -4,7 +4,6 @@ import cloud.fogbow.as.core.models.GoogleCloudSystemUser;
 import cloud.fogbow.common.constants.GoogleCloudConstants;
 import cloud.fogbow.common.exceptions.FogbowException;
 import cloud.fogbow.common.models.GoogleCloudUser;
-import cloud.fogbow.common.util.PropertiesUtil;
 import cloud.fogbow.common.util.connectivity.cloud.googlecloud.GoogleCloudHttpClient;
 import cloud.fogbow.ras.api.http.response.SecurityRuleInstance;
 import cloud.fogbow.ras.api.parameters.SecurityRule;
@@ -17,12 +16,11 @@ import cloud.fogbow.ras.core.plugins.interoperability.googlecloud.sdk.v1.securit
 import cloud.fogbow.ras.core.plugins.interoperability.googlecloud.sdk.v1.securityrule.models.CreateFirewallRuleResponse;
 import cloud.fogbow.ras.core.plugins.interoperability.googlecloud.sdk.v1.securityrule.models.GetFirewallRuleResponse;
 import cloud.fogbow.ras.core.plugins.interoperability.googlecloud.sdk.v1.securityrule.models.GetFirewallRulesResponse;
-import cloud.fogbow.ras.core.plugins.interoperability.googlecloud.util.GoogleCloudPluginUtils;
+import com.google.common.annotations.VisibleForTesting;
 import org.apache.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 import java.util.UUID;
 
 public class GoogleCloudSecurityRulePlugin implements SecurityRulePlugin<GoogleCloudUser> {
@@ -32,15 +30,7 @@ public class GoogleCloudSecurityRulePlugin implements SecurityRulePlugin<GoogleC
     private String prefixEndpoint;
     private GoogleCloudHttpClient client;
 
-    private static class NetworkProtocol {
-        private static final String TCP = SecurityRule.Protocol.TCP.toString();
-        private static final String UDP = SecurityRule.Protocol.UDP.toString();
-        private static final String ICMP = SecurityRule.Protocol.ICMP.toString();
-        private static final String ANY = SecurityRule.Protocol.ANY.toString();
-    }
-
     public GoogleCloudSecurityRulePlugin(String confFilePath) {
-        Properties properties = PropertiesUtil.readProperties(confFilePath);
         this.prefixEndpoint = GoogleCloudConstants.BASE_COMPUTE_API_URL
                 + GoogleCloudConstants.COMPUTE_ENGINE_V1_ENDPOINT;
         this.client = new GoogleCloudHttpClient();
@@ -70,11 +60,13 @@ public class GoogleCloudSecurityRulePlugin implements SecurityRulePlugin<GoogleC
         return securityRuleName;
     }
 
-    public CreateFirewallRuleRequest buildCreateSecurityRuleRequest(SecurityRule securityRule, Order majorOrder, String securityRuleName) {
+    @VisibleForTesting
+    CreateFirewallRuleRequest buildCreateSecurityRuleRequest(SecurityRule securityRule, Order majorOrder, String securityRuleName) {
         return buildCreateSecurityRuleRequest(securityRule, majorOrder, GoogleCloudConstants.Network.Firewall.DEFAULT_RULE_PRIORITY, securityRuleName);
     }
 
-    public CreateFirewallRuleRequest buildCreateSecurityRuleRequest(SecurityRule securityRule, Order majorOrder, int securityRulePriority, String securityRuleName) {
+    @VisibleForTesting
+    CreateFirewallRuleRequest buildCreateSecurityRuleRequest(SecurityRule securityRule, Order majorOrder, int securityRulePriority, String securityRuleName) {
         String networkName = ((NetworkOrder) majorOrder).getName();
 
         String network = GoogleCloudConstants.Network.GLOBAL_NETWORKS_PATH
@@ -100,7 +92,8 @@ public class GoogleCloudSecurityRulePlugin implements SecurityRulePlugin<GoogleC
         return request;
     }
 
-    private boolean isPortAllowed(SecurityRule securityRule) {
+    @VisibleForTesting
+    boolean isPortAllowed(SecurityRule securityRule) {
         String incomingProtocol = securityRule.getProtocol().toString();
 
         return !incomingProtocol.equalsIgnoreCase(SecurityRule.Protocol.ICMP.toString());
@@ -108,6 +101,8 @@ public class GoogleCloudSecurityRulePlugin implements SecurityRulePlugin<GoogleC
 
     @Override
     public List<SecurityRuleInstance> getSecurityRules(Order majorOrder, GoogleCloudUser cloudUser) throws FogbowException {
+        LOGGER.info(String.format(Messages.Log.GETTING_INSTANCE_S, majorOrder.getInstanceId()));
+
         String endpoint = this.prefixEndpoint
                 + GoogleCloudConstants.PROJECT_ENDPOINT
                 + GoogleCloudConstants.ENDPOINT_SEPARATOR
@@ -118,7 +113,7 @@ public class GoogleCloudSecurityRulePlugin implements SecurityRulePlugin<GoogleC
         String networkName = ((NetworkOrder) majorOrder).getName();
 
         GetFirewallRulesResponse firewallRuleResponse = GetFirewallRulesResponse.fromJson(responseJson);
-        if(firewallRuleResponse.getSecurityGroupRules() == null) {
+        if (firewallRuleResponse.getSecurityGroupRules() == null) {
             LOGGER.debug(String.format(Messages.Log.CONTENT_SECURITY_GROUP_EMPTY));
             return new ArrayList<SecurityRuleInstance>();
         }
@@ -126,17 +121,18 @@ public class GoogleCloudSecurityRulePlugin implements SecurityRulePlugin<GoogleC
         return getSecurityRuleInstances(firewallRuleResponse, networkName);
     }
 
-    private List<SecurityRuleInstance> getSecurityRuleInstances(GetFirewallRulesResponse firewallRulesResponse, String networkName) {
+    @VisibleForTesting
+    List<SecurityRuleInstance> getSecurityRuleInstances(GetFirewallRulesResponse firewallRulesResponse, String networkName) {
         List<SecurityRuleInstance> securityRuleInstances = new ArrayList<>();
         List<GetFirewallRulesResponse.FirewallRule> firewallRules = firewallRulesResponse.getSecurityGroupRules();
 
-        for(GetFirewallRulesResponse.FirewallRule firewallRule: firewallRules) {
+        for (GetFirewallRulesResponse.FirewallRule firewallRule: firewallRules) {
             String id = firewallRule.getName();
 
             String firewallName = id.replaceAll(SystemConstants.PN_SECURITY_GROUP_PREFIX, "");
             String actualNetworkName = firewallName.split(GoogleCloudConstants.ELEMENT_SEPARATOR)[0];
 
-            if(actualNetworkName.equalsIgnoreCase(networkName)) {
+            if (actualNetworkName.equalsIgnoreCase(networkName)) {
                 String direction = firewallRule.getDirection();
                 String portRange = firewallRule.getPort();
                 String cidr = firewallRule.getCidr();
@@ -151,9 +147,9 @@ public class GoogleCloudSecurityRulePlugin implements SecurityRulePlugin<GoogleC
         return securityRuleInstances;
     }
 
-    private SecurityRuleInstance createSecurityRuleInstance(String id, String directionString, String portRange, String cidr,
+    @VisibleForTesting
+    SecurityRuleInstance createSecurityRuleInstance(String id, String directionString, String portRange, String cidr,
                                                             String etherTypeString, String protocolString) {
-
         SecurityRule.Direction direction = null;
         if(directionString.equalsIgnoreCase(SecurityRule.Direction.IN.toString())) {
             direction = SecurityRule.Direction.IN;
@@ -173,18 +169,18 @@ public class GoogleCloudSecurityRulePlugin implements SecurityRulePlugin<GoogleC
         }
 
         SecurityRule.EtherType etherType = null;
-        if(etherTypeString.equalsIgnoreCase(SecurityRule.EtherType.IPv4.toString())) {
+        if (etherTypeString.equalsIgnoreCase(SecurityRule.EtherType.IPv4.toString())) {
             etherType = SecurityRule.EtherType.IPv4;
         } else {
             etherType = SecurityRule.EtherType.IPv4;
         }
 
         SecurityRule.Protocol protocol = null;
-        if(protocolString.equalsIgnoreCase(SecurityRule.Protocol.TCP.toString())) {
+        if (protocolString.equalsIgnoreCase(SecurityRule.Protocol.TCP.toString())) {
             protocol = SecurityRule.Protocol.TCP;
-        } else if(protocolString.equalsIgnoreCase(SecurityRule.Protocol.UDP.toString())) {
+        } else if (protocolString.equalsIgnoreCase(SecurityRule.Protocol.UDP.toString())) {
             protocol = SecurityRule.Protocol.UDP;
-        } else if(protocolString.equalsIgnoreCase(SecurityRule.Protocol.ICMP.toString())) {
+        } else if (protocolString.equalsIgnoreCase(SecurityRule.Protocol.ICMP.toString())) {
             protocol = SecurityRule.Protocol.ICMP;
         } else {
             protocol = SecurityRule.Protocol.ANY;
@@ -194,7 +190,8 @@ public class GoogleCloudSecurityRulePlugin implements SecurityRulePlugin<GoogleC
         return securityRuleInstance;
     }
 
-    public SecurityRuleInstance getSecurityRule(String securityRuleName, GoogleCloudUser cloudUser) throws FogbowException {
+    @VisibleForTesting
+    SecurityRuleInstance getSecurityRule(String securityRuleName, GoogleCloudUser cloudUser) throws FogbowException {
         String endpoint = this.prefixEndpoint
                 + GoogleCloudConstants.PROJECT_ENDPOINT
                 + GoogleCloudConstants.ENDPOINT_SEPARATOR
@@ -221,7 +218,6 @@ public class GoogleCloudSecurityRulePlugin implements SecurityRulePlugin<GoogleC
 
     @Override
     public void deleteSecurityRule(String securityRuleId, GoogleCloudUser cloudUser) throws FogbowException {
-        SecurityRuleInstance securityRuleInstance = getSecurityRule(securityRuleId, cloudUser);
         String endpoint = this.prefixEndpoint
                 + GoogleCloudConstants.PROJECT_ENDPOINT
                 + GoogleCloudConstants.ENDPOINT_SEPARATOR
